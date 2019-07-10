031c1298c06edb5fdf688277da5deb225519a68b
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-07-10 16:37:27 -0500

    learning effect hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({})
+  const getData = () =>{
+    axios.get("https://api.nasa.gov.planetary/apod?api_key=DEMO_KEY")
+  .then(res => {
+    console.log(res.data);
+    setData(res.data);
+  })
+  .catch(err => {
+    console.log(err, "error help sir")
+  })
+  }
+
+  useEffect(getData, []);
   return (
     <div className="App">
       <p>
