b1820fc7882e8f29b5e3ff86df7300d6be9f2795
Dave Irwin <david@redirwin.com>
2019-07-10 17:46:23 -0700

    Set up folders, created files, imported header.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { exportDefaultDeclaration } from "@babel/types";
+
+function Header() {
+  return (
+    <header>
+      <h1>CosmoScout</h1>
+      <h2>Explore the Cosmos Through Beautiful NASA Imagery.</h2>
+    </header>
+  );
+}
+
+export default Header;
