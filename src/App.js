a686dba0424915d3cf79df0de8662286c93940ae
Dave Irwin <david@redirwin.com>
2019-07-10 17:50:24 -0700

    Imported useState and useEffect. Added state to App.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
 
 function App() {
+  const [media, updateMedia] = useState("");
+
   return (
     <div className="App">
       <Header />
