297079d209835969e48b743e28b808d36ef1565a
Dave Irwin <david@redirwin.com>
2019-07-10 20:49:37 -0700

    Added ability to select date of returned photo.

--- src/components/YourChoice.js
+++ src/components/YourChoice.js
@@ -6,19 +6,42 @@ import MediaCard from "./MediaCard";
 function YourChoice() {
   const [media, updateMedia] = useState("");
   const [date, updateDate] = useState("");
+  const [dateInput, updateDateInput] = useState("");
 
   const apiKey = "HL7Cmszg6ukeCduFdzUnboaD2ummKzuYzfYIyQRL";
 
   useEffect(() => {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&hd=true`)
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&hd=true&date=${date}`
+      )
       .then(res => updateMedia(res.data))
       .catch(err => console.log(err));
   }, [date]);
 
+  function handleChange(e) {
+    updateDateInput(e.target.value);
+    console.log(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    updateDate(dateInput);
+  }
+
   return (
     <div className="your-choice-container">
       <h3>Your Choice</h3>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="date"
+          id="date"
+          value={dateInput}
+          onChange={handleChange}
+        />
+        <button type="submit">Go!</button>
+      </form>
       {date ? <MediaCard media={media} /> : null}
     </div>
   );
