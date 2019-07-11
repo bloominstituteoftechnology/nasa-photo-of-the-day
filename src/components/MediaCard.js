41f9fb1067db422f095d61cf17859c9f844e09d4
Dave Irwin <david@redirwin.com>
2019-07-10 18:57:26 -0700

    Checking status of axios call and type of media returned.

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -2,12 +2,23 @@ import React from "react";
 
 function MediaCard(props) {
   console.log(props);
-  return (
-    <div>
-      <img src={props.media.hdurl} alt="media" />
-      <p>{props.media.explanation}</p>
-    </div>
-  );
+
+  function cardContent() {
+    if (!props.media) {
+      return "Loading...";
+    } else if (props.media.media_type === "image") {
+      return (
+        <>
+          <img src={props.media.hdurl} alt="media" />
+          <p>{props.media.explanation}</p>
+        </>
+      );
+    } else {
+      return <p>Not and image!</p>;
+    }
+  }
+
+  return <div>{cardContent()}</div>;
 }
 
 export default MediaCard;
