90ebf5ca703b06d4b6abce6a5e20c35ff86554ba
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-17 12:56:37 -0400

    inital commit. created a simple wireframe

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <div className='header'>
+        <header>
+          im the header
+          <nav>
+            <a>link1</a>
+            <a>link2</a>
+            <a>link3</a>
+          </nav>
+        </header>
+        <div className='body'>
+          <body>
+            im the body
+          </body>
+          <div className='footer'>
+            <footer>
+              im the footer
+            </footer>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
