63d24f59e345a010e1eee2f234302653e58c28b3
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-07-17 17:01:44 -0500

    a work in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Posts from './components/Posts'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Posts />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
