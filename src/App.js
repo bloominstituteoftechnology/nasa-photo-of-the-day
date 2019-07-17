b11d1691d1437ddfed6dfc4ea5355c2c7dd27ba8
dangolbeeker <scottb305@outlook.com>
2019-07-17 16:57:54 -0400

    Imported useState and header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Header from "./Components/Header";
+
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+     
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+
+
+
+
+
+function Header() {
+    return (
+      <>
+        
+      </>
+    );
+  }
+  
+  export default Header;
+
+
+
+
