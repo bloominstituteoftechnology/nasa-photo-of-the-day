d3a7860ac59cb302edec1c03b4ff1545cbe646ce
dangolbeeker <scottb305@outlook.com>
2019-07-17 17:22:03 -0400

    imported axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 import Header from "./Components/Header";
 
 
 function App() {
+  const fetchData = () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      console.log(res.data);
+     
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  useEffect(fetchData, []);
+
+  console.log(fetchData);
   return (
     <div className="App">
      
