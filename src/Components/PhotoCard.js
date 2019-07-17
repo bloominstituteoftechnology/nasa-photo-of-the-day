aceba7151a504642e62658f7b68322657d827a5c
Brandon Allison <brandona8128@outlook.com>
2019-07-17 00:38:03 -0400

    almost

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,7 +1,16 @@
 import React, {useState, useEffect} from 'react'
 
 
-const PhotoCard = () => {
+
+const PhotoCard = (props) => {
+
+    return(
+        <div>
+            <h2>{props.title}</h2>
+            <img src={props.image}/>
+            <p>{props.exp}</p>
+        </div>
+    )
 
 
 }
