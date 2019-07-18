006bdfbbe38c2593fa0f491249210009f2eb8a8b
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-18 10:05:02 -0500

    added alt to img

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -4,7 +4,7 @@ export default function ImgCard({imgURL,imgTitle,explanation}) {
     return  (
         <div className = "img-card">
             <h2>{imgTitle}</h2>
-            <img src = {imgURL} style ={{maxWidth: "900px"}} />
+            <img src = {imgURL} style ={{maxWidth: "900px"}} alt = "nasa image of day" />
             <p>{explanation}</p>
             <br></br>
         </div>
