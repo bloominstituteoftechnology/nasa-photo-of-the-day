096172a186984303834006abcfc71e6911d84665
Kelly M <moreira.kd@outlook.com>
2019-07-18 19:04:45 -0400

    for air table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./App.css";
+import PhotoCard from "./components/PhotoCard";
 
 function App() {
+
+  const [data, setData] = useState({});
+  
+
+  axios.get("https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY")
+    
+  .then(res => {
+      console.log(res.data);
+      setData(res.data);
+      
+    });
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <PhotoCard title={data.title} 
+                  url={data.url}
+                  explanation={data.explanation} />
     </div>
   );
 }
