817a21a475c6a6c84136d0fcac97bf3a423cf929
Megan White <meganwhite@Megans-MacBook-Pro.local>
2019-07-18 10:07:08 -0500

    added additional styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header ({date}) {
     return (
         <div className = "temp-header">
-            <h1>NASA Photo of the Day</h1>
+            <h1>🚀 NASA Photo of the Day 🚀</h1>
             <h3>{date}</h3>
         </div>
     )
