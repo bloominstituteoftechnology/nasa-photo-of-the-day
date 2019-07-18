3fa215c78b73f174681596449af502cb9582f020
Greg Johnston <GCJohnston91@gmail.com>
2019-07-18 12:55:29 -0400

    init

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function ImageCard( {imgURL, imgTitle, imgInfo, imgDate} ) {
 
+
   return (
     <div className="imageCard">
     <p className="imgTitle"> {imgTitle} </p>
     <p className="imgDate"> {imgDate} </p>
-    <img className="mainImage" src={imgURL} alt="red sun" style={{ maxWidth: "360px" }} />
+    <img className="mainImage" src={imgURL} alt="red sun" style={{ maxWidth: "80%" }} />
     <p className="imgInfo"> {imgInfo} </p>
     </div>
   );
