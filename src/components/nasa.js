b19d74acc8c2221c80115046cf53cf8235441e0e
dannyvidal <vidaldanny94@gmail.com>
2019-07-18 16:48:59 -0400

    added styles

--- src/components/nasa.js
+++ src/components/nasa.js
@@ -2,20 +2,22 @@ import React from "react";
 
 
 
-function Video(props){
-    return(
+function Nasa(props) {
+    return (
         <>
-        {(props.media_type ==="video") ?  
-        <iframe width="420" height="315" src={props.src}></iframe>
-        :
-        <img src={props.hdurl} width="420" height="315"></img>}
-           
-         
-          
-            <p className="date">{props.date}</p>
-            <p className="explanation">{props.explanation}</p>
+            <div className="card NASA">
+                {(props.media_type === "video") ?
+                    <iframe className="card-img-top" width="420" height="315" src={props.src}></iframe>
+                    :
+                    <img className="card-img-top" src={props.hdurl} width="420" height="315"></img>}
+
+
+                <h1 className="card-title TITLE">{props.title}</h1>
+                <p className="DATE">{props.date}</p>
+                <p className="EXPLANATION card-text">{props.explanation}</p>
+            </div>
         </>
     )
 }
 
-export default Video;
+export default Nasa;
