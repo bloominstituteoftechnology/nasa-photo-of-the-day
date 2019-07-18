8e6fd2706ceccbac9b623db925b2edb5d23db4f0
Marcus Jones <indelibleink84@gmail.com>
2019-07-18 17:43:03 -0500

    updtd: yarn create react-app nasa, yarn add semantic-ui-react, added stylesheet

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
 
 import React, {useEffect, useState} from 'react'
 import Posts from './components/Posts.js'
+// import Neo from './components/Neo.js'
 import axios from 'axios'
-import './App.css';
+
+import './App.css'
+// {import '.Neo.css';}
 
 
 function App() {
     const [data, setData] = useState([])
-
     const fetch = () => {
-      axios.get('https://api.nasa.gov/planetary/apod?api_key=HL7Cmszg6ukeCduFdzUnboaD2ummKzuYzfYIyQRL')
+    //{ axios.all[}
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=HL7Cmszg6ukeCduFdzUnboaD2ummKzuYzfYIyQRL')
+      // {axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY')
+      // ]}
       .then(response => {
         setData(response.data)
       })
-
       .catch (error => {console.log("There was an error, please try your request at a later time")}
     )
   }
 useEffect(fetch, [])
-  console.log(data)
+console.log(data)
 
   return (
+//     <div>
+//       <Form>
+//         <Form.Group widths='equal'>
+//           <Form.Input fluid label='First name' placeholder='First name' />
+//           <Form.Input fluid label='Last name' placeholder='Last name' />
+//           <Form.Select fluid label='Gender' options={options} placeholder='Gender' />
+//         </Form.Group>
+//         <Form.Group inline>
+//           <label>Size</label>
+//           <Form.Radio
+//             label='Small'
+//             value='sm'
+//             checked={value === 'sm'}
+//             onChange={this.handleChange}
+//           />
+//           <Form.Radio
+//             label='Medium'
+//             value='md'
+//             checked={value === 'md'}
+//             onChange={this.handleChange}
+//           />
+//           <Form.Radio
+//             label='Large'
+//             value='lg'
+//             checked={value === 'lg'}
+//             onChange={this.handleChange}
+//           />
+//         </Form.Group>
+//         <Form.TextArea label='About' placeholder='Tell us more about you...' />
+//         <Form.Checkbox label='I agree to the Terms and Conditions' />
+//         <Form.Button>Submit</Form.Button>
+//       </Form>
+//     )
+//   }
+
+// export default FormExampleSubcomponentControl
+
+//     </div>
+
+
     <div className="App">
       <Posts date={data.date} nasaVid = {data.url} title = {data.title} paragraph = {data.explanation}  />
+      {/* <Neo /> */}
     </div>
     )
 } 
-
 export default App;
 
 // function App() {
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Posts.css'
+import './Posts'
 
 export default function Posts(props) {
 
@@ -7,24 +7,10 @@ export default function Posts(props) {
         <div className='Posting'>
             <h1> {props.title} </h1>
             <h5>{props.date}</h5>
-          <img className= "picture" title='Nasa Video' src = {props.nasaVid} alt= "Red Moon" /> 
+          <img className= "picture"  src = {props.nasaVid} alt= "Red Moon" /> 
             <p> {props.paragraph} </p>
+            <h5 >{'Watch the Video below for more informatin'}</h5>
+            <iframe title='Nasa Video' width="495" height="385" src="https://www.youtube.com/embed/xc1SzgGhMKc" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen> </iframe>
         </div>
     )
 }
-
-// import React from 'react'
-// import './Posts.css'
-
-// export default function Posts(props){
-    
-//     return (
-//         <div className= "Posting">
-//            <h1> {props.title} </h1>
-//            <iframe title="Nasa Video" src = {props.nasaVid} > from body </iframe>
-//         </div>
-//     )
-// }
-
-
-
