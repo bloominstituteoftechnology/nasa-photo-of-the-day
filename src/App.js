6d38cc686ccd934c426042109aa692f1f7aa0bae
johnschneider1 <johnlawschneider@gmail.com>
2019-07-19 08:39:13 -0500

    working on styles

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React from "react";
 import "./App.css";
 import MediaGrid from "./components/MediaGrid";
 import { WrapperDiv, NewH1, NewH3, Button } from "./components/styleMe";
-import {} from "semantic-ui-react";
+import { Header, Segment } from "semantic-ui-react";
 
 function App() {
   return (
-    <WrapperDiv>
-      <NewH1>
-        "Your Mind is like a parachute: If it isn't open, it doesn't work."
-      </NewH1>
-      <NewH3>Buzz Aldrin on Keeping an open mind</NewH3>
-      <Button>Take me to The Moon</Button>
+    <>
+      <Header attached as="h2yar" color="purple">
+        Attached Header
+      </Header>
+      <Segment attached>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </Segment>
 
-      <section>
-        <MediaGrid />
-      </section>
-    </WrapperDiv>
+      <WrapperDiv>
+        <NewH1>
+          "Your Mind is like a parachute: If it isn't open, it doesn't work."
+        </NewH1>
+        <NewH3>Buzz Aldrin on Keeping an open mind</NewH3>
+        <Button>Take me to The Moon</Button>
+
+        <section>
+          <MediaGrid />
+        </section>
+      </WrapperDiv>
+    </>
   );
 }
 
