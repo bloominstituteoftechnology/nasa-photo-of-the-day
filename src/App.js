b3059e523b06f3e29d68b887a62e83342ba7052f
Ricardo Barbosa <ric3639@gmail.com>
2019-07-30 21:26:01 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+import axios from 'axios';
+import Date from './date';
+import Explanation from './explanation';
+import Media from './media';
+import Title from './title';
+
+function App() {
+  const [spaceball, setSpaceball] = useState();
+
+  useEffect ( () => { 
+    axios 
+      .get("https://api.nasa.gov/planetary/apod?api_key=imNdceQO22NiaLCcNMCMhNDvcCa1ZdPFkTZBhd9h")
+      .then(result => {
+        setSpaceball(result.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [] );
+
+  let date = null;
+  let explanation = null;
+  let url = null;
+  let title = null;
+  
+  if(spaceball) {
+    date = spaceball.date;
+    explanation = spaceball.explanation;
+    url = spaceball.url;
+    title = spaceball.title;
+  }
+  return (
+  <div className = "main">
+    <div className = "header">
+    <h1>Nasa Picture of the Day</h1>
+      <Title title = {title} />
+      <Date date = {date} />
+      <Media url = {url} />
+      <Explanation explanation = {explanation} />
+    </div>
+  </div>
+  )
+}
+
+export default App;
