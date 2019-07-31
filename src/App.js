522012b49df95e459f49eb3471ba82a0ced1d27c
Priya Vaidya <pvaidya56@gmail.com>
2019-07-30 19:41:47 -0700

    Created file structure, built header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className="App, App-header">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <div>
+           <h1>Astronomy Picture of the Day</h1> 
+        </div>
+    );
+};
