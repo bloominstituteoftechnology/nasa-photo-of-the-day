0738900369d8fb2499ec477020cf32b5c62b0db4
gaetanodaidone <gaetanodaidone@gmail.com>
2019-07-31 23:33:45 -0400

    added copyright and explanation state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className="header-container">
         <nav className="nav-container">
-          <button className="buttons" href="https://nasa.gov">Home</button>
+ 
         </nav>
         <div className="title">
           <h1>Photo of the Day by NASA</h1>
--- src/components/NasaPhotos.js
+++ src/components/NasaPhotos.js
@@ -3,22 +3,28 @@ import Photo from './Photo';
 import axios from 'axios'
 
 export default function NasaPhotos(props) {
-  const [pictures, setPicture] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [pictures, setPicture] = useState([])  //setting state
+  const [isLoading, setIsLoading] = useState(false)  //setting state to false
+  const [copyright, setCopyright] = useState([]) //setting state for the copyright
+  const [explanation, setExplanation] = useState([]) //setting state for explanation by the copyrighter
 
-  useEffect(date => {
+  useEffect(date => { // hits an api
     setIsLoading(true)
     axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=KXLFCSFcc0A2LkvgNlG7acLVKNKmonK7SonHhvQL')
+      .get('https://api.nasa.gov/planetary/apod?api_key=KXLFCSFcc0A2LkvgNlG7acLVKNKmonK7SonHhvQL')  // gets the api
       .then(res => {
-        console.log(res)
+        console.log(res) //logs res
         setPicture(res.data.url)
-        setIsLoading(false)
+        setIsLoading(false) //loading is set to false because its been loaded
+        setCopyright(res.data.copyright)
+        setExplanation(res.data.explanation)
       })
-  }, [pictures])
+  }, [pictures])  //if pictures updates, useeffect will run again
   return (
     <div>
       {isLoading && <h1>Photos from Nasa are loading</h1>}
+        <h1>{copyright}</h1>
+        <p>{explanation}</p>
       <Photo img={pictures}/>
     </div>
   )
