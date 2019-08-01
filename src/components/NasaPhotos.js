029ad7a2ba3594720b2aa0ce6c9f0ff188e88328
gaetanodaidone <gaetanodaidone@gmail.com>
2019-07-31 23:46:16 -0400

    added comments

--- src/components/NasaPhotos.js
+++ src/components/NasaPhotos.js
@@ -9,21 +9,21 @@ export default function NasaPhotos(props) {
   const [explanation, setExplanation] = useState([]) //setting state for explanation by the copyrighter
 
   useEffect(date => { // hits an api
-    setIsLoading(true)
+    setIsLoading(true) //sets boolean to true for photo is loading
     axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=KXLFCSFcc0A2LkvgNlG7acLVKNKmonK7SonHhvQL')  // gets the api
-      .then(res => {
-        console.log(res) //logs res
-        setPicture(res.data.url)
+      .get('https://api.nasa.gov/planetary/apod?api_key=KXLFCSFcc0A2LkvgNlG7acLVKNKmonK7SonHhvQL')  // sends a GET request for the API.
+      .then(nasa => {              // changed res to nasa,why do we use res? is it just standard? ... If the request has been completed successfully the function is called with is passed into the call of the then method
+        console.log(nasa) //logs res
+        setPicture(nasa.data.url)
         setIsLoading(false) //loading is set to false because its been loaded
-        setCopyright(res.data.copyright)
-        setExplanation(res.data.explanation)
+        setCopyright(nasa.data.copyright)
+        setExplanation(nasa.data.explanation)
       })
   }, [pictures])  //if pictures updates, useeffect will run again
   return (
     <div>
       {isLoading && <h1>Photos from Nasa are loading</h1>}
-        <h1>{copyright}</h1>
+        <h1>{copyright}</h1> 
         <p>{explanation}</p>
       <Photo img={pictures}/>
     </div>
