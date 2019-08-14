9883f6f4ad6f9df88484d9f752b78783ca73a083
Kelly M <moreira.kd@outlook.com>
2019-08-14 18:19:32 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
 import PhotoCard from "./components/PhotoCard";
@@ -6,21 +6,25 @@ import PhotoCard from "./components/PhotoCard";
 function App() {
 
   const [data, setData] = useState({});
-  
-
-  axios.get("https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY")
+  useEffect(() => { 
+    
+    console.log("first render");
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=FV9XPDuFMNnxtlmvZKdmrjRKUNydPKsFj5pHNhVk")
     
   .then(res => {
       console.log(res.data);
       setData(res.data);
       
     });
+
+  }, []);
+  
+
+  
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <header> <h1>Nasa Photo Project</h1></header>
       <PhotoCard title={data.title} 
                   url={data.url}
                   explanation={data.explanation} />
