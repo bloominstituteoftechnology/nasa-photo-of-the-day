850569eff5aaeaadda16d59672b6e482ae3f05f2
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-14 13:05:05 -0400

    all dependencies added, axios, moment, material ui core, material ui pickers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import "./App.css";
+import DatePicker from "./components/DatePicker";
+import "./scss/App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <DatePicker />
     </div>
   );
 }
---
+++ src/components/DatePicker.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DatePicker = () => {
+  return (
+    <div>
+      <h1>Pick a Date</h1>
+    </div>
+  );
+};
+
+export default DatePicker;
