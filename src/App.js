c586cf534ed0c62a87a44197a3f40b2ba0399254
aaamg <andrewmanuelgarcia@gmail.com>
2019-08-14 21:06:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
+
+
+function App(props) {
+  const [pic, setPic] = useState();
+  //const [date, setDate] = useState();
+  //const [title, setTitle] = useState();
+
+  //axios function
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=RlVu0xWKQ2LGh5zKsgeSnpciQJvSxmA4e0OxOBqc')
+      .then (response => {
+        //console.log(response) 
+        const imageNasa = response.data.hdurl;
+        //const dateNasa = response.data.date;
+        //const titleNasa = response.data.title;
+        console.log("Nasa image", imageNasa)
+        setPic(imageNasa);
+        })
+    }, [] )
+
+    //normal JSX part
   return (
     <div className="App">
+      <div className="photoOfDay">
+        {imageNasa.map(pic => {
+          return(
+            {pic}
+          );
+        })}
+      </div>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        The photo above shows NASA's Photo of the day pulled from their API! Enjoy!
       </p>
     </div>
   );
 }
 
+//exporting data
 export default App;
