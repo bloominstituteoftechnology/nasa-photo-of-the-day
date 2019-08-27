e074705526169e4cd9fa425c7210a87bd37c451b
Greg Renfro <remph819@gmail.com>
2019-08-26 21:29:01 -0700

    trying out es7 syntax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>Read through the instructions in the README.md file to build your NASA app! Have fun 🚀!</p>
+		</div>
+	);
 }
 
 export default App;
