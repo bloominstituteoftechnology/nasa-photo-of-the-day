f22ec5444f6df04268652f22f290aba9f4506b94
spettigrew <sara.pettigrew5@gmail.com>
2019-08-27 19:56:32 -0600

    Stretch date added to app.js - pretty cool!

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import "./App.css";
 
 function App() {  
   const[data, setData] = useState({});
+  const[date, setDate] = useState('2019-08-26');
   useEffect(() => {
     console.log('first render');
   
-  axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
   .then(response => {
     console.log(response.data);
     setData(response.data)
       })
-  }, []);
+  }, [date]);
+
     // Use useEffect when we want to run some code, sometimes (conditionally). 
     // So the code won't run every time and we run out of our 'api' time.(Running an API is a 'side effect', modify some variable.) UseEffect takes a cb function as it's first argument (similar to setTimeout) and takes code to delay the execution on and then it takes as it's second argument, an array of data. Whenever the data changes, useEffect will run the cb function, when we want it to happen. An empty array [], the data will never change. The second time the function is ran, useEffect will see that nothing in the array changed. (The variables in the cb, that it depends on, may or may not change.) Inside of the array, it should include all the data we depend on. UseEffect helps us contain our communication with the server.
-
+  console.log(date);
   return (
     <div className="App">
       <p>
@@ -28,7 +30,13 @@ function App() {
       <PhotoCard title={data.title}
        url={data.url} 
        explanation={data.explanation}
-       date={data.date} />
+       date={data.date}
+        />
+        <form>
+        <input onChange={(event) => 
+          setDate(event.target.value)} type={'date'}>
+          </input>
+        </form>
     </div>
   );
 }
@@ -43,3 +51,13 @@ export default App;
 // url = { data.url }
 // explanation = { data.explanation }
 // date = { data.date } />)}
+
+// Ternary operator
+// <p>
+// {data
+// ?
+// <PhotoCard title={data.title}
+  // url={data.url}
+  // explanation={data.explanation}
+  // date={data.date} />
+// }
