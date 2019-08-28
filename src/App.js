23a9c297994e0d6552b6128fe6697d21fd07b34e
Andrew Nottoli <nottolivc@gmail.com>
2019-08-27 19:13:29 -0700

    added map comments for function to map over api

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import axios from 'axios';
 import PhotoCard from '../src/components/PhotoCard.js';
 
 function App() {
-  const [data, setData] = useState({});
+const [data, setData] = useState({});
+
   {/*
     const [title, setTitle] = useState('');
     const [url, setUrl] = useState('');
     const [explanation, setExplanation] = useState('');
     
       original methods converted */}
-  
-  useEffect(() => { 
+{/* const [photos, setPhotos] = useState([]); */}
+useEffect(() => { 
   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(response => {
       console.log(response.data);
       {/*setTitle(response.data.title);
       setUrl(response.data.url);
-      setExplanation(response.data.explanation); SONCERTING TO setDATA for DRY code */}
+      setExplanation(response.data.explanation); SONCERTING TO setDATA for DRY code 
+        set to one response
+      setPhotos(response.data) MAP*/}
       setData(response.data);
   });
 }, []);
@@ -29,7 +32,10 @@ function App() {
         This is the NASA Photo of the Day Page...
       </p>
       {/*<PhotoCard title={title} url={url} explanation={explanation} /> single function and single return w useEffect above! */}
-      <PhotoCard title={data.title} url={data.url} explanation={data.explanation} />
+      <PhotoCard title={data.title} url={data.url} explanation={data.explanation} date={data.date} /> 
+      {/* photos && photos.map(data => (
+      <PhotoCard title={data.title} url={data.url} explanation={data.explanation} date={data.date} /> */}
+      ))}
     </div>
   );
 }
