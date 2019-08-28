2fb9c5c6a76d7dbd39d63fef985b9d8e3377db98
Andrew Nottoli <nottolivc@gmail.com>
2019-08-27 18:09:32 -0700

    achieved api call w axios for object data image, title, explanation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from 'axios';
+import PhotoCard from '../src/components/PhotoCard.js';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [explanation, setExplanation] = useState('');
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(response => {
+      console.log(response.data);
+      setTitle(response.data.title);
+      setUrl(response.data.url);
+      setExplanation(response.data.explanation);
+    });
+
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        This is the NASA Photo of the Day Page...
       </p>
+      <PhotoCard title={title} url={url} explanation={explanation} />
     </div>
   );
 }
