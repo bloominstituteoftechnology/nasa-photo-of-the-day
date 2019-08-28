567204620f78aa980550b6adb7c635de3042b51e
Andrew Nottoli <nottolivc@gmail.com>
2019-08-27 18:26:20 -0700

    basic mvp api call with useEffect for api limitations on get

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import PhotoCard from '../src/components/PhotoCard.js';
 
 function App() {
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
-  const [explanation, setExplanation] = useState('');
+  const [data, setData] = useState({});
+  {/*
+    const [title, setTitle] = useState('');
+    const [url, setUrl] = useState('');
+    const [explanation, setExplanation] = useState('');
+    
+      original methods converted */}
+  
+  useEffect(() => { 
   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(response => {
       console.log(response.data);
-      setTitle(response.data.title);
+      {/*setTitle(response.data.title);
       setUrl(response.data.url);
-      setExplanation(response.data.explanation);
-    });
-
+      setExplanation(response.data.explanation); SONCERTING TO setDATA for DRY code */}
+      setData(response.data);
+  });
+}, []);
 
   return (
     <div className="App">
       <p>
         This is the NASA Photo of the Day Page...
       </p>
-      <PhotoCard title={title} url={url} explanation={explanation} />
+      {/*<PhotoCard title={title} url={url} explanation={explanation} /> single function and single return w useEffect above! */}
+      <PhotoCard title={data.title} url={data.url} explanation={data.explanation} />
     </div>
   );
 }
