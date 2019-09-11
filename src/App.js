7cbe1f4a5d1a1161d65379f33e73b52134686c0a
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2019-09-11 14:06:38 +0100

    Created the necessary components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from './Header';
+import Image from './Image';
+import ImageDescription from './ImageDescription';
+import Footer from './Footer';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      
     </div>
   );
 }
