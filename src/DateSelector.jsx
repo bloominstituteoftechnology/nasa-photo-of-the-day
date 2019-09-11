0551ddf8f5b8c793da67e20ffdb1bd7dd5934320
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-11 15:25:02 +0100

    Add default date to date selector

--- src/DateSelector.jsx
+++ src/DateSelector.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
 function DateSelector (props) {
+
+    const today = new Date();
+
+    const dd = ('0' + today.getDate()).slice(-2);
+    const mm = ('0' + today.getMonth()).slice(-2);
+    const yyyy = today.getFullYear();
+
+    const dateToday = yyyy + '-' + mm + '-' + dd;
+
     return (
         <div className='date-selector'>
             <h3>Get an image from a different date!</h3>
-            <input type="date"/>
+            <input type="date" defaultValue={dateToday} />
             <button>Go!</button>
         </div>
     )
