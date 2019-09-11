7761266b071b3ff90a718f73823d62a940115267
abarne <celius377@gmail.com>
2019-09-11 14:12:09 -0500

    picture and details displayed on screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import ImageList from './components/ImageList';
+import ImageCard from './components/ImageCard';
 
 function App() {
-	useEffect(() => {
-		axios
-			.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-			.then((response) => {
-				console.log(response);
-			})
-			.catch((error) => {
-				console.log('the data was not displayed', error);
-			});
-	}, []);
 	return (
 		<div className="App">
-			<p>Read through the instructions in the README.md file to build your NASA app! Have fun 🚀!</p>
-			<ImageList />
+			<h1>NASA's Picture of the Day!</h1>
+			<span>{Date()}</span>
+			<ImageCard />
 		</div>
 	);
 }
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ImageCard = (props) => {
-	return <h2>ImageCard Return</h2>;
+const ImageCard = () => {
+	const [ image, setImage ] = useState([]);
+	useEffect(() => {
+		axios
+			.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+			.then((response) => {
+				const imageInfo = response.data;
+				console.log(imageInfo);
+				setImage(imageInfo);
+			})
+			.catch((error) => {
+				console.log('the data was not displayed', error);
+			});
+	}, []);
+
+	return (
+		<div className="container">
+			<img className="image" src={image.url} />
+			<div className="image-info">
+				<h2>Title: {image.title}</h2>
+				<h3>Credit: {image.copyright}</h3>
+				<p>About this image: {image.explanation}</p>
+			</div>
+		</div>
+	);
 };
 
 export default ImageCard;
