fddecae94a96dc647e9097f4d1d27b1c962602b8
FearTheDev <w.m.t.b5115@gmail.com>
2019-09-11 18:26:43 -0400

    Updated Styling

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
 
   const API_KEY = "DEMO_KEY"; 
 
-  useEffect( () =>{
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
-      .then(response => {
-        setApod(apod => response.data);
-        console.log(apod);
-      })
-        .catch(error => console.log(`Error: ${error}`));
-  }, []);
+  // useEffect( () =>{
+  //   axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+  //     .then(response => {
+  //       setApod(apod => response.data);
+  //       console.log(apod);
+  //     })
+  //       .catch(error => console.log(`Error: ${error}`));
+  // }, []);
 
   return (
     <div className="App">
-      <Apod title={apod.title} date={apod.date} description={apod.explanation} url={apod.url}  copyright={apod.copyright}/>
+      <Apod title={"IC 1805: The Heart Nebula"} date={"2019-09-11"} description={"What energizes the Heart Nebula? First, the large emission nebula dubbed IC 1805 looks, in whole, like a human heart. The nebula glows brightly in red light emitted by its most prominent element: hydrogen. The red glow and the larger shape are all powered by a small group of stars near the nebula's center. In the center of the Heart Nebula are young stars from the open star cluster Melotte 15 that are eroding away several picturesque dust pillars with their energetic light and winds. The open cluster of stars contains a few bright stars nearly 50 times the mass of our Sun, many dim stars only a fraction of the mass of our Sun, and an absent microquasar that was expelled millions of years ago. The Heart Nebula is located about 7,500 light years away toward the constellation of Cassiopeia. Coincidentally, a small meteor was captured in the foreground during imaging and is visible above the dust pillars. At the top right is the companion Fishhead Nebula."} url={"https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg"} copyright={"Bray Falls"} />
     </div>
   );
 }
--- src/components/Apod.jsx
+++ src/components/Apod.jsx
@@ -5,21 +5,43 @@ const Apod = props =>{
     const {title, date, description, url, copyright} = props;
     console.log(props);
 
+
+    if(!props.url){
+        return (
+            <div className="wrapper">
+                <div className="display-header">
+                    <h1>NASA Astronomy Picture of the Day</h1> <div><h1>Pick a Date: </h1><select><option>{date}</option></select></div>
+                </div>
+                <div className="display-body">
+    
+                    <h1>Loading..</h1>
+    
+                </div>
+            </div>
+        );
+    }else{
+
+    
+
+
     return (
         <div className="wrapper">
             <div className="display-header">
-                <h1>NASA Astronomy Picture of the Day {date}</h1>
-                <h2>{title}</h2>
+                <h1>NASA Astronomy Picture of the Day</h1> <div><h1>Pick a Date: </h1><select><option>{date}</option></select></div>
             </div>
             <div className="display-body">
+
                 <img src={url} alt={title}/>
-            </div>
-            <div className="display-description">
-                <h3>Picture provided by {copyright}</h3>
-                <p>{description}</p>
+
+                <div className="display-description">
+                    <h3>{title} picture provided by <i>{copyright}</i></h3>
+                    <p>{description}</p>
+                </div>
+
             </div>
         </div>
     );
+}
 };
 
 export default Apod;
