f8b45af2cff66adb634d16f102849029e92e0634
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-11 12:31:39 +0100

    create Figure component

---
+++ src/components/Figure.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Figure = ({ imgUrl }) => (
+  <figure>
+    <img src={imgUrl} alt={""} />
+    <figcaption>{""}</figcaption>
+  </figure>
+);
+
+export default Figure;
