b1bb2013e044972946cecfbad8d9deb65aa54b80
abarne <celius377@gmail.com>
2019-09-11 14:41:42 -0500

    choose date and background added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import ImageCard from './components/ImageCard';
 
 function App() {
+	let dateObj = new Date();
+	let month = dateObj.getUTCMonth() + 1;
+	let day = dateObj.getUTCDate();
+	let year = dateObj.getUTCFullYear();
+
+	let newDate = month + '/' + day + '/' + year;
+
+	const [ date, setDate ] = useState(newDate);
+
+	console.log(newDate);
 	return (
 		<div className="App">
 			<h1>NASA's Picture of the Day!</h1>
-			<span>{Date()}</span>
+			<span>Today is: {newDate}</span>
 			<ImageCard />
 		</div>
 	);
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,19 +2,32 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ImageCard = () => {
+	let dateObj = new Date();
+	let month = dateObj.getUTCMonth() + 1;
+	let day = dateObj.getUTCDate();
+	let year = dateObj.getUTCFullYear();
+
+	let newDate = `${year}-${month}-${day}`;
+	console.log('current day', newDate);
+
 	const [ image, setImage ] = useState([]);
-	useEffect(() => {
-		axios
-			.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-			.then((response) => {
-				const imageInfo = response.data;
-				console.log(imageInfo);
-				setImage(imageInfo);
-			})
-			.catch((error) => {
-				console.log('the data was not displayed', error);
-			});
-	}, []);
+	const [ date, setDate ] = useState(newDate);
+
+	useEffect(
+		() => {
+			axios
+				.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+				.then((response) => {
+					const imageInfo = response.data;
+					console.log(imageInfo);
+					setImage(imageInfo);
+				})
+				.catch((error) => {
+					console.log('the data was not displayed', error);
+				});
+		},
+		[ date ]
+	);
 
 	return (
 		<div className="container">
@@ -23,6 +36,10 @@ const ImageCard = () => {
 				<h2>Title: {image.title}</h2>
 				<h3>Credit: {image.copyright}</h3>
 				<p>About this image: {image.explanation}</p>
+				<label>
+					Choose Date: <input type="text" value={date} onChange={(e) => setDate(e.target.value)} />
+				</label>
+				<input type="submit" value="Submit" />
 			</div>
 		</div>
 	);
