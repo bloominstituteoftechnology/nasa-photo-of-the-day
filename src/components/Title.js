46b552c9e9149ea367d35c6ae4d3db1d106feecf
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-09-11 17:50:32 +0100

    style

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = ({ date, ...otherProps }) => (
   <div {...otherProps}>
-    <h1>NASA Photo of the Day</h1>
+    <h1>Astronomy Picture of the Day</h1>
     <h4>{date}</h4>
   </div>
 );
