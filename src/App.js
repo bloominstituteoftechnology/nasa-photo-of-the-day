1943f410ecda102e562d952ae5cdf1bdb52aeb7d
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2019-09-11 16:23:49 +0100

    Completetion of MVP WEBU3-nasa-photo-of-the-day

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.css";
-import Header from './Header';
-import Image from './Image';
-import ImageDescription from './ImageDescription';
-import Footer from './Footer';
 import axios from 'axios';
 
+
+const nasaData = 'https://lambda-github-api-server.herokuapp.com/';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+  const [nasaInfo, setNasaInfo] = useState();
+
+  // All functions here
+
+  useEffect(() => {
+    axios.get(nasaData)
+    .then(response => {
+      console.log(response);
+      console.log(response.data);
+
+        setNasaInfo(response.data);
+      })
+      .catch(error => {
+        
+      })
+  }, []);
+console.log(nasaInfo);
+
+  if (nasaInfo) {
+    return (
+      <div className="App">
+        <div>
+          <h1>{nasaInfo.title}</h1>
+        </div>
+        <div>
+          <h3>{nasaInfo.date}</h3>
+        </div>
+        <div>
+          <img src={nasaInfo.url}></img>
+        </div>
+        <div>
+          <h2>Description</h2>
+          <p>{nasaInfo.explanation}</p>
+        </div>
+        <footer>
+          <p>{nasaInfo.copyright}</p>
+        </footer>
+      </div>
+    )
+  } else {
+    return null;
+  }
 }
 
 export default App;
