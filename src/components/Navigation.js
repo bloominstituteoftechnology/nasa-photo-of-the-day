ad115516551931535466aa103bce76c2c47393a7
Mark Gowen <mark.gowen78@gmail.com>
2019-09-12 14:35:54 -0700

    completed project

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import Logo from '../assets/nasa-logo-web-rgb.png';
 import Menu from '../assets/dashicons-menu.svg';
-import styled from 'styled-components'
-
+import styled from 'styled-components';
 
 const NavBar = styled.section`
-width: 100%;
-height: 60px;
-background: #f1f1f1;
+  width: 100%;
+  height: 60px;
+  background: #f1f1f1;
 `;
 
 const Nav = styled.div`
-width: 90%;
-max-height: 60px;
-display: flex;
-justify-content: space-between;
-align-items: center;
-margin: auto;
-padding-top: 8px;
+  width: 90%;
+  max-height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+  padding-top: 8px;
 `;
 
 const NavLogo = styled.img`
-width: 80px;
+  width: 80px;
 `;
 
-const NavMenu = styled.img``;
-
+const NavMenu = styled.img`
+  width: 30px;
+`;
 
 export default function Navigation() {
-    return <NavBar>
-        <Nav>
-            <NavLogo alt='NASA Logo' src={Logo}></NavLogo>
-            <NavMenu src={Menu}></NavMenu>
-        </Nav>
+  return (
+    <NavBar>
+      <Nav>
+        <NavLogo alt="NASA Logo" src={Logo}></NavLogo>
+        <NavMenu src={Menu}></NavMenu>
+      </Nav>
     </NavBar>
-    
+  );
 }
--- src/components/PODCard.js
+++ src/components/PODCard.js
@@ -1,16 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.section`
+width: 520px;
+margin-top: 5%;
+margin-bottom: 5%;
+margin-left: 120px;
+`;
+
+const PoDImg = styled.img`
+width: 100%;
+object-fit: scale;
+flex-shrink: 2;
+`;
+
+const ImgDate = styled.p`
+font-family: 'Roboto', sans-serif;
+font-size: 12px;
+text-align: left;
+color: #b8b8b8;
+`;
+
+const ImgTitle = styled.h2`
+font-family: 'Roboto', sans-serif;
+font-weight: bold;
+font-size: 24px;
+text-align: left;
+`;
+
+const ImgExp = styled.p`
+font-family: 'Roboto', sans-serif;
+font-size: 12px;
+line-height: 14px;
+text-align: left;
+`;
+
+const Copy = styled.h3`
+font-family: 'Roboto', sans-serif;
+font-size: 14px;
+text-align: left;
+`;
+
+
 
 const PODCard = props => {
   console.log(props);
   return (
-    <div className="photo-info" key={props.url}>
-      <img className="podImg" alt="NASA Photo of the Day" src={props.url}></img>
-      <h3>Date: {props.date}</h3>
-      <h2>{props.title}</h2>
-      <p>{props.explanation}</p>
-      
-      <h3>Copyright: {props.copyright}</h3>
-    </div>
+    <Card key={props.url}>
+      <PoDImg alt="NASA Photo of the Day" src={props.url}></PoDImg>
+      <ImgDate>Date: {props.date}</ImgDate>
+      <ImgTitle>{props.title}</ImgTitle>
+      <ImgExp>{props.explanation}</ImgExp>
+      <Copy>Copyright: {props.copyright}</Copy>
+    </Card>
   );
 };
 
