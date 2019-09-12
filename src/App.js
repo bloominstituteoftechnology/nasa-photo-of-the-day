e34cefa03b193ff202f2d60f81b7693f93d3b3dc
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2019-09-12 13:03:13 +0100

    Testing out Styled-components on my NASA app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import "./App.css";
 import axios from 'axios';
+import styled from 'styled-components';
 
 
 const nasaData = 'https://lambda-github-api-server.herokuapp.com/';
+
+const WholeApp = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+`
+const StyledTitle = styled.div`
+  width: 100%;
+  background: peru;
+  min-height: 80px;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+
+  h1 {
+    color: #f2f2f2;
+  }
+  p {
+    margin-right: 20px;
+  }
+`
+const StyledImage = styled.div`
+  width: 80%;
+  margin: auto;
+  height: 400px;
+  background: peru;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const StyledDescription = styled.div`
+  width: 80%;
+  margin: auto;
+  height: auto;
+  padding: 10px 30px;
+
+  h2 {
+    text-align: center;
+    color: #636363;
+  }
+  p {
+    text-align: center;
+    font-size: 20px;
+    line-height: 1.5;
+    color: #333
+  }
+`
+const StyledFooter = styled.header`
+  width: 100%;
+  background: peru;
+  min-height: 60px; 
+  margin: 0;
+  padding: 10px 30px;
+
+`
 
 function App() {
 
@@ -23,28 +82,29 @@ function App() {
         
       })
   }, []);
-console.log(nasaInfo);
+// console.log(nasaInfo);
 
   if (nasaInfo) {
     return (
-      <div className="App">
-        <div>
+      <WholeApp>
+        {/* <StyledHeader>
+          <a>{nasaInfo.logo}</a>
+        </StyledHeader> */}
+        <StyledTitle>
           <h1>{nasaInfo.title}</h1>
-        </div>
-        <div>
-          <h3>{nasaInfo.date}</h3>
-        </div>
-        <div>
+          <p>{nasaInfo.date}</p>
+        </StyledTitle>      
+        <StyledImage>
           <img src={nasaInfo.url}></img>
-        </div>
-        <div>
+        </StyledImage>
+        <StyledDescription>
           <h2>Description</h2>
           <p>{nasaInfo.explanation}</p>
-        </div>
-        <footer>
+        </StyledDescription>
+        <StyledFooter>
           <p>{nasaInfo.copyright}</p>
-        </footer>
-      </div>
+        </StyledFooter>
+      </WholeApp>
     )
   } else {
     return null;
