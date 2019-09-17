dd3097ae98a2a35ab86ce2fcbc7c6cfb6039eb5f
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-09-17 14:50:11 -0400

    updates

--- src/components/CardStyles.js
+++ src/components/CardStyles.js
@@ -4,12 +4,14 @@ import styled from "styled-components";
 
 /*card styles: card container, picture, title text, and date */
 
+//photo of the day container
 const CardContainer = styled.div`
     display: flex;    
     flex-direction: column;     
     width: 80%;
     height: auto;
     font-family: sans-serif;  
+    
 
     @media screen and (max-width: 800px) {
         width: 100%;
