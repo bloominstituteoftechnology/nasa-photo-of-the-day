d2dfcbbf3e405e5c32e333780a99890f08115ec0
Juan Aleman <juan.j.aleman@gmail.com>
2019-09-26 00:02:59 -0500

    added components, axios and made api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+
+  const [nasa, setNasa] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .then(res => {
+            const nasa = res.data;
+            console.log(nasa);
+            setNasa(nasa);
+        })
+        .catch(error => {
+          console.log("The data was not returned", error);
+        });
+    },[]);
+
   return (
     <div className="App">
       <p>
---
+++ src/Components/NasaList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const NasaList = () => {
+
+    
+    return (
+        <h1>Hello World!</h1>
+    )
+}
+
+export default NasaList;
+
