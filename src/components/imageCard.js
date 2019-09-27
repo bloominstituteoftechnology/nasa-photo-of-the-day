c5c286cfcd64c2b86b05bf412a46d22c0bd6cee5
anatulea <anatulea14@.gmali.com>
2019-09-26 22:56:23 -0500

    Implemented and used a few styled components

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import { Card,  CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card,  CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap'; 
+import styled from 'styled-components'; 
 
-/*export default function ImageCard(props) {
+const StyledHeading = styled.h1`
+font-size:35px;
+font-weight:bold;
+color:DarkSlateBlue;
+font-family: 'Amiri', serif;
+`;
+
+const StyledP = styled.p`
+font-family: 'Amiri', serif;
+font-size:20px;
+`;
+const StyledDiv = styled.div`
+background:#F0F8FF;
+`;
+
+
+/*export default function ImageCardOLD(props) {
     console.log(ImageCard);
     return (
             <div 
@@ -33,19 +50,27 @@ import { Card,  CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reac
 const ImageCard = (props) => {
   return (
     <div>
-      <Card>
-        <CardBody>
-          <CardTitle>{props.titleOf}</CardTitle>
-          <CardSubtitle>-by {props.author}</CardSubtitle>
-          <br></br>
-          <Button color="secondary" size="sm">{props.dateOf}</Button>
-        </CardBody>
-        <img width="100%" src={props.picture} alt="Card image cap" />
-        <CardBody>
-          <CardText>{props.description}</CardText>
-        </CardBody>
-      </Card>
+        <Card>
+            <StyledDiv>
+                <CardBody>
+                    <StyledHeading>
+                        <CardTitle>{props.titleOf}</CardTitle>
+                    </StyledHeading>
+                        <CardSubtitle>-by {props.author}</CardSubtitle>
+                            <br></br>
+                         <Button color="secondary" size="sm">{props.dateOf}</Button>
+                </CardBody>
+                    <img width="100%" src={props.picture} alt="Card image cap" />
+                <CardBody>
+                    <StyledP>
+                        <h2>Description of the image</h2>
+                        <CardText>{props.description}</CardText>
+                    </StyledP>
+                </CardBody>
+             </StyledDiv>
+        </Card>
     </div>
+
   );
 };
 
