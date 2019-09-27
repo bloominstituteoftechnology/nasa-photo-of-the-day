61c666626d5424068aa5c7ef60d948c02d949fbb
Juan Aleman <juan.j.aleman@gmail.com>
2019-09-26 22:29:12 -0500

    MVP met

--- src/App.js
+++ src/App.js
@@ -4,36 +4,31 @@ import "./App.css";
 import NasaCard from "./Components/NasaCard";
 
 function App() {
-
   const [nasa, setNasa] = useState([]);
 
-    useEffect(() => {
-        axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=fRhzD5Fq2Qcsf7R8H3XFGTiHPtp322SiK7HAVWl1`)
-        .then(res => {
-            const otherNasa = res.data
-            console.log(otherNasa);
-            setNasa(otherNasa);
-        })
-        .catch(error => {
-          console.log("The data was not returned", error);
-        });
-    },[]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=fRhzD5Fq2Qcsf7R8H3XFGTiHPtp322SiK7HAVWl1`
+      )
+      .then(res => {
+        const otherNasa = res.data;
+        console.log(otherNasa);
+        setNasa(otherNasa);
+      })
+      .catch(error => {
+        console.log("The data was not returned", error);
+      });
+  }, []);
 
   return (
     <div>
-      <NasaCard title={nasa.title} date={nasa.date} explanation={nasa.explanation}/>
-      <img src={nasa.url}/>
-      {/* {nasa.map(display => {
-        return(
-          <NasaCard 
-          key={display.id} 
-          title={display.title}
-          date={display.date}
-          explanation={display.explanation}
-          />
-        );
-      })} */}
+      <NasaCard
+        title={nasa.title}
+        date={nasa.date}
+        explanation={nasa.explanation}
+      />
+      <img src={nasa.url} />
     </div>
   );
 }
