3778a223663ffb0e21ff4b53af40bed40e2ebfb1
craig-paiva <craiglpaiva@gmail.com>
2019-09-28 21:33:08 -0400

    NASA APOD MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+  const [nasaImg, setImg] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const nasaData = await axios.get('https://api.nasa.gov/planetary/apod?api_key=LNPoBywgSDLGE27u4IKuzb2ocXxz3peGPbtVxz44')
+        console.log(nasaData);
+        setImg(nasaData.data);
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+    fetchData()
+  }, [])
+  console.log(nasaImg);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>The NASA picture of the day 🚀!</h1>
+      <h2>{nasaImg.title}</h2>
+      <h2>{nasaImg.date}</h2>
+      <p>{nasaImg.explanation}</p>
+      <img src={`${nasaImg.url}`} />
     </div>
   );
 }
