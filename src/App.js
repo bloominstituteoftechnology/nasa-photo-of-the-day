b8101397fc867af026feccd9e8fe7deed8151791
Katrina_Dierking <kalynndier@gmail.com>
2019-09-30 00:10:29 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ImgCard from "./components/ImgCard.js";
 import "./App.css";
+import styled from "styled-components";
 
+const TitleH1 = styled.h1`
+  font-size: 65px;
+  color: white;
+  font-family: The Wild Hammers;
+  background-color: black;
+  padding: 3%;
+  margin-top: 1%;
+  border: 1.5px solid white;
+  box-shadow: 5px 5px 5px slategray;
+  `
+
+  const BorderPara = styled.p`
+    font-size: 12px;
+    padding:2%;
+    background-color: black;
+    color: white;
+    align-text: center;
+    border: 1.5px solid white;
+    box-shadow: 5px 5px 5px 0px slategray;
+    `;
 
 function App() {
 return (
   <div className = "App">
-    <h1>Nasa Image of the Day</h1>
+    <TitleH1>Nasa Image of the Day</TitleH1>
    <ImgCard />
-    </div>	   
+   <div className = "border">
+     <BorderPara>Copyright Nasa 2019 | All Rights Reserved</BorderPara>
+   </div>
+    </div>	 
+      
   
 );
 }
--- src/components/ImgInfo.js
+++ src/components/ImgInfo.js
@@ -1,15 +1,52 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
+const WrapperDiv = styled.div`
+    font-family: The Wild Hammer;
+    text-align: center;
+`;
+
+
+const InfoImg = styled.img`
+    width: 50%;
+    height: 50%;
+    padding:2%;
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 5%;
+    margin-bottom: 2%;
+    box-shadow: 5px 5px 5px slategray;
+    `;
+
+    const NewH1 = styled.h1`
+    font-size: 30px;
+    margin-top:2%;
+    `;
+
+    const InfoPara = styled.p`
+    font-size: 16px;
+    width: 75%;
+    text-align: center;
+    margin-left:12%;
+    padding: 3%;
+    border: .5px dotted slategray;
+    `;
+
+    const NewH3 = styled.h3`
+    font-family: The Wild Hammers;
+    font-weight: bold;
+    `
 
 const ImgInfo = props => {
     return (
+        <WrapperDiv>
         <div>
-            <h1>{props.title}</h1>
-            <h3>{props.day}</h3>
-            <img src={props.image} alt="Space"/>
-            <p>{props.text}</p>
+            <NewH1>{props.title}</NewH1>
+            <NewH3>{props.day}</NewH3>
+            <InfoImg src={props.image} alt="Space"/>
+            <InfoPara>{props.text}</InfoPara>
         </div>
+        </WrapperDiv>
     )
 }
 
