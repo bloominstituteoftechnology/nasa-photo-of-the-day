f966c815a783b18fe3f36f8c74d4be70b7998688
itsericfig <itsericfig@gmail.com>
2019-10-09 16:51:15 -0500

    Initial commit, setup App.js with basic structure and content

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Astronomy Picture of the Day</h1>
+            <p>
+                <a href="https://apod.nasa.gov/apod/archivepix.html" target="_blank" rel="noopener noreferrer">
+                    Discover the cosmos!
+                </a>
+                Each day a different image or photograph of our fascinating universe is featured, along with a brief
+                explanation written by a professional astronomer.
+            </p>
+
+            {/* COMPONENT */}
+        </div>
+    );
 }
 
 export default App;
