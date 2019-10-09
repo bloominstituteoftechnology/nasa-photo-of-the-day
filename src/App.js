c599c162bacb37097b23c9eb2305cb9be3b113d5
itsericfig <itsericfig@gmail.com>
2019-10-09 17:35:04 -0500

    Made api request with axios, created Photo component in Photo.js file, imported component to App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
         <div className="container">
             <h1>Astronomy Picture of the Day</h1>
             <p>
-                <a href="https://apod.nasa.gov/apod/archivepix.html" target="_blank" rel="noopener noreferrer">
-                    Discover the cosmos!
-                </a>
+                <a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a>
                 Each day a different image or photograph of our fascinating universe is featured, along with a brief
                 explanation written by a professional astronomer.
             </p>
--- src/Photo.js
+++ src/Photo.js
@@ -1,3 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "./Photo.css";
 import axios from "axios";
+
+function Photo() {
+    const [photoData, setPhotoData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://api.nasa.gov/planetary/apod?api_key=CdR7WVJSUDdSq37rTIIo3tr7DNsUBfTv3zalYquD`)
+            .then(response => {
+                // console.log(response.data);
+                setPhotoData(response.data);
+            })
+            .catch(error => {
+                console.log("the data was not return", error);
+            });
+    }, []);
+
+    return (
+        <div className="photo">
+            <p>{photoData.date}</p>
+            <img src={photoData.url} alt="#" />
+            <p>
+                <strong>{photoData.title}</strong>            
+            </p>
+            <p>
+                <strong>Image Credit: </strong>
+                <a href="https://www.nasa.gov/">NASA</a>,<a href="https://www.esa.int/About_Us/Welcome_to_ESA">ESA</a>,
+                <a href="https://hla.stsci.edu/">Hubble Legacy Archive</a>;
+            </p>
+            <p>
+                <i>Processing & Copyright: </i>
+                <a href="https://www.astrobin.com/users/Rudy_Pohl/">Rudy Pohl</a>
+            </p>
+            <p>
+                <strong>Explanation:</strong> {photoData.explanation}            
+            </p>
+            <p>
+                <strong>Tomorrow's picture:</strong> the window seat             
+            </p>
+                    
+        </div>
+    );
+}
+
+export default Photo;
