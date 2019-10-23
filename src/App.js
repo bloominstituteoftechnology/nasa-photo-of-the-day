255b4deb9c957b01961e2b4db65f52f58b71a6eb
Anna Morris <annapatton444@gmail.com>
2019-10-23 18:49:22 -0500

    styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [nasaData, setNasaData] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=ey66eWBrtfW9TOIVS4F75WoJIBvAZAE4sWAhqaKF"
+      )
+      .then(res => {
+        setNasaData(res.data);
+      })
+
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>NASA Photo of the Day App</h1>
+      <div className="flexbox">
+        <div className="text">
+          <h2>{nasaData.title}</h2>
+          <p>{nasaData.explanation}</p>
+        </div>
+        <div>
+          <img src={nasaData.url} alt={nasaData.title}></img>
+          <p>{nasaData.date}</p>
+        </div>
+      </div>
+      <footer>Anna made this!</footer>
     </div>
   );
 }
