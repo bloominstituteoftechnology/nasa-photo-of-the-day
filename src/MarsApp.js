b536358d1dac706561b9a099fb6a2a46146d42ad
bamfranceschi <anna.franceschi@gmail.com>
2019-11-06 16:10:52 -0600

    attempt at creating a Mars weather app

---
+++ src/MarsApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./MarsApp.css";
+
+function MarsApp() {
+  return (
+    <div className="MarsApp">
+      <h1>Mars Weather Update</h1>
+    </div>
+  );
+}
+
+export default MarsApp;
