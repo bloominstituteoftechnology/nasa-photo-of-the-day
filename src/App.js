b5da16f56b7cf23c900f8431e6ce6cc687b66ca9
jarrodskahill <jarrodskahill@gmail.com>
2019-11-07 14:26:19 -0800

    added information from nasa api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import NavLinks from "./Components/PageTop/NavBar"
-import Photos from "./data.js"
+import NavLinks from "./Components/PageTop/NavBar";
+import Photos from "./data.js";
+import axios from "axios";
 
 function App() {
+  const [photoData, setPhotoData] = useState([]);
+  useEffect(()=>{
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(response =>{
+        setPhotoData(response.data)
+        console.log(response.data);
+    })
+
+},[])
   return (
     <div className="App">
       <NavLinks />
       <h2>Picture of the Day</h2>
-      <Photos />
-      <p>Description: </p>
-      <p>Date</p>
+      <h3>{photoData.title}</h3>
+      <img src={photoData.hdurl} alt='vast universe'/>
+      <p>Taken by: {photoData.copyright} </p>
+      <p>Description: {photoData.explanation} </p>
+      <p>Date taken: {photoData.date}</p>
     </div>
     
   );
--- src/data.js
+++ src/data.js
@@ -1,26 +1,16 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+// import React, {useState, useEffect} from "react";
+// import axios from "axios";
 
 
-function Photos() {
-    const [photoData, setPhotoData] = useState([]);
-    useEffect(()=>{
-        axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-        .then(response =>{
-            setPhotoData(response.data)
-            console.log(response.data);
-        })
+// function Photos() {
+//     const [photoData, setPhotoData] = useState([]);
 
-    },[])
-    
-
-    return (
-        <img></img>
-    )
+//     return (
+//         <img></img>
+//     )
 
        
     
-}
+// }
 
-export default Photos
+// export default Photos
