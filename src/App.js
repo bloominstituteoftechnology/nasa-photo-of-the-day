b29a9ee9531243bca805f774a338bd8e798aa9d1
coltynw <coltynw@users.github.com>
2019-11-06 21:49:18 -0500

    coltyn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Box from "./components/box.js"
+import axios from "axios"
 import "./App.css";
 
+
 function App() {
+  const [nasaImg, setNasaImg] = useState([]);
+  useEffect(() => {
+    
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=HeJ9yb3WaxSY1faBvrEJLfH4brmlYkRaYxo3eOPK")
+    .then(res => {
+      console.log(res.data);
+      setNasaImg(res.data);
+    });
+
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <header><h1>BIG PICTURE SHOW UP HERE</h1></header>
+      <Box title={nasaImg.title} url={nasaImg.url} explanation={nasaImg.explanation} date={nasaImg.date} /> 
     </div>
   );
 }
