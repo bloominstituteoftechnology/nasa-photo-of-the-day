0a1b7f6523c755c58c4fa692b9e4d49e654e0e0a
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2019-11-08 09:12:32 -0500

    finished my project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
  import PictureGrid from "./components/PictureGrid.js";
+ import {alert} from "bootstrap";
 
 function App() {
   
---
+++ src/components/PictureGrid.js
@@ -0,0 +1,42 @@
+import React,{useState, useEffect} from "react";
+//  import Picturecard from "./PictureCard";
+import axios from "axios";
+import {
+  Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
+
+const PictureGrid = () => {
+  const [picture, setPictures] = useState({})
+
+  useEffect(() =>{
+    axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=OPlABOMDOfWvHrty7YqdZxAYClkWMEhvFEOVlTiv`)
+    .then(res => {
+    console.log(res);
+    setPictures(res.data);
+  }) 
+  .catch((err)=> console.log(err));
+},[])
+
+   return(
+     <div>
+       <h1>To days Nasa image</h1>
+       <Card>
+        <CardBody>
+        <CardTitle>{picture.title}</CardTitle>
+        </CardBody>
+        <img className="picture-image" width="100%" height="20%" src={picture.url} alt="Card image cap" />
+        <CardBody>
+          <CardText>{picture.explanation}</CardText>
+          <CardLink href="https://api.nasa.gov/">Card Link</CardLink>
+          <CardLink href="https://www.linkedin.com/notifications/">Another Link</CardLink>
+        </CardBody>
+      </Card>
+    </div>
+   )
+
+   
+}
+
+export default PictureGrid;
