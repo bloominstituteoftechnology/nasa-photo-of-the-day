fd3ccd3e8046963532f3b7a98d2dfda758712048
Notesong <notesong.git@outlook.com>
2019-11-21 11:36:44 -0500

    Use styled-components for video

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,4 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const VideoContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Video = styled.iframe`
+  width: 100%;
+  min-height: 300px;
+`;
 
 export default function Photo({ photo }) {
   return (
@@ -11,27 +22,9 @@ export default function Photo({ photo }) {
                 <img src={`${photo.url}`} alt={`${photo.title}`}/>
               }
               {!`${photo.url}`.toString().endsWith(".jpg") &&
-                <div
-                  className="video"
-                  style={{
-                    position: "relative",
-                    paddingBottom: "56.25%" /* 16:9 */,
-                    paddingTop: 25,
-                    height: 0
-                  }}
-                >
-                  <iframe
-                    style={{
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
-                      width: "100%",
-                      height: "100%"
-                    }}
-                    src={photo.url}
-                    frameBorder="0"
-                  />
-                </div>  
+                <VideoContainer className="video">
+                  <Video src={photo.url} frameBorder="0" />
+                </VideoContainer>  
               }    
             </div>
             <div className="image_links">
