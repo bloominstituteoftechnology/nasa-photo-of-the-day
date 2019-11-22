57fddf54690a495a40ecfd7ae3bfcb526bdcb553
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-11-21 21:37:31 -0800

    Added a Date Picker Input

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,21 +2,26 @@ import React, {useState, useEffect} from "react"
 import axios from "axios"
 import ImageCard from "./imageCard.js"    
 
-export default function Card(props){
+
+export default function Card(){
     const [card, setCard] = useState([]);
-     
+    const [date, setDate] = useState("");
+
+  function handleChange(e) {
+    setDate(e.target.value);
+  }  
     useEffect(() => {
          axios  
-        .get(`https://api.nasa.gov/planetary/apod?api_key=zlqB9lUQl36nWqt1XGT211C1EaSx154A3bZVbvUW`)
+        .get(`https://api.nasa.gov/planetary/apod?api_key=zlqB9lUQl36nWqt1XGT211C1EaSx154A3bZVbvUW&date=${date}`)
         .then (response => {
          const image = response.data;
-             console.log(image);
+             console.log("data here", image);
              setCard(image);
         })
         .catch (err => {
              console.log(err)
         })
-    }, []);
+    }, [date]);
         
     return (
         <div className="container">
@@ -28,6 +33,7 @@ export default function Card(props){
                     picture={card.url} 
                     author={card.copyright}
                     description={card.explanation}
+                    handleChange={handleChange}
                 />
             </div> 
         </div>
--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card,  CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap'; 
+import { Card,  CardText, CardBody, CardTitle, CardSubtitle, InputGroup,Input} from 'reactstrap'; 
 import styled from 'styled-components'; 
 
 const StyledHeading = styled.h1`
@@ -18,35 +18,6 @@ background:#F0F8FF;
 `;
 
 
-/*export default function ImageCardOLD(props) {
-    console.log(ImageCard);
-    return (
-            <div 
-                className="cardBox"
-                key= {props.id}>
-                  
-                <h1> {props.titleOf}</h1>
-                <p>-by {props.author}</p>
-
-                <button
-                        className="date-btn"
-                    >
-                    <p>{props.dateOf}</p>
-                </button>
-
-                <div className="imageBox">
-                    <img 
-                        className="image" 
-                        alt="no pic" 
-                        src={props.picture}
-                    />
-                </div>
-                <h2>Description of the image</h2>
-                <p>{props.description}</p>
-             </div>
-    );
-};*/
-
 const ImageCard = (props) => {
   return (
     <div>
@@ -58,7 +29,14 @@ const ImageCard = (props) => {
                     </StyledHeading>
                         <CardSubtitle>-by {props.author}</CardSubtitle>
                             <br></br>
-                         <Button color="secondary" size="sm">{props.dateOf}</Button>
+                            <CardTitle>Pick a date</CardTitle>
+                                <InputGroup>
+                                    <Input
+                                    type="date"
+                                    onChange={props.handleChange}
+                                    />
+                                </InputGroup>
+
                 </CardBody>
                     <img width="100%" src={props.picture} alt="Card image cap" />
                 <CardBody>
