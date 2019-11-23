a6c843c013577e5bafa750c4fdcdbff32d905d63
shyama <shyabiswas@gmail.com>
2019-11-22 19:04:13 -0500

    added styling to nasa project

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        NASA PHOTO OF THE DAY 🚀!
       </p>
       <NasaCard />
     </div>
--- src/Components/NasaCardLink.js
+++ src/Components/NasaCardLink.js
@@ -1,13 +1,42 @@
 import React from "react";
+import styled from "styled-components"
+
+const Nasainfo=styled.div`
+width: 80%;
+text-align: center;
+display: flex;
+margin-left: 100px;
+flex-direction: column
+`
+const NasaImg = styled.img
+`height: 40%;
+display: block;
+justify-content: center;
+align-items: center;
+margin-top: 30px;`
+
+const Para = styled.p`
+width: 50%;
+display: block;
+text-align: center;
+margin: auto;`
+
+
 
 const NasaCardLink = props => {
   return (
-    <div key={props.id}>
-      <img alt="Random NASA pic of the day" src={props.url} />
-      <h2>{props.title}</h2>
+  
+     
+    <Nasainfo>
+      <NasaImg alt="Random NASA pic of the day" src={props.url} />
+      <h1>{props.title}</h1>
+      
       <h3>{props.date}</h3>
-      <p>{props.explanation}</p>
-    </div>
+      <Para>{props.explanation}</Para>
+      </Nasainfo>
+  
+    
+  
   );
 };
 export default NasaCardLink
