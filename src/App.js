d276585430695ed9f206b6111baf033a807403b6
Ashraf Heleka <ashraf.heleka@insead.edu>
2019-12-11 11:37:09 -0800

    linked to api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import NASAContent from "./../src/components/NASAContent";
+
 import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app!
       </p>
+      <NASAContent />
     </div>
   );
 }
--- src/components/NASAContent.js
+++ src/components/NASAContent.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import NASACard from "./NASACard";
+// import NASACard from "./NASACard";
 
 export default function NASAContent() {
-const [photos, setPhotos] = useState([])
+const [photos, setPhotos] = useState()
 
   useEffect(() => {
-    axios.get(`https://ghibliapi.herokuapp.com/films/`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=vfrnFWvpKczWYpphewMCOu4Cs2ofS4Ulfok6Z8kA`)
     .then(response => {
-      console.log(response.data);
-      setFilms(response.data)
+      console.log(response);
+    //   setFilms(response.data)
     })
     .catch(error => {
       console.log("the data was not returned", error)
@@ -17,12 +17,12 @@ const [photos, setPhotos] = useState([])
   }, [])
   return (
     <div className="photo">
-      {photos.map(photo => {
+      {/* {photos.map(photo => {
         return (
           <NASACard key={film.id} 
           title={photo.title}  />
         )
-      })}
+      })} */}
     </div>
     )
 }
