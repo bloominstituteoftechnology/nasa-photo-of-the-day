fe89fd5d5cf6a1627635a8eb2da25120284a2664
kika <anamonteiro430@gmail.com>
2019-12-11 23:48:39 +0000

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import CardCont from './Nova pasta/CardContainer';
 
 function App() {
+  //use state
+
+  //use effect
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <CardCont />
     </div>
   );
 }
