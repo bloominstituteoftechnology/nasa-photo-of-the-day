debee2704a6632a72178dfc627c4f25b782f63cd
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2019-12-11 13:33:43 -0500

    add nasa photo of the day

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  
+  const [photoOfTheDay, setPhotoOfTheDay] = useState("")
+
+  useEffect( () => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=Q6RD3lJAhyj5FK5PBQDjbzBnfmHtEtIbEcrCVWjS")
+    .then(res => {
+      console.log(res.data)
+      setPhotoOfTheDay(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+  
   return (
     <div className="App">
       <div className="photoHolder">
-        🚀
+        <img src={photoOfTheDay.url} alt={photoOfTheDay.title} />
       </div>
     </div>
   );
