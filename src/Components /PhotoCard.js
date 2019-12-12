4157d5c3a55838c4259de19f43d9b8e789159851
Gus Baden <gusbaden96@gamil.com>
2019-12-12 15:52:13 -0800

    added initial  styling

--- src/Components /PhotoCard.js
+++ src/Components /PhotoCard.js
@@ -4,10 +4,17 @@ import React from "react";
 function PhotoCard (props) {
     return (
         <div className ="Apod-Card">
-            <img className ="Apod-img" alt ="NASA APOD" src = {props.imgUrl}/>
+           
+             <img className ="Apod-img" alt ="NASA APOD" src = {props.imgUrl}/>
+            
+          
             <h1>{props.title}</h1>
+          
+        
             <h2>{props.date}</h2>
+          
             <p>{props.explanation}</p>
+         
 
 
         </div>
