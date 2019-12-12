343ebce9509f86b79f17908d3e8c30311e2d9789
wesley.moody <wgm3190@gmail.com>
2019-12-11 19:44:31 -0600

    forgot to commit all day

---
+++ src/components/ApodDay.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+export default class MyDayPicker extends React.component {
+    constructor(props) {
+        super(props);
+        this.handleDayClick = this.handleDayClick.bind(this);
+        this.state = {
+            selectedDay: null,
+        };
+    }
+
+    handleDayClick(day, { selected }) {
+        this.setState({
+            selectedDay: selected ? undefined : day,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <DayPicker
+                    selectedDays={this.state.selectedDay}
+                    onDayClick={this.handleDayClick}
+                />
+                <p>
+                    {this.state.selectedDay
+                        ? this.state.selectedDay.toLocaleDateString()
+                    : 'Please select a day holder'}
+                    </p>
+                </div>
+        );
+    }
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavLinks() {
+    return (
+        <div>
+            <nav>
+                {/* <a>Mercury</a>
+                <a>Venus</a>
+                <a>Earth</a>
+                <a>Mars</a>
+                <a>Jupiter</a>
+                <a>Saturn</a>
+                <a>Neptune</a>
+                <a>Uranus</a>
+                <a>Pluto!</a> */}
+            </nav>
+        </div>
+    )
+}
+
+export default NavLinks;
