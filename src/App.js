60ee3ce3c90063a0c629edda646dbf8a8a86f3e4
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-12 10:44:16 -0800

    aeson-johnson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import CardHolder from "./Components/CardHolder.js"
 
 function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        <CardHolder />
       </p>
     </div>
   );
---
+++ src/Components/CardHolder.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+
+function CardHolder(props) {
+    const [card, setCard] = usestate("");
+
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=SqCWTnAiuxURR2njh4OibNOUHiuGaCuTzsKywafx")
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    return(
+        <div>
+            <Card 
+                key = {card.index}
+                title = {card.title}
+                imgURL = {card.hdurl}
+                date = {card.date}
+                article = {card.explanation}
+                copyright = {card.copyright}/>
+        </div>
+    );
+}
+
+export default CardHolder;
