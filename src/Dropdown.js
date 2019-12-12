613d02061d81543342305ee42ea6aa2d9b7e71b5
Ashraf Heleka <ashraf.heleka@insead.edu>
2019-12-11 19:07:23 -0800

    more stretch

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import  setDate from "./components/NASAContent"
+
 
 export default function DropDownNASA (props) {
   const [dropdownOpen, setOpen] = useState(false);
@@ -12,11 +14,13 @@ export default function DropDownNASA (props) {
         Select date
       </DropdownToggle>
       <DropdownMenu>
-        <DropdownItem header>Header</DropdownItem>
-        <DropdownItem disabled>Action</DropdownItem>
-        <DropdownItem>Another Action</DropdownItem>
+        {/* <DropdownItem header>Header</DropdownItem> */}
+        {/* <DropdownItem disabled>Action</DropdownItem> */}
+        <DropdownItem onClick={() => setDate("2019-12-08")}>DECEMBER 8, 2019</DropdownItem>
         <DropdownItem divider />
-        <DropdownItem>Another Action</DropdownItem>
+        <DropdownItem onClick={() => setDate("2019-12-09")}>DECEMBER 9, 2019</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem onClick={() => setDate("2019-12-10")}>DECEMBER 10, 2019</DropdownItem>
       </DropdownMenu>
     </ButtonDropdown>
   );
--- src/components/NASAContent.js
+++ src/components/NASAContent.js
@@ -3,11 +3,15 @@ import axios from "axios";
 import NASACard from "./NASACard";
 // import { tsPropertySignature } from "@babel/types";
 
+
+
+
 export default function NASAContent() {
-const [photos, setPhotos] = useState([])
+const [photos, setPhotos] = useState([]);
+const [date, setDate] = useState("2019-11-11");
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
     .then(response => {
       console.log(response.data);
       setPhotos(response.data)
@@ -15,7 +19,7 @@ const [photos, setPhotos] = useState([])
     .catch(error => {
       console.log("the data was not returned", error)
     });
-  }, [])
+  }, [date])
   return (
     <div className="photo">
       < NASACard title={photos.title} imgURL={photos.url} explanation={photos.explanation} date={photos.date}/>
