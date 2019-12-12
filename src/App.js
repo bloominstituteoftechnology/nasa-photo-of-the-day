f54ea3a66c41498b244d04ab401ce91e3fbbe93f
ljh-c <57099181+ljh-c@users.noreply.github.com>
2019-12-11 19:18:20 -0800

    date for APOD can be set by entering from text field

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import "./App.css";
 import AboveTheFold from "./components/AboveTheFold";
 
 function App() {
+  // * * * * * TODAY'S DATE IN YYYY-MM-DD FORMAT
+  const today = new Date();
+  const todayDateFormatted = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+
   const [apod, setApod] = useState({});
+  const [date, setDate] = useState(todayDateFormatted);
+
+  const changeDate = (event, hello) => {
+    event.preventDefault();
+    setDate(hello);
+  }
 
   useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2018-05-29')
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=wJIOkWHVxfRrEKLdY8IAw29SgQy94LgEpCcdB9st&date=' + date)
     .then(response => {
       // console.dir(response.data);
       setApod(response.data);
     })
     .catch(error => {
-      console.dog('Data was not returned', error);
+      console.log('Data was not returned', error);
     });
-  }, []);
+  }, [date]);
 
   return (
     <div className="App">
-      <AboveTheFold url={apod.url} date={apod.date} title={apod.title} explanation={apod.explanation} />
+      <AboveTheFold url={apod.url} date={apod.date} title={apod.title} explanation={apod.explanation} changeDate={changeDate}/>
     </div>
   );
 }
--- src/components/AboveTheFold.js
+++ src/components/AboveTheFold.js
@@ -1,15 +1,26 @@
 import React from "react";
 
-export default function AboveTheFold({ url, date, title, explanation }) {
+export default function AboveTheFold({ date, url, title, explanation, changeDate }) {
   // * * * * * DISPLAYS LOADING MESSAGE WHILE DATA IS FETCHING
-  if (!url) return <h2>Loading...</h2>
+
+  if (!url) return <h2>Loading...</h2>;
+
+  let input = '';
+
+  const handleChange = (event) => {
+    input = event.target.value;
+  }
 
   return (
-    <div>
+    <>
       <p>{date}</p>
       <img src={url} alt="Astronomy of the Day" /><br />
       <b>{title}</b><br />
       <p>{explanation}</p>
-    </div>
+      <form>
+        <input type="text" name="date" placeholder="Date YYYY-MM-DD" onChange={handleChange} />
+        <input type="submit" value="Change Date" onClick={event => changeDate(event, input)} />
+      </form>
+    </>
   );
 }
