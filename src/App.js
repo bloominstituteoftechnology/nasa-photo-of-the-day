060930ebff189372cba66d0a96226634a6ad5151
Anthony Streett <arjstreett@gmail.com>
2019-12-12 10:43:01 -0500

    Work so far

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import Axios from "axios";
+import { HeaderComponent } from "./Components/headerComponent";
+import { SatImageComponent } from "./Components/imgComponent";
 
 function App() {
+
+const [lat, setLat] = useState(0);
+const [lon, setLon] = useState(0);
+
+handleLatChange = handleLatChange.bind(this);
+handleLonChange = handleLonChange.bind(this);
+
+function handleLatChange (event) {
+setLat(event.target.value);
+}
+
+function handleLonChange (event) {
+  setLon(event.target.value);
+}
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <HeaderComponent />
+      <div className="input">
+            <form>
+                <label>
+                    Latitude:
+                    <input type="text" value={lat} onChange={handleLatChange} />
+                </label>
+                <label>
+                    Longitude:
+                    <input type="text" value={lon} onChange={handleLonChange} />
+                </label>
+            </form>
+        </div>
+      <SatImageComponent latProp={lat} lonProp={lon}/>
     </div>
   );
 }
 
 export default App;
+
