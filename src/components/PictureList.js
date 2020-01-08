0cf6501549664c004c5df7e7b1f0cc097bb74b1c
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-08 01:46:57 +0100

    imported several things into PictureList.js and set up axios

---
+++ src/components/PictureList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PictureCard from "./PictureCard";
+
+export default function PictureList(){
+    const [picture, setPicture] = useState([])
+
+    const didUpdate = () => {
+        axios
+        .get ("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then (response => {
+
+            setPicture(response.data);
+            console.log(response);
+        })
+        .catch(error => console.log(error));
+    }
+
+    useEffect(didUpdate, []);
+    
+    return (
+        <div className="picture">
+           
+        </div>
+    )
+
+
+};
+
+
