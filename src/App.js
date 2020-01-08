159c21e06e39cf3d4996832ff8811718673f0703
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-08 02:59:30 +0100

    Got the image to appear on the screen. Worked in PictureList.js, got my own API key from nasa. Still failing to compile because can't resolve axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PictureList from "./components/picture_components/PictureList";
-import "./images/astronaut.png";
+import logo from "./images/astronaut.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" src="./images/astronaut.png" alt="astronaut" />
+        <img className="App-logo" src={logo} alt="astronaut" />
         <div className="App Title">
         <p>Photo of the Day from NASA</p>
         </div>
--- src/components/picture_components/PictureList.js
+++ src/components/picture_components/PictureList.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import PictureCard from "./PictureCard";
 
 export default function PictureList(){
     const [picture, setPicture] = useState([])
 
-    // const didUpdate = () => {
-    //     axios
-    //     .get ("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-    //     .then (response => {
+    const didUpdate = () => {
+        axios
+            .get ("https://api.nasa.gov/planetary/apod?api_key=s0zNjpbfiiM7FI7UhsxFv0X0IqmhdkxtMJ2QzAfg")
+            .then (response => {
 
-    //         setPicture(response.data);
-    //         console.log(response);
-    //     })
-    //     .catch(error => console.log(error));
-    // }
+                setPicture(response.data);
+                console.log(response);
+            })
+            .catch(error => console.log("The data was not returned", error));
+    }
 
-    // useEffect(didUpdate, []);
+    useEffect(didUpdate, []);
     
     return (
         <div className="picture">
-           
+           {picture.map(picture => {
+               return <PictureCard picture={picture} copyright={picture.copyright} date={picture.date} explanation={picture.explanation} />
+           })}
         </div>
     )
 
