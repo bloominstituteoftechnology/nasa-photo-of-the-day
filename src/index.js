325d947ae994d3368999e6099ccec7375ad1ecfe
Candace Wilson <candacewilson@Candaces-MacBook-Air.local>
2020-01-08 00:23:07 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
