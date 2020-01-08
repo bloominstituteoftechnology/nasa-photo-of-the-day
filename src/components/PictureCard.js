d77a6b092f8ece659a09987d983638fb86baacb4
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-08 03:28:38 +0100

    Laid out the PictureCard.js and created a PictureCard.css to style it. Still Failing to compile with Module not found: Can't resolve axios

--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -2,8 +2,20 @@ import React from "react";
 
 const pictureCard = props => {
     return (
-        <div className="picture-list">
-            <h2>{props.tile}</h2>
+        <div className="picture-card">
+            <div className="card-head">
+                <h2>{props.tile}</h2>
+                <h3>{props.date}</h3>
+            </div>
+            <div className="card-img">
+                <img src={props.hdurl}></img>
+            </div>
+            <div className="card-description">
+                <p>{props.description}</p>
+            </div>
+            
         </div>
     )
 }
+
+export default pictureCard;
