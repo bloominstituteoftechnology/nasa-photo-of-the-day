004e895e50a21f85db5498284330f05155044572
Trevor Thompson <trevthom18@gmail.com>
2020-01-08 08:52:06 -0500

    Pushing up updated code, so I can edit it at work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import NasaPicList from "./NasaPicList";
+import DatePicker from 'react-date-picker';
 import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
-      <NasaPicList
-      />
+      <DatePicker />
+      <NasaPicList />
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DatePicker from 'react-date-picker';
+
+
+const Calendar = () => {
+
+}
+
+export default Calendar;
