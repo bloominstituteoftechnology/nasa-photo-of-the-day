2d9ecf377ee4a8063f5beded0394159e79381878
unknown <fnumilat@gmail.com>
2020-01-09 22:08:20 -0600

    work progressed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import Api from "./Components/Api";
+import styled from "styled-components";
+
+const BodyBackground = styled.div`
+  background-color: #100f35;
+  padding-bottom: 2px;
+  padding-top: 4px;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <BodyBackground className="App">
       <Api />
-    </div>
+    </BodyBackground>
   );
 }
 
--- src/Components/ApodCard.js
+++ src/Components/ApodCard.js
@@ -1,13 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+
+
+
+const StyledHeadingOne = styled.h1`
+    background: #d39f16;
+    color: white;
+    width: 40%;
+    margin-left: 30%;
+    margin-top: 1%;
+    padding-top: 8px;
+    padding-bottom: 10px;
+`;
+
+const StyledImage = styled.img`
+    width: 700px;
+    height: 600px;
+    margin-top: 1%;
+`;
+
+const StyledHeadingThree = styled.h3`
+    color: white;
+`;
+
+const StyledParagraph = styled.p`
+    width: 60%;
+    color: white;
+    margin-left: 20%;
+`;
+
+const StyledLogo = styled.img`
+    margin-top: 1%;
+`;
 
 const ApodCard = (props) => {
     return (
         <div className="apodCard">
-            <h1>Astronomy Picture of the Day</h1>
-            <img src={props.hdurl}></img>
-            <h3>Copyright: {props.copyright}</h3>
-            <h3>Date: {props.date}</h3>
-            <p>Explanation: {props.explanation}</p>
+            <StyledLogo src="https://www.festisite.com/static/partylogo/img/logos/nasa.png"></StyledLogo>
+            <StyledHeadingOne>Astronomy Picture of the Day</StyledHeadingOne>
+            <StyledImage src={props.hdurl}></StyledImage>
+            <StyledHeadingThree>Copyright: {props.copyright}</StyledHeadingThree>
+            <StyledHeadingThree>Date: {props.date}</StyledHeadingThree>
+            <StyledParagraph>Explanation: {props.explanation}</StyledParagraph>
         </div>
     )
 };
