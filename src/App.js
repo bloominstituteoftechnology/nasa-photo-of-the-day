cd90bc518833d68a8eb91edb180036c49807f4d1
Trevor Thompson <trevthom18@gmail.com>
2020-01-09 20:46:32 -0500

    Added reactstrap components to the webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
 import NasaPicList from "./NasaPicList";
+import { Alert, Toast, ToastBody, ToastHeader } from 'reactstrap';
 import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
+      <Alert color="primary">
+        This is the NASA image of the day!
+      </Alert>
+      <br />
       <NasaPicList />
+      <br />
+      <Alert color="dark">
+        Check back tomorrow for a new image of the day!
+      </Alert>
     </div>
   );
 }
--- src/NasaCard.js
+++ src/NasaCard.js
@@ -1,28 +1,32 @@
 import React from "react";
 import "./App.css";
+import { Toast, ToastBody, ToastHeader } from "reactstrap";
 
 const NasaCard = props => {
-    
+
     return (
         <div className='img-centerer'>
-         <div>
-            {props.title}
-            <br/>
-            {props.media_type}
-            <br/>
-            {props.service_version}
-            <br/>
-            {props.url}
-            <br/>
-            {props.hdurl}
-            <br/>
-            <br/>
-            <br/>
-            {props.explanation}
-            <br/>
-            <br/>
-            <img src={props.url} alt={props.title}/>
-          </div>
+            <div>
+                <Toast>
+                    <ToastHeader>
+                        {props.title}
+                    </ToastHeader>
+                    <ToastBody>
+                        {props.media_type}
+                        <br />
+                        {props.service_version}
+                        <br />
+                        {props.url}
+                        <br />
+                        {props.hdurl}
+                    </ToastBody>
+                </Toast>
+                <br />
+                {props.explanation}
+                <br />
+                <br />
+                <img src={props.url} alt={props.title} />
+            </div>
         </div>
     )
 }
