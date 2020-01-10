553a6b4e27613659c3fa3cae0ab2cbb84dc5f51c
Tyler Alsop <tylerjalsop@gmail.com>
2020-01-09 18:31:26 -0800

    Installed styled components and imported it to PictureComponent.js

--- src/components/PictureComponent.js
+++ src/components/PictureComponent.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
+import styled from "styled-components";
 
 
 export default function PictureComponent(){
