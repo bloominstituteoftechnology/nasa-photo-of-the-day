54593e501b8babcbcd0024319829f4ba89b14265
zacharyarney <zachary.arney@gmail.com>
2020-01-18 19:05:38 -0800

    dotenv config in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
