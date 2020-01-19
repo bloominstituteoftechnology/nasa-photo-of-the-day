7d13beb43c73514db8a5cb16d5b61da41c725e6a
zacharyarney <zachary.arney@gmail.com>
2020-01-18 19:06:01 -0800

    Displaying image of the day

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
+  const [todaysImage, setTodaysImage] = useState({});
+
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+      .then((res) => {
+        console.log(res.data);
+        setTodaysImage(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [apiKey]);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>{todaysImage.title}</h1>
+      <h3>by</h3>
+      <h2>{todaysImage.copyright}</h2>
+      <p>{todaysImage.explanation}</p>
+      <img src={todaysImage.url} alt={todaysImage.title} />
     </div>
   );
 }
