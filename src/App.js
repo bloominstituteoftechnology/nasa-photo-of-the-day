b8d2164cfe1a126b0183766b79fbfe74ef2f17ea
Alison Law <alisparklaw@gmail.com>
2020-01-22 11:41:51 +0000

    header, footer and background image set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Footer/>
     </div>
   );
 }
