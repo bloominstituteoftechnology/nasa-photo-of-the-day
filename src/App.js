4dba9d2db7b479bf85c0af53b8bbd293cc59b13a
richknicks <rlynch@gmail.com>
2020-01-22 16:34:16 -0500

    add axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+const [data, setData]=useState([])
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() =>{
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(response =>{
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch(error =>{
+      console.log("The data was not returned", error);
+    } )
+  },[])
   return (
     <div className="App">
       <p>
