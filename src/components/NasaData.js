71b5cd5efb282b8870c41901310f4db9b2d2b36d
Alison Law <alisparklaw@gmail.com>
2020-01-22 12:23:55 +0000

    Image card linked

--- src/components/NasaData.js
+++ src/components/NasaData.js
@@ -1,19 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import NasaImagecard from "./NasaImagecard";
+
 
 export default function NasaData(){
+    const [nasaData, setNasaData] = useState([]);
     useEffect(()=>{
 
         console.log("nasa component mounted")
         axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
         .then(response=>{
-            console.log(response)
+            console.log(response.data)
         })
         .catch(error=>{
-            console.log("error")
+            console.log("error: ",error)
         },[])
        
 
     },[]);
- return (<div><h1>card</h1></div>)
+ return (<NasaImagecard/>
+
+     )
 }
---
+++ src/components/NasaImagecard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NasaImagecard(props){
+    return <div>Card</div>
+}
