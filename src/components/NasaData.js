6887a630e7195a3b0f7e9245d6544237e7fe1744
Alison Law <alisparklaw@gmail.com>
2020-01-22 12:10:56 +0000

    receiving data from NASA

--- src/components/NasaData.js
+++ src/components/NasaData.js
@@ -2,5 +2,18 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 export default function NasaData(){
+    useEffect(()=>{
+
+        console.log("nasa component mounted")
+        axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then(response=>{
+            console.log(response)
+        })
+        .catch(error=>{
+            console.log("error")
+        },[])
+       
+
+    },[]);
  return (<div><h1>card</h1></div>)
 }
