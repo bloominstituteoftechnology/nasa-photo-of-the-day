d9a527d43f96735e36c39318e60b128fac50eb62
Alison Law <alisparklaw@gmail.com>
2020-01-22 13:25:28 +0000

    Image and data populating

--- src/components/NasaData.js
+++ src/components/NasaData.js
@@ -2,23 +2,36 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import NasaImagecard from "./NasaImagecard";
 
-
-export default function NasaData(){
-    const [nasaData, setNasaData] = useState([]);
-    useEffect(()=>{
-
-        console.log("nasa component mounted")
-        axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-        .then(response=>{
-            console.log(response.data)
+export default function NasaData() {
+  const [nasaDataArray, setNasaDataArray] = useState([]);
+  const [nasaData, setNasaData] = useState({
+    copyright: "",
+    date: "",
+    explanation: "",
+    hdurl: "",
+    media_type: "image",
+    service_version: "",
+    title: "",
+    url: ""
+  });
+  useEffect(() => {
+    console.log("nasa component mounted");
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=OzNA7KGSQNWftbQgC4w9TH8ZmGkFCBO1tqhVLJAY"
+      )
+      .then(response => {
+        console.log(response);
+        console.log(response.data);
+        return setNasaData(response.data);
         })
-        .catch(error=>{
-            console.log("error: ",error)
-        },[])
-       
-
-    },[]);
- return (<NasaImagecard/>
-
-     )
+      .catch(error => {
+        console.log("error: ", error);
+      }, []);
+  }, []);
+  return (
+  
+  <div>
+<NasaImagecard nasaData={nasaData}/>;
+  </div>)
 }
--- src/components/NasaImagecard.js
+++ src/components/NasaImagecard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function NasaImagecard(props){
-    return <div>Card</div>
+export default function NasaImagecard(props) {
+  return (
+    <div>
+        <h2>CARD</h2>
+        <h4>{props.nasaData.title}</h4>
+        <img src={props.nasaData.hdurl} alt="NASA iotd"/>
+      <h4>{props.nasaData.date}</h4>
+      <h2>{props.nasaData.explanation}</h2>
+    </div>
+  );
 }
