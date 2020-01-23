dd209e20181e86b7a73b2f29510313fc2ef7e464
Freelance Development <49110640+KejdaCelaj@users.noreply.github.com>
2020-01-23 16:39:26 -0500

    Added select date form

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import "./index.css";
 import axios from "axios";
-import Card from "./Components/Card";
+import Card, {changeDate} from "./Components/Card";
 
 
 function App() {
   const [ apod, setApod ] = useState([]);
 
+  
     useEffect(() => {
-        axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-            .then(response => {
-                console.log(response.data);
-                setApod(response.data);
-            })
-            .catch(error => {
-                console.log("The data was not returned", error)
-            });
-    }, []);
+      axios
+          .get('https://api.nasa.gov/planetary/apod?api_key=7pwSaTDuJbsj6nF85g44NoTS5ABm3C5umZp4UhdC')
+          .then(response => {
+              //console.log(response.data);
+              setApod(response.data);
+          })
+          .catch(error => {
+              console.log("The data was not returned", error)
+          });
+   }, []);
+    
 
   return (
     <div className="App">
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "../index.css";
 
+export function changeDate(){
+    if(document.getElementById('inputID').value !== null){
+        //console.log(document.getElementById('inputID').value);
+        return document.getElementById('inputID').value;
+    }else{
+        return "";
+    }
+}
 
 const Card = (props) => {
     return (
         <div className="card">
             <div  className ="header">
-            <img className="logo" src="https://www.festisite.com/static/partylogo/img/logos/nasa.png"/>
-            <p className="title">NASA's Picture of the Day</p>
+                <img className="logo" src="https://www.festisite.com/static/partylogo/img/logos/nasa.png"/>
+                <p className="title">NASA's Picture of the Day</p>
+                <form action="/App.js" method="GET">
+                    <div style={{color:"white", paddingRight:10+"px"}}>Enter date (yyyy-mm-dd):</div>
+                    <input type="text" name="date" id="inputID"></input>
+                    <input type="submit" value="Submit" onClick={changeDate}></input>
+                </form> 
             </div>
         
             <img className="styledImg" src={props.hdurl}/>
@@ -20,7 +33,7 @@ const Card = (props) => {
                 <p className = "descP">{props.explanation}</p>
             </div>
             <footer>
-                <p>Copyright NASA</p>
+                <p>Copyright Lambda School</p>
             </footer>
         </div>
     )
