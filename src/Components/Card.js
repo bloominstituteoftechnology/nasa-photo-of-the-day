a0053b6ff164d3d6629d8a2bc962f36a0c15b1cd
Freelance Development <49110640+KejdaCelaj@users.noreply.github.com>
2020-01-23 19:03:30 -0500

    converted css to styled components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,5 +1,71 @@
 import React from "react";
 import "../index.css";
+import styled from "styled-components";
+
+const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: #100f35;
+    align-items: center;
+`;
+
+const Title = styled.p`
+    color: white;
+    font-size: 30px;
+`;
+
+const Logo = styled.img`
+    padding: 1% 1%;
+    padding-left: 50px;
+    width: 70px;
+    height: 70px;
+`;
+
+const Form = styled.form`
+    display: flex;
+    align-items: flex-end;
+    margin-left: auto;
+    padding-right: 30px;
+`;
+
+const PhotoOfTheDay = styled.img`
+    width: 98%;
+    height: 500px;
+    margin-top: 1%;
+    margin-bottom: 1%;
+    border-radius: 5px;
+`;
+
+const Description = styled.div`
+    display: flex;
+    flex-direction: row;
+    border: 5px solid slategrey;
+    margin: 5px;
+`;
+
+const Info = styled.div`
+    width: 20%;
+    text-align: left;
+    padding-top: 12px;
+    padding-left: 15px;
+    line-height: 1px;
+`;
+
+const DescP = styled.p`
+    width: 80%;
+    padding-left: 10px;
+    margin-right: 10px;
+    text-align: left;
+    border-left: 5px solid slategrey;
+`;
+
+const Footer = styled.footer`
+    text-align: center;
+    background-color: #100f35;
+    color: white;
+    font-style: italic;
+    padding: 5px;
+`;
 
 export function changeDate(){
     if(document.getElementById('inputID').value !== null){
@@ -13,28 +79,28 @@ export function changeDate(){
 const Card = (props) => {
     return (
         <div className="card">
-            <div  className ="header">
-                <img className="logo" src="https://www.festisite.com/static/partylogo/img/logos/nasa.png"/>
-                <p className="title">NASA's Picture of the Day</p>
-                <form action="/App.js" method="GET">
+            <Header>
+                <Logo src="https://www.festisite.com/static/partylogo/img/logos/nasa.png"></Logo>
+                <Title>NASA's Picture of the Day</Title>
+                <Form action="/App.js" method="GET">
                     <div style={{color:"white", paddingRight:10+"px"}}>Enter date (yyyy-mm-dd):</div>
                     <input type="text" name="date" id="inputID"></input>
                     <input type="submit" value="Submit" onClick={changeDate}></input>
-                </form> 
-            </div>
+                </Form> 
+            </Header>
         
-            <img className="styledImg" src={props.hdurl}/>
-            <div className = "description">
-                <div className = "info">
+            <PhotoOfTheDay src={props.hdurl}></PhotoOfTheDay>
+            <Description>
+                <Info>
                     <p>Copyright:</p>
-                    <p className = "date">{props.copyright}</p>
-                    <p className = "date">{props.date}</p>
-                </div>
-                <p className = "descP">{props.explanation}</p>
-            </div>
-            <footer>
+                    <p>{props.copyright}</p>
+                    <p>{props.date}</p>
+                </Info>
+                <DescP>{props.explanation}</DescP>
+            </Description>
+            <Footer>
                 <p>Copyright Lambda School</p>
-            </footer>
+            </Footer>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import 'bootstrap/dist/css/bootstrap.min.css'; 
+// import './node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById("root"));
