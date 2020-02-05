09c6adf044cbc46ff1897c23a3ab1f4dce751e6c
Douglas Little <douglas.a.little@gmail.com>
2020-02-04 22:40:20 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PhotoCard  from "./components/PhotoCard";
 import "./App.css";
 
 function App() {
+const [image, setImage] = useState({});
+useEffect(() => {
+  axios
+     .get('https://api.nasa.gov/planetary/apod?api_key=w44QUG2ECbsfHOtpLlFGuMF5GYogIeRbyiGVewze')
+     .then(response => {
+       setImage(response.data);
+     });
+}, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+        <PhotoCard title={image.title}
+        url={image.url}
+        explanation={image.explanation} />
     </div>
   );
 }
