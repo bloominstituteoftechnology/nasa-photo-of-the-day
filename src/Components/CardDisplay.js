2497125ec3a98a5ead0c7e3096e6c6dd259b614e
Ian Fragoso <ianfragoso@adsl-172-10-1-14.dsl.sndg02.sbcglobal.net>
2020-02-06 07:17:22 -0700

    added bootstrap

--- src/Components/CardDisplay.js
+++ src/Components/CardDisplay.js
@@ -6,13 +6,27 @@ import React, { useEffect, useState } from "react";
  export default function CardDisplay() {
   const [cardInfo, setCardInfo] = useState([]);
 
+  //start
+  function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
+  const randomDate = `${randomNum(2016, 2018)}-${randomNum(1, 12)}-${randomNum(
+    1,
+    28
+  )}`;
+  console.log(randomNum(2016, 2018));
+
+//finish
 
   useEffect(() => {
     axios
     .get(
-      'https://api.nasa.gov/planetary/apod?api_key=Rj04ZLzDBooS9mgpGMQxFTS7WCXpHVfWl7nboSQJ'
+       'https://api.nasa.gov/planetary/apod?api_key=Rj04ZLzDBooS9mgpGMQxFTS7WCXpHVfWl7nboSQJ'
     )
+
+
+
     .then(response => {
         const cardInfo = response.data;
         (console.log(response))
