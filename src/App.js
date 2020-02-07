5c20c6de56ba328bb6d6453462b16287f652400d
Donna Ballew <ballewdd@gmail.com>
2020-02-06 23:44:09 -0800

    added .get link and key

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "reactstrap";
+// import { Button } from "reactstrap";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+  useEffect (() => {
+    axios
+      .get (https://api.nasa.gov/planetary/apod?api_key=SzAoKCd6rTFUztmoHWqnZUdIiXm8aAeKe7ViyXAf
+  }
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-      <Button color="danger">Danger!</Button>
+      {/* <Button color="danger">Danger!</Button> */}
     </div>
   );
 }
