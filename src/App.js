5ac9c54291126fabadb69d814d78d49c87cb009d
Devin Cooper <Dcbekillin@Devins-MacBook-Pro.local>
2020-02-06 20:01:56 -0800

    MVP-day-two

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./App.css";
 import  ApodCard from "./components/ApodCard";
 import ApodGrid from "./components/ApodGrid";
-
-
-
+import { Button } from 'reactstrap'
+import { Spinner } from 'reactstrap';
 function App() {
   return (
     <div className="App">
       <p>
-      Astronomy Picture of the Day!
+      
+      <h1>Astronomy Picture of the Day</h1>
+      <Spinner type="grow" color="primary" />
+      <Spinner type="grow" color="secondary" />
+      <Spinner type="grow" color="dark" />
       </p>
         <ApodGrid/>
     </div>
--- src/components/ApodCard.js
+++ src/components/ApodCard.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Alert
+  } from 'reactstrap';
 
 const ApodCard = (props) => {
     return (
         <div className="apod-info">
-            <div>  {props.title}</div>
-            <div>{props.date}</div>
-            <div>{props.copyright}</div>
-            <img src= {props.hdurl} alt="anything"/>
-            <div>{props.explanation}</div>
-
+            <Card>
+           <CardImg top width="100%" src= {props.hdurl} alt="APOD"/>
+           <CardBody>
+            <Alert color="dark">
+           <CardTitle> {props.title}</CardTitle>
+           <CardTitle>{props.date}</CardTitle>
+           <CardText>{props.explanation} </CardText>
+           </Alert>
+           <Alert color="info">
+            <div>copyright {props.copyright}</div>
+            </Alert>
+            </CardBody>
+            </Card>
         </div>
     );
 };
