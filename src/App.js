3b37e20d101b28c14daaa4bad1c0ed53e7856527
flowersfaerie <flowersfaerie@gmail.com>
2020-02-06 21:05:53 -0800

    Project Started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>
+//         Read through the instructions in the README.md file to build your NASA
+//         app! Have fun 🚀!
+//       </p>
+//     </div>
+//   );
+// }
+
+function App = () => {
+  <div className="App">
+    
+  </div>
 }
+
 
 export default App;
