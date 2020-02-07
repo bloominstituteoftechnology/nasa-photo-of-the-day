44477530e505c4b5165e3cc3b58aa76c3c5a9b8c
flowersfaerie <flowersfaerie@gmail.com>
2020-02-06 22:40:31 -0800

    Started creating and modifying files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
 import "./App.css";
+import Fetch from "./Components/Fetch";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <p>
-//         Read through the instructions in the README.md file to build your NASA
-//         app! Have fun 🚀!
-//       </p>
-//     </div>
-//   );
-// }
-
-function App = () => {
-  <div className="App">
-    
-  </div>
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
+  );
 }
-
 
 export default App;
---
+++ src/Components/Fetch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Fetch = () => {
+  const photo = "#";
+
+  return <div></div>;
+};
+
+export default Fetch;
