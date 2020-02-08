9a161f10a6a380716d1e5af3307b3b11381189a1
Steven Waldron <stevenwaldron@Stevens-MBP.home>
2020-02-08 12:08:43 -0500

    MVP

--- src/cardcreator.js
+++ src/cardcreator.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Body = styled.body`
+    background-image: url("./images/planet-581239_1280.jpg");
+`;
+
+const RedText = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Image = styled.img`
+    width: 50%
+`;
+
+const Paragraph = styled.p`
+    width: 50%;
+    border: solid;
+    
+
+`;
 
 const Cardcreator = props => {
     console.log(props.data)
     return (
-        <div>
-            <h1>{props.data.title}</h1>
-            <p>{props.data.date}</p>
-            <img src = {props.data.url} alt = ""></img>
-            <p>{props.data.explanation}</p>
-        </div>
+        <Body>
+            <RedText>
+                <h1>{props.data.title}</h1>
+                <p>{props.data.date}</p>
+                <Image src = {props.data.url} alt = ""></Image>
+                <Paragraph>{props.data.explanation}</Paragraph>
+            </RedText>  
+        </Body>
+        
     );
 }
 
