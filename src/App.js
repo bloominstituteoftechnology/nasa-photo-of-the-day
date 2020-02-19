337ae0f2307bc7e66e6a3d2c2d83cebcaac6e1f3
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-19 18:07:08 -0500

    bug finally fixed git push -u origin Reagan-Barrington

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Title from "./components/title";
-
+import axios from "axios";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(); 
+  }, []);
+
+  const fetch = () => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=D4OIdTxuBQNfCngsUEVY0h4eAnMQde0clqwVlgc9`)
+    .then(response => {
+        setData(response.data);
+    })
+    .catch(err => {
+        console.log(`Error: ${err}`);
+    })
+  }
+  
+  console.log(data);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-      <Title />
+      words
+
+    <p>{data.date}</p>
+    <Title title={data.title}/>
+
+      {/* {data.map(item => {
+        console.log(item, " is item in App.js")
+        return (<Title title={item.title} />
+        ); CANNOT MAP THROUGH
+      })} */}
+
     </div>
   );
 }
 
-export default App;
--- src/components/title.js
+++ src/components/title.js
@@ -5,7 +5,7 @@ const Title = props => {
   console.log(props, " is props in Title")
   return (
     <div className="Title-container">
-      <h1 className="title">TITLE HERE</h1>
+      <h1 className="title">{props.title}</h1>
     </div>
   );
 };
