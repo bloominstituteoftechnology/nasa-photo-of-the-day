219a3024166a6cf2b2220808d1c000a2c15400f7
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-20 16:18:18 -0500

    image basic styles

--- src/components/img.js
+++ src/components/img.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from "styled-components";
+
+const StyledImg = styled.img`
+  width: 50%;
+  border: black 15px solid;
+  padding: 5%;
+  `;
+  
 
 
 const Img = props => {
   console.log(props, " is props in Img")
   return (
     <div className="Img-container">
-      <img src={props.src} className="img" alt="Space"></img>
+      <StyledImg src={props.src} className="img" alt="Space"></StyledImg>
     </div>
   );
 };
