1ea1ee4168bc9b9392db0f1355b2d51d962b8284
JW Nicholson <john.nicholson812@gmail.com>
2020-02-20 16:07:17 -0500

    color, top border, and alignment  added to explanation text

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const CardWrapper = styled.section`
+    padding:4px;
+    width:80%;
+    margin:auto;
+`;
+
+const ExplanationText = styled.p`
+    color:#3c3c3c;
+    font-weight:bold;
+    border-top:1px solid #3a4454;
+    text-align:left;
+`;
 
 
 {/*image uses state*/}
 const Card = (props) => {
     return (
         <div key={props.id}>
+
+            
             <img alt="nasa photo of the day" src={props.url} />
             <div>
             <p>{props.date}</p>
@@ -13,8 +29,15 @@ const Card = (props) => {
             <p>Image Title</p>
             </div>
            <div>
-           <p>{props.explanation}</p>
-           <p>Bacon ipsum dolor amet drumstick beef ribs doner salami, pig leberkas corned beef tri-tip ham hock boudin meatball. Swine tongue burgdoggen porchetta, alcatra salami venison pastrami short ribs. Beef shankle ham hock kielbasa short loin. Filet mignon shank leberkas ham ham hock prosciutto burgdoggen tongue spare ribs pork loin fatback </p>
+           <CardWrapper>
+               <ExplanationText>
+           <p>{props.explanation}
+           Bacon ipsum dolor amet drumstick beef ribs doner salami, pig leberkas corned beef tri-tip ham hock boudin meatball. Swine tongue burgdoggen porchetta, alcatra salami venison pastrami short ribs. Beef shankle ham hock kielbasa short loin. Filet mignon shank leberkas ham ham hock prosciutto burgdoggen tongue spare ribs pork loin fatback </p>
+
+           </ExplanationText>
+
+
+           </CardWrapper> 
            </div>
            
         </div>
