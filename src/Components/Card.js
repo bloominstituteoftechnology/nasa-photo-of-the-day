d52b806414570febf16c852381cae1d5a1cc08e6
JW Nicholson <john.nicholson812@gmail.com>
2020-02-20 15:38:59 -0500

    added background color to header and changed H1 color using styled-components

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,10 +8,13 @@ const Card = (props) => {
             <img alt="nasa photo of the day" src={props.url} />
             <div>
             <p>{props.date}</p>
+            <p>Date 2002</p>
             <p>{props.title}</p>
+            <p>Image Title</p>
             </div>
            <div>
            <p>{props.explanation}</p>
+           <p>Bacon ipsum dolor amet drumstick beef ribs doner salami, pig leberkas corned beef tri-tip ham hock boudin meatball. Swine tongue burgdoggen porchetta, alcatra salami venison pastrami short ribs. Beef shankle ham hock kielbasa short loin. Filet mignon shank leberkas ham ham hock prosciutto burgdoggen tongue spare ribs pork loin fatback </p>
            </div>
            
         </div>
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const PageTitle = styled.h1`
+    font-size: 2em;
+    text-aligned:center;
+    color:blue;
+`;
+
+//wrapper to render a section tag
+const Wrapper = styled.section`
+    padding:2rem;
+    background: #96939b;
+`;
 
 const Title = () => {
     return(
-        <div className="title">
-            <h1>Nasa Photo of the day</h1>
-        </div>
+
+        <Wrapper>
+            <PageTitle>
+                NASA's Photo of the Day
+            </PageTitle>
+        </Wrapper>
+       
     )
 }
 
