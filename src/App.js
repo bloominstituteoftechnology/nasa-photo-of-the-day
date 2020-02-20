baa29f7bb4323d298825379793c95cf29308c1c7
JW Nicholson <john.nicholson812@gmail.com>
2020-02-19 19:09:40 -0500

    GetData - api is returning data in object form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import GetData from "./Components/GetData";
 
 function App() {
   return (
--- src/Components/GetData.js
+++ src/Components/GetData.js
@@ -1,17 +1,26 @@
-import React, {useState, useEffect} from React;
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-function GamepadHapticActuator(){
+function GetData(){
+const [data, setData] = useState({});
+
+ useEffect (() => {
     axios
-    .get(`https://api.nasa.gov/planetary/apod`)
+    .get(`https://api.nasa.gov/planetary/apod?api_key=LxxuQJv9MQxooAm6LVjWmS2oVhR9BouJKu0xhYYY`)
     .then(response => {
-      console.log(response)
+      console.log(response.data)
+      setData(response.data)
     })
     .catch(error => {
       console.log("Error ", error);
-    },[]);
+    });
+ },[]) 
+    
+
+    return <div></div>
 }
 
+export default GetData;
 
 
 
