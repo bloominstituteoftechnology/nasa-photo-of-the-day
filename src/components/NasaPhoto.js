6816d0889feec9cfb1ac3ce7e5493a61202f3c10
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-02-20 17:09:01 -0800

    edited

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -7,25 +7,25 @@ import {
 import styled from "styled-components";
 
 const Box = styled.div`
-  background: pink;
-  padding: 5%;
+  margin-top: 5%;
 `;
 
 const H1 = styled.h1`
-  font-size: 30px;
+  font-size: 1.5rem;
   color: hotpink;
   transform: translateZ(500px);
 `;
 
 const H2 = styled.h2`
-  font-size: 20px;
+  font-size: 1.2rem;
   color: deeppink;
-  transform: translateZ(200px);
+  transform: translateZ(500px);
 `;
 
 const Text = styled.p`
   color: mediumvioletred;
-  transform: translateZ(200px);
+  transform: translateZ(500px);
+  font-size: 0.9rem;
 `;
 
 const Content = styled.div`
@@ -38,21 +38,13 @@ const Content = styled.div`
 
 const Front = styled.div`
   position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
   transform: rotateY(180deg);
 `;
 
 const Back = styled.div`
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    transform: rotateY(180deg);
-    backface-visibility: hidden;
+  position: absolute;
+  transform: rotateY(180deg);
+  backface-visibility: hidden;
 `;
 
 const NasaPhoto = props => {
@@ -60,7 +52,7 @@ const NasaPhoto = props => {
         <Box>
             <Container>
                 <Row>
-                    <Col sm="12" md={{ size: 8, offset: 2 }}>
+                    <Col sm="12" md={{ size: 10, offset: 1 }}>
                         <Card>
                         <Content>
                         <Front><CardImg top width="100%" src={props.picture} alt="Nasa's Universe of the Day" /></Front>
