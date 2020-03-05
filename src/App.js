54b0d6831ebe7873bca8b8591f96bb921544b7d1
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-05 14:52:58 -0500

    forgot to commit saved wrk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import nasaData from "./../Components/nasaData";
+import nasaCard from "./../Components/nasaCard";
 
 function App() {
   return (
