868cec9c0caca650a6a2ea2ed45bf2501f5f0910
Henry Brophy <henrybrophy@Henrys-MacBook-Pro.local>
2020-03-10 11:39:20 -0400

    Prior to Netlify.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import AstroApi from "./components/AstroApi";
 // import AstroCard from "./components/AstroCard";
 import styled from "styled-components";
 
-const Heading = styled.h1`
-  color: white;
-  background: black;
-  font-size: 70px;
-`;
+// const Heading = styled.h1`
+//   color: white;
+//   background: black;
+//   font-size: 70px;
+// `;
 
 function App() {
   return (
     <div className="App">
-      <Heading>NASA ROCKS</Heading>
+      {/* <Heading>NASA ROCKS</Heading> */}
       <AstroApi />
     </div>
   );
--- src/components/AstroCard.js
+++ src/components/AstroCard.js
@@ -1,28 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 
+const SpaceLi = styled.li`
+  background-color: #27276d;
+`;
+
 const SpaceImg = styled.img`
-  width: 80%;
+  width: 70%;
+  margin-top: 10%;
   border-radius: 5px;
-  border: 2px solid green;
+  border: 2px solid #87619d;
+`;
+
+const SpaceTitle = styled.h1`
+  color: white;
 `;
 
 const Subheading = styled.h2`
-  color: red;
+  color: white;
 `;
 
 const AstroExplainer = styled.p`
   text-align: left;
+  color: white;
+  font-size: 20px;
 `;
 
 const AstroCard = props => {
   return (
-    <li className="astro-card" key={props.id}>
+    <SpaceLi className="astro-card" key={props.id}>
       <SpaceImg src={props.image} alt={props.alt} />
-      <h1>{props.title}</h1>
+      <SpaceTitle>{props.title}</SpaceTitle>
       <Subheading> {props.date} </Subheading>
       <AstroExplainer>{props.explanation}</AstroExplainer>
-    </li>
+    </SpaceLi>
   );
 };
 
