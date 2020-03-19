48f9999eab264ed07dc700496b96c6f9e6df1b37
unknown <jethanhargrave@gmail.com>
2020-03-18 22:24:12 -0400

    changed API img to background

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 const PhotoCard = (props) => {
     return (
-        <div className="photo-list">
-            <img src={props.url} alt=""/>
-            <p>{props.explanation}</p>
-            <p>Date Taken: {props.date}</p>
+        <div className="photo-list" style={{  
+            backgroundImage: "url(" + props.url + ")",
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            height: '100vh'
+          }}>
+            <p style={{
+                color: 'white'
+            }}>{props.explanation}</p>
+            <p style={{
+                color: 'white'
+            }}>Date Taken: {props.date}</p>
         </div>
     )
 }
