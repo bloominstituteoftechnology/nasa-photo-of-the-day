82ec732e6451175f4cf8aa265238940c1caea41b
John Rossi <DACOOK1983@gamil.com>
2020-04-14 13:27:55 -0400

    adding advanced styling to card

--- src/components/conainter/ApodCard.js
+++ src/components/conainter/ApodCard.js
@@ -1,40 +1,59 @@
 import React from "react"
+import styled from "styled-components";
+
+//styles
+const WrapperDiv = styled.div`
+    display:  flex;
+    flex-direction:column;
+    width: 50vw;
+    align-items: center;
+    justify-content: center;
+
+`;
+const Image = styled.img`
+width: 50%;
+border-radius: 5px;
+`;
+
+const Title = styled.h2`
+font-size: 1.5rem;
+`;
 
 const ApodCard = props => {
     // console.log(props);
    
 
     return (
-        <div className="nasa-image"
-        style={{
-            display:"flex",
-            flexDirection:"column",
-            width:"50vw",
-            alignItems:"center",
-            justifyContent:"center",
-            // border:"1px red solid"
-
-        }}
-        >
-            <h2>{props.title}</h2>
-            <p>{props.date}</p>
-            <div className="content"
+ 
+        <WrapperDiv>
+         <Title>{props.title}</Title>
+          {/* <h2>{props.title}</h2> */}
+          <p>{props.date}</p>
+          <div
+            className="content"
             style={{
-                width:"100%",
+              width: "100%",
             }}
+          >
+            <Image src={props.image} alt={props.title} />
+            {/* <img
+              src={props.image}
+              alt="{props.title}"
+              style={{
+                width: "50%",
+                borderRadius: "5px",
+              }}
+            /> */}
+            <p
+              style={{
+                width: "100%",
+              }}
             >
-                <img src={props.image} alt="{nasa.title}"
-                style={{
-                    width:"50%",
-                    borderRadius:"5px"
-                }}
-                 />
-             <p style={{
-                 width:"100%",
-                 
-             }}>{props.exp}</p>
-            </div>
-        </div>
+              {props.exp}
+            </p>
+          </div>
+        </WrapperDiv>
+    
     );
 };
 
