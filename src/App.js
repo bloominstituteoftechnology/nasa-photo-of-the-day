4b52c83f97dd565688d0cbfcddebc437ec516327
Taja House <ahlliahtaja@yahoo.com>
2020-04-14 23:07:20 -0700

    Completed adding information to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
-function App() {
+function App () {
+  const[nasaPic, setNasaPic] = useState({data:[]});
+  console.log("Here is your array", nasaPic.data);
+  const nasa = nasaPic.data;
+
+  useEffect(()=>{
+    const fetchImg = () => {
+      axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=RLYbKz9tWF55dRNCVGiaxs0odRHkptZaP5eOXwbm")
+        .then(res => setNasaPic(res))
+    };
+    fetchImg();
+  },[])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>NASA Pic of the day!</h1>  
+ 
+      <img 
+        src={nasa.url} 
+        alt='Nasa Pic of the Day'
+        key={nasa.id} 
+      />
+  <p>{nasa.explanation}</p>
+
+      
     </div>
   );
 }
