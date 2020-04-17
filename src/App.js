af26a84e18dc1b34054165d30855edd855892d1c
Taja House <ahlliahtaja@yahoo.com>
2020-04-16 18:16:23 -0700

    Added some styled-components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import DropDown from "./DropDown";
+import styled from 'styled-components';
+
+const P = styled.p`
+max-width: 80%;
+padding-left: 11%;
+font-size: 1rem;
+color:white;
+text-shadow: 2px 2px 2px black;
+`;
 
 function App () {
   const[nasaPic, setNasaPic] = useState({data:[]});
@@ -11,14 +20,16 @@ function App () {
   useEffect(()=>{
     const fetchImg = () => {
       axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=RLYbKz9tWF55dRNCVGiaxs0odRHkptZaP5eOXwbm")
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
         .then(res => setNasaPic(res))
     };
     fetchImg();
   },[])
   return (
     <div className="App">
-      <h1 className="heading">NASA Pic of the day!</h1>  
+      <h1 className="heading">NASA Pic of the day!</h1>
+
+      <P>{nasa.date}</P>  
     
  
       <img 
@@ -27,7 +38,7 @@ function App () {
         key={nasa.id} 
       />
         <DropDown />
-  <p className="explanation">{nasa.explanation}</p>
+      <P>{nasa.explanation}</P>
 
       
     </div>
