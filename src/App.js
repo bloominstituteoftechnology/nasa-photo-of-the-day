a9f7a51b5c6ec7ddc7fa375e71ba0dd4b08fc7e9
ballewaer <ballewaer@email.com>
2020-04-17 17:36:21 -0500

    added to src/app.ja

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react";
 import "./App.css";
+import NasaPhoto from "../src/components/NasaPhoto";
 
-function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+
+       {/* <Button color="danger">Danger!</Button> */}
+       <NasaPhoto />
     </div>
   );
-}
 
 export default App;
