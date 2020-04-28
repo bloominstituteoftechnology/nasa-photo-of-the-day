d19054741c8ca24116b69c72ead443b54fcd7a0c
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-27 22:14:49 -0400

    Just to get started

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+//PUTTING THIS HERE FOR PULL REQUEST
+
 ReactDOM.render(<App />, document.getElementById("root"));
