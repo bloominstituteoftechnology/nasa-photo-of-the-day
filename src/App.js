68e52a68a12a6a13062e982d6630fdfa77c9adb5
Kirk Tolliver <kirktolliver28@gmail.com>
2020-05-01 16:42:31 -0500

    Update axios and useffect data.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Planet from "./components/Planet.js"
 import "./App.css";
 
 /* const axios = require('axios').default; */
 
 function App() {
 
+const [nasaState,setNasaState] = useState({});
+
 useEffect(() =>{
 
-axios.get(https://api.nasa.gov/#apod)
-     .then((response)=>console.log(response))
-     .catch(error => console.log(error))
-    
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+         .then((response)=>{
+             console.log(response.data)
+             setNasaState(response.data)
+         })
+         .catch(error => console.log(error))
 },[])
-              
-const [state,setState] = useState()
+
 
 
   return (
@@ -23,6 +27,7 @@ const [state,setState] = useState()
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Planet description={nasaState.explanation} />
     </div>
   );
 }
