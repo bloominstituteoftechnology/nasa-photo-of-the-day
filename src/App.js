2add9ef6c84fac7b0ee8f7e2f554974067fc0b2e
Owlspec3086 <ethical.worker3@gmail.com>
2020-05-14 12:23:48 -0400

    Flex assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+
+//based Components
+import Welcome from '../src/components/Welcome';
+import Nav from '../src/components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    // inside div put base components also it will be inside of retun.
+    <div>
+      <Nav />
+      <Welcome />
     </div>
   );
 }
