2e7e932f320d2bb7dcabf5e7677b744990077048
Teagan Keith <tokiyn@yahoo.com>
2020-05-14 01:24:20 -0400

    MVP / Added some styling

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Header from "./components/header/header.js"
 import PostContainer from "./components/PostContainer/PostContainer.js"
 
 
+
+const apiKey= "api_key=wdgMfHqmpIs81ojLq6QzxTYCV9Zkwto17RgzmCtR"
+
 function App() {
   const [info, setInfo] = useState({});
 
 
+
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=wdgMfHqmpIs81ojLq6QzxTYCV9Zkwto17RgzmCtR`)
+    axios.get(`https://api.nasa.gov/planetary/apod?${apiKey}`)
           .then(res => {
             console.log(res)
             setInfo(res.data)
@@ -23,11 +27,11 @@ function App() {
           })
         }, []);
   
-  
+        
   return (
     <div className="App">
       <Header/>
-      <PostContainer title={info.title} url={info.url} date={info.date} explanation={info.explanation} />
+      <PostContainer title={info.title} url={info.url} date={info.date} explanation={info.explanation} copyright={info.copyright}/>
     </div>
   );
 
--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -1,15 +1,17 @@
 import React from "react";
+import "./postcontainer.css"
 
 
 const PostContainer = props => {
     return (
-    <> <h2>{props.title}</h2>
+    <> <h2 className="title-heading">{props.title}</h2>
     <img alt="" src={props.url} className="main-image"></img>
     
     <h2 className="date-heading">{props.date}</h2>
 
     <p className="explaination">{props.explanation}</p>
     
+    <p className="copyright">Copyright: {props.copyright}</p>
     </>
 
     );
