4e3961894de9109fbb3449d5478557a1156f0c47
JenVest2020 <jenvest2019@gmail.com>
2020-05-27 00:32:09 -0500

    added styles to btn and worked on Images Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Images from './images/Images';
 import Axios from "axios";
 
 function App() {
@@ -8,13 +9,13 @@ function App() {
     Axios.get('https://api.nasa.gov/planetary/apod?api_key=t14m2JW1Ga7UKDuT1mksMGcLeRKYSX8lojAiQtAD')
       .then(Response => {
         console.log('from App file', Response)
-
       })
   }, [])
   return (
     <div className="App">
       {/* <h1 className='App-header'>{data.data.title}</h1> */}
-      <button className='btn'>Next Day Preview</button>
+      <Images />
+      <button className='btn'>==Next Day Preview==></button>
     </div>
   );
 }
---
+++ src/images/Images.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Images(props) {
+    const [image, setImage] = useState([])
+    console.log('from Images', props)
+    return null;
+}
+
+
+export default Images;
