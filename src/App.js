7522c91595fa5433c1ad6bc54d9f4f1c151f6857
JenVest2020 <jenvest2019@gmail.com>
2020-05-26 22:45:45 -0500

    Axios response successful

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    Axios.get('https://api.nasa.gov/planetary/apod?api_key=t14m2JW1Ga7UKDuT1mksMGcLeRKYSX8lojAiQtAD')
+      .then(Response => {
+        console.log('from App file', Response)
+      })
+  }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+
     </div>
   );
 }
