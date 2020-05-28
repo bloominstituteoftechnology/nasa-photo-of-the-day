452067d18130dce968b11026d1222b0442f07c9f
JenVest2020 <jenvest2019@gmail.com>
2020-05-28 17:20:59 -0500

    styled App component&removed cssfile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import Images from './images/Images';
 import Axios from "axios";
+import styled from "styled-components";
+
+const AppDiv = styled.div`
+text-align: center;
+`;
+
+const AppHeader = styled.h1`
+background-color: rgba(146, 146, 245, .15);
+ margin: 0;
+  min-height: 15vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  `;
 
 function App() {
   const [data, setData] = useState([]);
@@ -16,10 +32,10 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <h1 className='App-header'>{data.title}</h1>
+    <AppDiv>
+      <AppHeader>{data.title}</AppHeader>
       <Images img={data} />
-    </div>
+    </AppDiv>
   );
 }
 
