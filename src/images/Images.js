5426632597859413a8177ef1a13c81e0694fcc3b
JenVest2020 <jenvest2019@gmail.com>
2020-05-28 17:38:21 -0500

    styled ImagesComponent&removed cssfile

--- src/images/Images.js
+++ src/images/Images.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import './Images.css';
+import styled from "styled-components";
+
+const ImageContainer = styled.div`
+width:80%;
+    margin: 0 auto;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    `;
+const Image = styled.img`height: 400px;
+    width: 70%;
+    margin: 2% 0;
+    `;
+const ImageInfo = styled.p`
+color:whitesmoke;
+background-color:rgba(146, 146, 245, .15);
+font-size: large;
+font-weight: bold;
+padding: 2%;
+`;
 
 function Images(props) {
     console.log('from Images', props)
     return (
-        <div className='imageContainer'>
-            <img src={props.img.url} alt='random outerspace'></img>
-            <p className='imageInfo'>{props.img.explanation}</p>
-        </div>
+        <ImageContainer>
+            <Image src={props.img.url} alt='random outerspace'></Image>
+            <ImageInfo className='imageInfo'>{props.img.explanation}</ImageInfo>
+        </ImageContainer>
     )
 }
 
