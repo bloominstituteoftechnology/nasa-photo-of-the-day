450b01e7b3e13c9ea415973f7fa47fa09779d79d
jleon1992 <36936276+jleon1992@users.noreply.github.com>
2020-06-10 16:44:21 -0400

    Phew I got through that

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  // const apikey = "bAcLUqRaRZ8eAxrG9b4TBiz6d1agngrqFTegqQZz"
+  const[details, setDetails]=useState([])
+
+  useEffect(() => {
+    console.log("first render")
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=bAcLUqRaRZ8eAxrG9b4TBiz6d1agngrqFTegqQZz`)
+    .then(response => {
+      console.log(response.data)
+      setDetails(response.data)
+
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+    
+  },[] )
+
+  function Photo(props){
+    const {details} =props
+    
+    return(
+     
+    <div>
+      <h1>{props.details.title}</h1>
+      
+      <img src = {props.details.url}></img>
+      <p>{props.details.explanation}</p>
+    <p>{props.details.date}</p>
+    <p>copyright: {props.details.copyright}</p>
+    </div>
+  
+  
+    )
+  }
+
   return (
+
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+  
+      <Photo details  = {details} />
+      
     </div>
   );
 }
