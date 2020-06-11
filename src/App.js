ed4ae2a1033604f588425d91d284dde23f85154b
unknown <admin@social-collab.org>
2020-06-10 21:56:19 -0400

    Not finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
+import NasaFile from '../src/Component/Axios/NasaFile'
+import NavMenu from'../src/Component/NavBar/NavMenu'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <p>
+      <NavMenu />
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+        app! Have fun 🚀!
+        
+      </p> */}
+      
+      <NasaFile />
+      
     </div>
   );
 }
