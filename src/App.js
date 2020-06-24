c3cc7766e1bb6661cdab89498d773abb7a3f8c38
noah niesen <noah@noahs-MacBook-Pro.local>
2020-06-23 20:19:11 -0500

    completed assignment and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./App.css";
+import axios from 'axios';
+import Card from './components/Card';
 
-function App() {
+const App = () => {
+  const [picture, setPicture] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=OPfamgCyYwhJvre3PNrqYoTF4OdMQEPSYJ2XNNTg&date=1998-10-11')
+      .then((res) => {
+        console.log(res);
+        setPicture(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Card
+        key={picture.id}
+        title={picture.title}
+        date={picture.date}
+        explanation={picture.explanation}
+        copyright={picture.copyright}
+        imgUrl={picture.url}
+      />
     </div>
   );
 }
