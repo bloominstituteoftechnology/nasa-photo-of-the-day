0109c981cccb46b5f2d5f013a7f3634b58f2d474
Terry Edwards <terryedwardsjr113@gmail.com>
2020-06-25 19:09:47 -0400

    some style

--- src/components/post.js
+++ src/components/post.js
@@ -1,12 +1,11 @@
 import React from "react" 
-
 const Post = ({title,date,url,explanation,copyright})=>{
     return (
-        <div>
-           <h3>{title}-{date}</h3>
-           <img src={url} alt="pic of the day"/>
-           <p>{explanation}</p>
-           <p>{copyright}</p>
+        <div className="background">
+           <h2 className="title">{title} - {date}</h2>
+           <img className="image"src={url} alt="pic of the day"/>
+           <p className="explain" >{explanation}</p>
+           <p className="copyright">{copyright}</p>
         </div>
     );
 }
