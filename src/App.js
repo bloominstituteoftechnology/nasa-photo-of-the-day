a9da469e4662c856f5a52f26f6f8713bbaf5f767
Alex Williams <alexjwilliams1@gmail.com>
2020-07-15 15:18:36 -0700

    pulled api, added axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import Photo from './Photo'
 
 function App() {
+  const [photo, setPhoto] = useState("")
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then( res => {
+      console.log("works")
+      console.log(res.data.title)
+    })
+    .catch(() => {
+      console.log("Failure to launch")
+    })
+  }, [])
+  
   return (
     <div className="App">
+      <div></div>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
