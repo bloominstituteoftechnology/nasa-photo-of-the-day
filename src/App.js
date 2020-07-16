cbf45f7500d98eff6bb9d6c5fb519063f2e72664
Nicholas Wooden <uniloki009@gmail.com>
2020-07-16 13:54:47 -0400

    Finished now I just want to clean up and add maybe one more api

--- src/App.js
+++ src/App.js
@@ -5,20 +5,40 @@ import { BASE_URL, API_KEY } from "./constants/index"
 import Photoheader from "./components/Photoheader";
 import Header from "./components/Header";
 import Info from "./components/Info";
+import styled from 'styled-components';
 
+const RandomButton = styled.button`
+  color: black;
+  padding: 15px;
+  margin: 10px;
+  background: darkgrey;
+  border: none;
+  font-size: 15px;
+  font-family: 'Raleway', sans-serif;
+
+  &:hover{
+    background: grey;
+  }
+`;
 
 function App() {
   const [photoData, setPhotoData] = useState('')
   const [marsData, setMarsData] = useState('')
+  const [date, setDate] = useState('')
   const [currentPhoto, setCurrentPhoto] = useState('')
   const [monthlyData, setMonthlyData] = useState([]) 
   const [dropDown, setDropDown] = useState(1)
+  const [currentExplain, setCurrentExplain] = useState('')
+
+  
   useEffect(() => {
     axios.get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
     .then(res =>{
       console.log(res.data)
       setPhotoData(res.data)
       setCurrentPhoto(res.data.url)
+      setCurrentExplain(res.data.explanation)
+      setDate(res.data.date)
     })
     .catch(err =>{
       console.log('error')
@@ -36,7 +56,7 @@ function App() {
     })
   }, [])
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=fiJ0anE4tukk6Vd1QNH4e0NNjZNWOUAyi0brdVjK&start_date=2020-06-01&hd=true&end_date=2020-07-15`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=fiJ0anE4tukk6Vd1QNH4e0NNjZNWOUAyi0brdVjK&start_date=2020-06-01&hd=true&end_date=${date}`)
     .then(res =>{
       setMonthlyData(res.data)
       })
@@ -50,6 +70,7 @@ function App() {
     let marsArrayL = marsArray.length;
     let randomPicture = Math.floor(Math.random() * (marsArrayL + 1));
     setCurrentPhoto(marsArray[randomPicture].img_src)
+    setCurrentExplain(marsArray[randomPicture].earth_date)
     console.log(currentPhoto)
   }
 
@@ -59,6 +80,7 @@ function App() {
       if (element.date === selectedDate)
       {
         setCurrentPhoto(element.url)
+        setCurrentExplain(element.explanation)
         return element.url
       }
     })}
@@ -67,9 +89,10 @@ function App() {
     <div className="App">
       <Header />
       <Photoheader photoUrl = {currentPhoto} />
-      <Info dateInfo= {photoData.date} />
-      <button onClick={() => newMars()}> More Space mars</button>
-
+      <Info dateInfo= {photoData.date} explaination={currentExplain} />
+     <div className='buttonDiv'> 
+      <RandomButton onClick={() => newMars()}> More Space mars</RandomButton>
+      <RandomButton onClick={() => newMars()}> More Space mars</RandomButton>
       <form>
         <select
         onChange={(e)=> setDropDown(e.target.value),(e) => checkDate(monthlyData,e.target.value)}>
@@ -81,8 +104,8 @@ function App() {
             )
           })}
         </select>
-  
       </form>
+      </div>
     </div>
 
 
--- src/components/Info.js
+++ src/components/Info.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 
 export default function Info(props) {
-    const { dateInfo } = props
+    const { dateInfo, explaination } = props
 return (
     <div className='infoDiv'>
             <p>  Hello todays date is {dateInfo} </p>
+            <p> {explaination} </p>
     </div>
 )
 }
