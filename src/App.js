cfed9967ac3853c1215701212c2939d5061052c2
unknown <olem2233@gmail.com>
2020-07-16 18:54:39 -0700

    NASA Picture of the day

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React, { useState, useEffect }  from "react";
 import axios from 'axios';
 import Photo from './Photo'
 import "./App.css";
-
+import styled, { keyframes } from 'styled-components';
 
   //1oJrF13Q1tIu0fHn4XyN8p8cl5YxouQkmmiqTgXn&date=2012-03-14
   
 
 function App() {
-  const [apod, setAp]=useState();
+
+  const [apod, setApod]=useState([]);
   const [imgMars, setMars]=useState([]);
+
 useEffect(()=>{
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=1oJrF13Q1tIu0fHn4XyN8p8cl5YxouQkmmiqTgXn&date=2020-07-15')
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=1oJrF13Q1tIu0fHn4XyN8p8cl5YxouQkmmiqTgXn&date=2020-07-16')
   .then((r)=>{
     console.log(r);
-    setAp(r.data.url);
+
+    setApod([r.data.url,r.data.explanation]);
   })
   .catch(()=>{
     console.log("errorr")
@@ -34,17 +37,64 @@ useEffect(()=>{
 
 console.log(apod)
 console.log(imgMars)
+
+
+
+
+
+const StyledDetails = styled.div`
+
+background-color: lightblue;
+
+h1 {color: red;
+  font-size: 3.2em;
+  &:hover {
+    color: #4d79ff;
+    transform: translate(10px, 10px) rotate(360deg);
+    font-size: 4em;
+    transition: all 2s ease-in-out;
+  }
+}
+
+p {
+  width: 80%;
+  text-align: center;
+  color: green;
+  font-size: 1.2em;
+  padding-left: 10%;
+  &:hover {
+    color: #000d33;
+  }
+}
+
+
+.contMars {
+ 
+  &:hover {
+    background-color: #ff704d;
+   
+  }
+}
+
+imgMars {
+  &:hover {
+    transform: translate(200px, 150px) rotate(20deg)
+  }
+
+}
+
+
+
+`
+
   return (
+    <StyledDetails id='detailsView' className ='container'>
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-      <img src={apod} alt="space"/>
-      <p>Comet NEOWISE has been wowing photographers around much of the world during dawn and dusk, at the margins of day and night.  For the most northern residents of planet Earth, however, the comet circles the North Star and never sets. 
-        The night part of this circular arc is apparent in the featured composite of images assembled from a webcam located at a ski resort in the Swiss Alps.  Images were selected at 30-minute intervals throughout the night from July 12th -13th. 
-        Comet NEOWISE (C/2020 F3) will continue to become more accessible to northern hemisphere observers as its motion places it higher in the sky each evening after sunset over the next few weeks, as it begins its outbound journey.  As with all comets, departure from the inner Solar System comes with inevitable fading.  
-        Binoculars are the best way to find and observe the comet visually.</p>
+      <h1>
+      NASA Astronomy Picture of the Day
+      </h1>
+      <img src={apod[0]} alt="space"/>
+  <p>{apod[1]}</p>
         <div className='contMars'>
         {imgMars.map(i =>
   <Photo imgMars = {i.img_src}/>
@@ -52,12 +102,14 @@ console.log(imgMars)
       </div>
     </div>
     
-
+    </StyledDetails>
     
 
 
 
   );
 }
+
+
 
 export default App;
