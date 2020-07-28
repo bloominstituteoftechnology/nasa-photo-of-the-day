6faa269ee19af5a6fffa93f3f235eae122d2f9bd
David Veele <you@example.com>
2020-07-28 17:37:00 -0400

    imported api and start of structure added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+/** @format */
+
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=59u4qxyJKj9io0THIuahiScnXgdQRSqfNCIBT84f"
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
