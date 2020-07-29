ba689522e95434320fc9e6d6ede8d1ef917e6a67
Brianna Larkins <blarkins123@gmail.com>
2020-07-28 20:43:48 -0700

    not done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
 import "./App.css";
 
 function App() {
+  const [apod, setApod] = useState([]);
+  
+  useEffect(() =>{
+    axios.get('https://api.nasa.gov/planetary/apod')
+    .then((r) => {
+      console.log(r);
+      setApod([r.data.url, r.data.explanation]);
+    })
+    .cath(() =>{
+      console.log('error')
+    })
+  },[])
+
+  console.log(apod)
   return (
     <div className="App">
       <p>
