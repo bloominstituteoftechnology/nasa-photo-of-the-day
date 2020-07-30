a3068d04956d4b084341ec94808f77f64721a35b
kato hawkins <kristhahawkins@icloud.com>
2020-07-30 02:02:35 -0400

    finished most of MVP, still working on the url link

---
+++ src/components/NasaCard.js
@@ -0,0 +1,18 @@
+// import React from "react";
+
+// const NasaCard = ({ nasaInfo }) => {
+  
+//     return(
+//         <div className ="nasa-info">
+//             <div className = "App-logo"></div>  
+//           <div className = "App-header">NASA Photo of the Day</div>
+//           <div className = "App-link">The App Link</div>
+//           <div className = "outerEyeOpen"> {title} </div>
+//           <img src= {photo} alt= "NPOD"/>
+//           <div className = "date"> {date}</div>
+//           <div className ="explanation"> {explanation} </div>
+//           <div className = "NPOD"> </div> 
+//         </div>
+//     )
+//     }
+//     export default NasaCard;
