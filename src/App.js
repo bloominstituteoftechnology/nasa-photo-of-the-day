a32ec9162ba671ecbf3133e291be077660769b7e
Jeanellle96 <jeanellepazos@gmail.com>
2020-07-30 20:45:11 -0700

    half way done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 import "./App.css";
+import axios from "axios"
+import NasaPhoto from "./NasaPhoto"
+import NasaHeader from "./component/NasaHeader";
 
 function App() {
+
+  const [date, setDate] = useState([])
+  const [photo, setPhoto] = useState([])
+
+  useEffect(() => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?start_date=2020-07-30&end_date=2020-07-30&api_key=KlcaosUJ0HlEb72piQHAMgUUmzGZgCgGHJony8un")
+    .then((response) => {
+        console.log(response);
+        setPhoto(response.data);
+    })
+    .catch((error => console.log(error)));
+}, []);
+console.log(photo);
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <NasaHeader/>
+      <NasaPhoto/>
     </div>
   );
-}
-
+};
+  
 export default App;
---
+++ src/component/NasaHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const NasaHeader = () => {
+    return(
+        <div className = "NasaHeader">
+            <h1>Nasa</h1>
+            <h2>Photo Of The Day</h2>
+        </div>
+    );
+};
+
+export default NasaHeader;
