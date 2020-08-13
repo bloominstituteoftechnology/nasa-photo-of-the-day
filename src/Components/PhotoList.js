525a5c3ab41e343c4c01682db1a16d5400fdf771
Francis Bonacic <francisbonacic@icloud.com>
2020-08-13 08:48:57 -0700

    made progress but got stuck at .mapping the data

--- src/Components/PhotoList.js
+++ src/Components/PhotoList.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import PhotoCard from './PhotoCard';
 
 export default function PhotoList() {
 
@@ -8,15 +9,27 @@ const [photos, setPhotos] = useState([]);
 useEffect(() => {
 axios.get('https://api.nasa.gov/planetary/apod?api_key=cCT6X5kWMjBBdQfhvqzWXZY3KP8Ivtsb7vtbrnbY')
 .then(response => {
-    console.log(response)
+    
+setPhotos(response.data)
+console.log(response.data)
+    
 })
 .catch(error => {
     console.log("the data was not returned",error)
 })
 }, [])
 return (
+    
     <div className = "photo">
 
+    {photos.map(photo => {
+        return (
+            <PhotoCard
+            title = {photo.title}
+            about = {photo.explanation}
+             />
+        )
+    })}
 
     </div>
 )    
