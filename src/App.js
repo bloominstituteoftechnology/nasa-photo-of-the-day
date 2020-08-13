6a3dd6c9204d9e8b36387e7b251f5a9898e5ab05
BrettMcAdams96@gmail.com <brettmcadams96@gmail.com>
2020-08-13 02:35:07 -0400

    finished basic app styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  const [photo, setPhoto] = useState(null)
+
+
   return (
     <div className="App">
+      <h1>NASA Photo Of The Day</h1>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
