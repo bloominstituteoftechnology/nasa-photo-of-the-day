8413aa530cdbdc4afafbc8ca6a2231179205a839
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-09 18:22:47 -0600

    effect hook added to app.js

---
+++ constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://localhost:3000/'
+export const API_KEY = 'https://api.nasa.gov/planetary/apod?api_key=zVEKK7s0g39H4TFfgq7J2u5cUDboLRYwtvGXyDhc'
--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
+import { API_KEY, BASE_URL } from '../constants/index'
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/nasaData?api_key=${API_KEY}`)
+    .then(res => {
+      setNasaData(res.data)
+      console.log(nasaData);
+    })
+    .catch(err => {
+      debugger
+    })
+   }, []) 
+
   return (
     <div className="App">
       <p>
