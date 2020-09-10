ac37f6438236a974edb64f8a3875be881bdb0c7a
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-09-10 16:24:00 -0400

    Styled Components added to NASA Photo of the Day

--- src/NasaContent.js
+++ src/NasaContent.js
@@ -1,5 +1,27 @@
 import axios from "axios"
 import React, {useState, useEffect} from "react";
+import styled, { keyframes} from "styled-components"
+
+const keyFra = keyframes `
+  50% {
+    opacity: 0.5;
+}
+  100%{
+    opacity: 1;
+}`
+
+const StyledNasaContent = styled.div`
+    opacity: 0;
+    animation: ${keyFra} 2s forwards;
+    background-color: ${props => props.theme.primaryColor};
+    color: ${props => props.theme.secondaryColor};
+    padding: ${props => props.theme.paddingSmall};
+      
+    &:hover {
+      color: ${props => props.theme.tertiaryColor};
+      transition: all 0.5s ease-in-out;
+    }
+    `
 
 
  function NasaContent(){
@@ -18,7 +40,7 @@ import React, {useState, useEffect} from "react";
     })
   },[])
   return (content &&
-    <div className="App">
+    <StyledNasaContent className="App">
         <div>
       <h1>
         {content.title} <span role="img" aria-label='go!'>🚀</span>!
@@ -27,7 +49,7 @@ import React, {useState, useEffect} from "react";
       </div>
       <div><img alt="data" src={content.url}></img></div>
      <div> <p><span> </span>{content.explanation}</p> </div>
-    </div>
+    </StyledNasaContent>
   );
   }
   export default  NasaContent; 
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+// Theme object
+    export default{
+        primaryColor: "#86A3C3",
+        secondaryColor:"#ED553B",
+        tertiaryColor: "#F6D55C",
+        white: "white",
+        black: "black",
+        paddingSmall: "8px",
+}
