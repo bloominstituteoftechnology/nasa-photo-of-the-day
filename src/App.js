d7fa18a9b458658f997a7b68ad4f36e22e641b14
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-09-10 10:41:07 -0500

    moved file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from "axios"
-import Card from "./Card";
+import Card from "./Components/Card";
 
 function App() {
   const [nasaData, setNasaData]= useState([])
@@ -13,7 +13,7 @@ function App() {
       console.log(response.data)
       // const nasaPhoto = (response.data)
       // const objectInfo1 = Object.values(nasaPhoto)
-      setNasaData(response.data)
+      setNasaData(response.data.title)
     })
     .catch(error =>{
       console.log("sorry no image", error)
@@ -21,9 +21,9 @@ function App() {
   }, [])
   return (
     <div className="container">
-    {nasaData.map((image) =>{
-      return <Card key={image.id} imgURL={image.url} title = {image.title} />
-    })}
+    {/* {{nasaData.map((image) =>{
+      return <Card key={image.id} imgURL={image.url} title = {image.title} /> }
+    })} */}
     </div>
   );
 }
