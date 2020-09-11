282447aa759ac3287c77badcae520013cb7aa39a
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-09-10 21:44:04 -0500

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import axios from "axios"
+import axios from "axios";
 import Card from "./Components/Card";
+
 
 function App() {
   const [photo, setPhoto]= useState([])
 
   useEffect(() => {
     axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=GjNb0rJyTAIgAWhma78UqFZdzMGzdtcCNT98FcDC`)
+    .get(`https://api.nasa.gov/planetary/apod?api_key=GjNb0rJyTAIgAWhma78UqFZdzMGzdtcCNT98FcDC&date=2020-09-09`)
     .then(response => {
       const nasaPicInfo = (response.data)
       console.log(nasaPicInfo)
@@ -24,6 +25,7 @@ function App() {
     <div className="container">
       <Card imgUrl = {photo.url} title = {photo.title} description = {photo.explanation}/>
     </div>
+   
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,60 @@
 import React from "react"
+import styled, { keyframes } from 'styled-components'
+
+
+
+const StyledBody = styled.body `
+background-color: ${props => props.theme.primaryColor};
+`
+
+const StyledCard = styled.div `
+display: flex;
+flex-direction: column;
+`
+
+
+const StyledHeader = styled.div`
+    text-align:  ${props => props.theme.headerTextAlign};
+    padding: ${props => props.theme.headerPadding};
+    font-size: ${props => props.theme.headerFontSize};
+`
+
+const StyledDescription = styled.div`
+    padding: 2%;
+    margin-left: 20%;
+    margin-right: 20%;
+`
+
+const StyledImgContainer = styled.div`
+display: flex;
+justify-content: center;
+border-top: 8px ${props => props.theme.secondaryColor} solid;
+border-bottom: 8px ${props => props.theme.secondaryColor} solid;
+
+`
+
+const StyledImg = styled.img `
+    width: 40%;
+    height: 60vh;
+`
+
 
 const Card = props => {
     console.log(props)
     return(
-        <div className="nasa-card">
-            <img className = "nasa-img" alt="random" src ={props.imgUrl}/>
+        <StyledBody>
+        <StyledCard lassName="nasa-card">
+            <StyledHeader  className="img-title">
             <h2> {props.title}</h2>
+            </StyledHeader >
+            <StyledImgContainer className="img">
+            <StyledImg className = "nasa-img" alt="random" src ={props.imgUrl}/>
+            </StyledImgContainer>
+            <StyledDescription className = "description">
             <p>{props.description}</p>
-        </div>
+            </StyledDescription>
+        </StyledCard>
+        </StyledBody>
     )
 }
 export default Card
