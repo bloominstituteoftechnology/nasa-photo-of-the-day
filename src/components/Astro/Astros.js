d3f4eb71ee31171e7a52e57199a870099a5fd96f
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-23 06:25:16 -0700

    Stuck loading

--- src/components/Astro/Astros.js
+++ src/components/Astro/Astros.js
@@ -14,24 +14,30 @@ const Astros = () =>{
 
     // Set a new apikey if there is no more room for this one
     const [apiKey, setApiKey] = useState('7MH99v254pwUt83ewWyggb86uQjDcbUCJrAzM2xf');
+    const [reqGet, setReqGet] = useState([{}]);
    
     const [nasaPhotos,setNasaPhotos] = useState([{}]);
     const reUrl = ["https://api.nasa.gov/planetary/apod?api_key="+apiKey+"&hd=false&start_date=2020-02-01&end_date=2020-02-01","https://api.nasa.gov/planetary/apod?api_key="+apiKey+"&hd=false&start_date=2020-01-01&end_date=2020-01-02"] ;
-    const [reqUrl,setReqUrl] = useState(reUrl[1]);
+    const [reqUrl,setReqUrl] = useState(reUrl[0]);
     // let url = reUlr[1];
     useEffect(() =>{
         
         axios.get(reqUrl)
         .then((re) =>{
             let ar = [{}];
-            re.data.map( (el,i) =>{
-                // console.log(re.data[0].title);
+            setReqGet(re);
+            console.log(re);
+            if(!reqGet.data ) return <h3>Loading</h3>;
             
+            re.data.map( (el,i) =>{
+                console.log(re.data[0].title);
+                console.log('else')
             ar[i] = el;
-             console.log(ar);
+            //  console.log(el);
              setNasaPhotos(ar[i]);
              return astroFn(nasaPhotos);
             });
+        
         })
         .catch((er) =>{
             console.log(er);
