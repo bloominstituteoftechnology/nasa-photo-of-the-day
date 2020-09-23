54adaac7dc90b44c8cfb1b2e9a3d2d03fc9f1968
KVlearn <kavya.mdu@gmail.com>
2020-09-23 13:42:08 -0700

    updated style

--- src/Components/MoreInfo.js
+++ src/Components/MoreInfo.js
@@ -13,21 +13,20 @@ const toggleInfo = () => {
 return(
 <div>
 <h5>More info here!</h5>
-{infoOn && 
+  {infoOn && 
     <p style={{textAlign:"left",backgroundColor:"#080808"}}>
       {info}</p>
   }
- 
-<Button 
+  <div>
+  <button 
+     style={{color:"indianred",fontSize:"18px",borderRadius:"5px",paddingBottom:"2px",transition:"ease-in-out 3s"}}
      id='expand'
      variant="outline-danger"
      onClick={toggleInfo} 
-> {infoOn ? 'Hide' : 'Show'} More Info
-</Button>{' '}
-
-
+  >{infoOn ? 'Hide' : 'Show'} More Info
+    </button>{' '}
+  </div>
 </div>
-
 )
 
 }
