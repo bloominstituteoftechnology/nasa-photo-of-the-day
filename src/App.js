3f5470988af2086219613a30389c301c48ef082c
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-09-23 08:07:04 -0700

    added useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 function App() {
+const [data, setData] = useState([])
+
   return (
     <div className="App">
       <p>
