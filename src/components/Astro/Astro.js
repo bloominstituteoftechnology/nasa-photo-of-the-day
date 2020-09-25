1a52e2c4f61717e349e1490bfee863fdd67a7a39
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-25 05:39:11 -0700

    Styled component update need to make a date picker

--- src/components/Astro/Astro.js
+++ src/components/Astro/Astro.js
@@ -1,41 +1,48 @@
 
 import React,{useState} from "react";
 import {altTxt} from "../../Globals";
-import Media from "../Vid/Media";
-import Vplayer from "../Vid/Vplayer";
+import Media from "../Vid/Media"; 
+import styled from "styled-components";
 
+
+
+
+const AstroCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    height:100%;
+    width: 100%;
+    padding: 20px;
+    display: flex;
+    min-height:100%;
+    min-width:15rem;
+    justify-content: center;
+    border-radius: 5px;
+    box-shadow: 0 1px 6px -2px #000;
+    background-color: #FFF;
+    margin-bottom: 30px;
+`;
+
+const AstroCardInfo = styled.p`
+    height:100%;
+    width:100%;
+    padding-left:12px;
+    box-shadow: 1px 1px black;
+    border:1px solid black;
+
+
+`;
+
+
+const AstroImgD = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: column;
+`;
 
 const Astro = ({props,isImg}) => {
-  // const title = props.film.title
-  // const film = props.film
-  // const { film } = props
 
-  /* Props --> "Properties of our component"
-
-  There are a couple ways of accessing prop values. All are valid and you'll find different
-  examples of these online & in future jobs.
-  1. Props as a param (MovieCard = props => {}). Any reference of inner value would be with props.key 
-  2. create const with key/value pair in props: const film = props.film
-  3. use object destructuring to create const of key/value pair: const { film } = props
-  NOTE ^^^ the name of the const needs to be the same name of key in props object
-  4. use object destructuring in parameters: MovieCard = ({ film }) => {}
-  */
-
-//   const { title, description, director, release_date } = film;
-//   return (
-//     <div className="film-list">
-//       <h2>{title}</h2>
-//       <p>{description}</p>
-//       <div className="bottom">
-//         <p>{director}</p>
-//         <p>{release_date}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-        // const data = props;
-        // const [isImg,setIsImg] = useState(true);
         const title = props.title;
         const description = props.explanation;
         const url = props.url;
@@ -49,14 +56,15 @@ const Astro = ({props,isImg}) => {
         if (!props.url) return <h3>Loading...</h3>; 
         else{
         return (
-            <div  className="card">
+            <AstroCard>
               <h2>{title}</h2>
-              <p className="card-info">{description}</p>
-              <div className="imgD" >
+              <AstroCardInfo>{description}</AstroCardInfo>
+
+              <AstroImgD>
                 <Media url={url} isImg={isImg}/>
                 <p>{date}</p>
-              </div>
-            </div>
+              </AstroImgD> 
+            </AstroCard>
           );
         }
 };
