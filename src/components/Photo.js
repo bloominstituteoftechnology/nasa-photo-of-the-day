32550cc85bba851bdc43363098f2a97b2f10d663
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-09-24 20:56:45 -0700

    added styled components

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,18 +1,38 @@
 import React from "react";
-import Text from "./Text";
+// import Text from "./Text";
+import styled from "styled-components";
 import { Card, Button, CardImg, CardBody,
     CardTitle, CardText } from 'reactstrap';
 
+
+const Image = styled.div`
+    width: 75%;
+    border: none:
+    border-radius: 50px;
+    margin: 0 auto;
+    
+    `;
+
+const Paragraph = styled.p`
+    margin: 2%;
+`;
+
+
 const Photo = (props) => {
     return(
+        <div>
         <Card>
         <CardBody>
             <CardTitle tag="h1">{props.data.title}</CardTitle>
-            <CardImg src={props.data.url} alt="#"/>
-            <CardText>{props.data.explanation}</CardText>
+            <Image>
+            <CardImg Image src={props.data.url} alt="#" />
+            </Image>
+            <Paragraph><CardText>{props.data.explanation}</CardText></Paragraph>
         <Button>{props.data.date}</Button>
+
         </CardBody>
     </Card>
+    </div>
     )
 }
 
