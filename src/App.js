6c5e1a84632d92cf3e73469b1296102b8c505430
Ramsha <ramshanasir.rn@gmail.com>
2020-10-07 15:29:40 -0500

    'working'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
-import "./App.css";
-import picList from './components/picList'
 
+import "./App.css";
+import { useState, useEffect } from "react";
+
+import {API_KEY, BASE_URL} from '../src/constants/index'
+import Axios from "axios";
 
 function App() {
+  const [nasaImg, setNasaImg] = useState()
+
+
+  useEffect(() => {
+    const fetchImg = () => {
+      Axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+        .then(res => {
+          setNasaImg(res.data)
+        })
+        .catch(err => {
+          debugger
+        })
+    }
+    fetchImg ()
+    
+}, [])
+
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
-      <picList />
+      
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=API_KEY'
+export const API_KEY = 'K3sHNgJzsB3jrEvkYuKTc6CsT5JNFFaEkYPbxmnc'
