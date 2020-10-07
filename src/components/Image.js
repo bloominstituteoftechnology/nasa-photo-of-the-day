81fcafbe19b5ceecfbfbcf2a68328b79c4476e7e
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-07 19:19:23 -0400

    got the image showing

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,25 +6,30 @@ import { API_KEY, BASE_URL } from "../constants/index";
 
 export default function Image(props) {
     const { camera, close } = props
-    const [imageData, setImageData] = useState(null)
+    const [imageData, setImageData] = useState('')
 
     function randomImage(data) {
-        return null;
+        return data[Math.floor(Math.random() * (data.length + 1))]
     }
 
 
   useEffect(() => {
     axios.get(`${BASE_URL}${camera}/photos?earth_date=2015-10-6&api_key=${API_KEY}`)
     .then(res => {
-         setImageData(randomImage(res.photos))
-        console.log(res.data.photos[1].img_src)
+         setImageData(randomImage(res.data.photos))
+        // console.log(res.data.photos[1].img_src)
     })
     .catch(err => {
         debugger
     })
-  });
+    console.log(imageData)
+  }, []);
+  console.log(imageData)
 
 return (
-    <div>test</div>
+    <div className="image">
+       <img src={imageData.img_src} alt="NASA_image"></img>
+  
+    </div>
 )
 }
