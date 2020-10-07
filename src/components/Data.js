c226762cd85db5ed04041544218686252f8f2440
Ramsha <ramshanasir.rn@gmail.com>
2020-10-07 17:03:48 -0500

    mvp

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,9 +5,24 @@ import NasaCard from "../components/NasaCard";
 
 
 function Data(props) {
+    const [nasaInfo, setNasaInfo] = useState([])
+
+    useEffect(() => {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=K3sHNgJzsB3jrEvkYuKTc6CsT5JNFFaEkYPbxmnc')
+            .then(res => {
+                console.log('Res data :', res)
+                setNasaInfo.apply(res.data)
+            })
+            .catch(err => {
+                console.log('YOU MESSED UP!')
+            })
+    }, [])
     
-return null
-}
+    return (
+        <div className='dataContainer'>
+        <NasaCard dat = {nasaInfo} />
+    </div>
+)
 
 
 export default Data;
