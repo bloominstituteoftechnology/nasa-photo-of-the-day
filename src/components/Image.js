8eaa916ca2f6e797adba210705884de4b891d4ea
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-08 18:27:57 -0400

    Styled image.js. Need to add media queries

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,18 +1,41 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
 import axios from "axios";
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes } from "styled-components";
 
 import { API_KEY, BASE_URL } from "../constants/index";
 
 const StyledImageFrame = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 50%;
   margin: auto;
-`
+  padding-top: 5%;
+  color: ${pr => pr.theme.blueLight};
+  text-transform: capitalize;
+  button {
+    background-color: ${pr => pr.theme.blueLight};
+    color: ${pr => pr.theme.pale};
+    font-size: ${pr => pr.theme.fontSize.large};
+    border: solid 3px ${pr => pr.theme.pale};
+    border-radius: 15px;
+    padding: 5px;
+    margin-bottom: 5%;
+    width: 20%;
+    &:hover {
+      background-color: ${pr => pr.theme.pale};
+      color: ${pr => pr.theme.black};
+      transition: all 0.4s ease-in-out;
+    }
+  }
+`;
 
-
+const StyledImage = styled.img`
+  width: 100%auto;
+  max-width: 100%;
+  object-fit: cover;
+`;
 
 export default function Image(props) {
   const { camera, close, reClick } = props;
@@ -36,11 +59,11 @@ export default function Image(props) {
       });
   }, [camera, reClick]);
 
-
   return (
     <StyledImageFrame className='image'>
+      <h2> {camera}: {imageData.earth_date}</h2>
       <div className='image-container'>
-      <img src={imageData.img_src} alt='NASA_image'></img>
+        <StyledImage src={imageData.img_src} alt='NASA_image'></StyledImage>
       </div>
       <button onClick={close}>Close</button>
     </StyledImageFrame>
