28e5ccf163e6afeb24287cf81f01193d01157a3a
Trey Roberts <treywroberts@gmail.comgit config --global user.name TreywRobertsrungit.addgit commit -m added>
2020-10-08 18:52:50 -0400

    Okay got a picture and some other stuff!

--- src/App.js
+++ src/App.js
@@ -2,15 +2,51 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 import "./App.css";
 import "./rover.js"
+import {API_KEY} from './keyData'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  *{
+    background-color: black;
+    color:white;
+  }
+  
+  h1{
+    color:red;
+    font-size:3rem;
+    margin-top:0%;
+    margin-bottom:0%;
+  }
+  h2{
+    margin-top:0%;
+    margin-bottom:0%;
+  }
+  h3{
+    margin-top:0%;
+    margin-bottom:0%;
+  }
+  p{
+    margin-top:0%;
+    width:50%;
+    margin-left:25%                          ;
+  }
+  img{
+    margin-top:0%;
+    margin-bottom:0%;
+    width:50%;
+    border:dashed 3px white;
+  }
+`
 
 function App(){
   const[photo, setPhoto] = useState({})
 
   useEffect(() =>{
     const fetchImage = () =>{
-      axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
       .then(res=>{
         setPhoto(res.data)
+      console.log(res.data)
       })
       .catch(err=>{
 
@@ -20,16 +56,19 @@ function App(){
   },[] )
 
 
-
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build y <span role="img" aria-label='go!'>🚀</span>!
+    <StyledApp className="App">
+      <h1>NASA Photo of the Day</h1>
+      <h2>{photo.title}</h2>
+        <h3>Date</h3>
+        <h3>{photo.date}</h3>
         <img src={photo.url}/>
+      <p>
+        {photo.explanation}
       </p>
-    </div>
+      <p>by {photo.copyright}</p>
+    </StyledApp>
   );
 }
-
 
 export default App;
---
+++ src/keyData.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'MJ8RIYUM2a04XNKVMXEUnBj1vdcOgvHJAUy1064a'
