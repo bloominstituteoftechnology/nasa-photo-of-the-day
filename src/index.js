9e1405a16797411f53de97a9eacdea1bf3e6dcb3
Vikash Viraj <48885303+vikashviraj@users.noreply.github.com>
2020-10-10 14:30:52 +0530

    send data by props - props handling

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App details="Read through the instructions in the README.md file to build your NASA
+        app! Have fun" />, document.getElementById("root"));
