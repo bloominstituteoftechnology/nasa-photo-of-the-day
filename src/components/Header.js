712a6cdd5e94cb417e2df457b1e6f91fdb995377
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-10-20 22:34:35 -0400

    fixed error in logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
    
       <div className="header__container">
-       <h1>>_nasapod</h1>
+       <h1>{`>_nasapod`}</h1>
         <a href="https://github.com/rmscrlos/nasa-photo-of-the-day/tree/carlos-ramos"><i className="fab fa-github"></i></a>
       </div>
   
