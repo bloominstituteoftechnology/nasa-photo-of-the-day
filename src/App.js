92e5dde6a59356dfb744d1f0fc8451580a8ed652
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-10-22 22:01:30 -0400

    cannot get API to work

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React, { useState, useEffect} from "react";
-import axios from 'axios';
+import React from "react";
 import "./App.css";
+import NasaPhoto from './NasaPhoto.js';
 
 function App() {
 
-  const [imgSrc, setImgSrc] = useState("");
-  // const [data, setData] = useState([]);
-
-  useEffect(() => {
-  axios.get(`https://api.nasa.gov/planetary/apod?api_key=zHixeT7Pa2hNqzpzqKAikorH9xVrquOCZi5oxrW5`)
-  .then(res => {
-    console.log(res)
-    setImgSrc(res.data)
-  }) .catch(err => {
-    console.log(err);
-  });
-  // return function () {
-  //   console.log('cleanup');
-  // }
-}, []);
+  // const [src, setSrc] = useState(NasaPhoto);
   
   return (
     <div className="App">
-      {imgSrc.map()}
+      <NasaPhoto />
+      {/* data={imgSrc} */}
       <h1>NASA Photo Of the Day!</h1>
-      <img alt="NASA Photo Of the Day!" src={imgSrc} />
+      {/* <img alt="NASA Photo Of the Day!"/> */}
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
