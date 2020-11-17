dd4038d62dcfe2b89ae7ed7581636301ecc25f88
Nico Herrera <nherrera0831@gmail,com>
2020-11-16 22:34:35 -0800

    set axios data to state, laid out almost all framework for css, need to start making title and daily image dynamic in the jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=N8TjJsyaQZV1KcUqWt74gj6Cajc3t1vXhp8vLWkk"
+      )
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <section className="title-apod-container">
+        <div className="title-container">
+          <div className="title-box">
+            <h1 className="title">Astronomy Photo Of The Day</h1>
+          </div>
+        </div>
+        <div className="image-container">
+          // insert dynamic image into image tag
+          <div className="image-box">
+            <img src="" className="apod-image" />
+          </div>
+          <div className="apod-description">
+            // insert dynamic title from data object
+            <p className="apod-description-title">{}</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
