b8637483b8a95ee59663ef01895aae15b7c10b52
Dominick Groven <36062458+DominickGroven@users.noreply.github.com>
2020-11-17 18:06:36 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
+import NPODcard from "./Components/NPODcard"
 import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <NPODcard/>
     </div>
   );
 }
