8aee3b250b9c7dc9b384220b2caa700e7db3311c
Nico Herrera <nherrera0831@gmail,com>
2020-11-18 22:42:09 -0800

    added StyledText and StyledDate styled components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Calendar from "react-calendar";
+import styled from "styled-components";
 
 function App() {
   const [data, setData] = useState({});
@@ -88,6 +89,22 @@ function App() {
     );
   }
 
+  const StyledText = styled.div`
+    height: 100%;
+    width: 100%;
+    font-size: 0.8rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+  `;
+
+  const StyledDate = styled.div`
+    font-size: 1rem;
+    height: 30px;
+    width: 150px;
+  `;
+
   console.log(value);
   return (
     <div className="App">
@@ -103,20 +120,24 @@ function App() {
             <div className="image-box">
               <img src={data.hdurl} className="apod-image" alt={data.title} />
             </div>
-            <div className="text-container">
-              <div className="apod-description">
-                {/* // insert dynamic title from data object */}
-                <p className="apod-description-title">{data.title}</p>
-              </div>
-            </div>
+            <StyledDate>{formatDate(value)}</StyledDate>
           </div>
         </section>
-        <section className="calendar-container">
-          <div>
-            <Calendar onChange={(event) => onChange(event)} value={value} />
+        <section className="text-container">
+          <div className="apod-description">
+            {/* // insert dynamic title from data object */}
+            <p className="apod-description-title">{data.title}</p>
           </div>
+          {/* insert dynamic styled component for .explanation */}
+          <StyledText>{data.explanation}</StyledText>
         </section>
       </div>
+
+      <section className="calendar-container">
+        <div>
+          <Calendar onChange={(event) => onChange(event)} value={value} />
+        </div>
+      </section>
     </div>
   );
 }
