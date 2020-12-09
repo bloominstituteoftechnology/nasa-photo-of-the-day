32ea522bcc0f783acb3ffbcca9bbc37ceda05f75
george-hatzigeorgio <georgehatzigeorgio@zohomail.com>
2020-12-09 18:20:04 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./App.css";
+import axios from "axios"
+
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+  const [nasaData, setNasaData]= useState(result)
+  const date = 
+
+  useEffect(() => {
+    
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=leHoEfK9OcSztrGdehxyUGRHuuLHnFxfKypXslXg&date=2012-03-14`)
+        .then((res) => {
+          const result = res.data
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    
+
+    ;
+  }, []);
+
+ 
 }
 
 export default App;
