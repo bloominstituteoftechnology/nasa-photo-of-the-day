80f6f5645e16dcda1c67ae86d0d282ee6bd61d93
Nathan Chagar <nathan.adel.chagar@gmail.com>
2020-12-09 14:32:09 -0800

    Not MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+
 
 function App() {
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
     </div>
   );
 }
