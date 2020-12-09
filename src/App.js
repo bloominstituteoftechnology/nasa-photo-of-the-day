7e12a4ea0769618680e52881610df030e096838c
Chaz Carbis <chazcarbis@gmail.com>
2020-12-09 17:15:06 -0500

    issa site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios"
+// import APOD from './APOD'
+
+const API_KEY = 'DEMO_KEY'
+const URL = 'https://api.nasa.gov/planetary/'
+
+
 
 function App() {
+  const [pics, setPic] = useState(null);
+
+
+  useEffect(() => {
+    const fetchAPOD = () => {
+      axios
+      .get(`${URL}/apod?api_key${API_KEY}`)
+      .then(res => {
+        setPic(res.data)
+      })
+      .catch(err => {
+        console.log(`ERROR: ${err}`)
+      })
+    }
+    fetchAPOD()
+  }, [])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      
     </div>
   );
 }
