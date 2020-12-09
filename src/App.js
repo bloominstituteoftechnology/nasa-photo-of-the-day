af24141d3fc18bdb8afdef94927d1ac3147d3611
Karla Cervantes <cervantes.karla14@gmail.com>
2020-12-09 17:23:07 -0600

    first page done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { BASE_URL, API_KEY, DATE } from "../src/constants/index";
 
 function App() {
+  const [picture, setPicture] = useState({});
+  
+  useEffect(() => {
+    const fetchData = () =>{
+    axios
+    .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}&date=${DATE}`)
+    .then((res) => {
+      setPicture(res.data.hdurl);
+      // console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://api.nasa.gov'
+export const API_KEY = 'DEMO_KEY'
+export const DATE = '2012-03-14'
