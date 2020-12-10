9756cfe6272835b8cb9525a28dba2ef24e70e714
Gatien Orti-vinas <gatien.orti@gmail.com>
2020-12-10 09:47:44 -0500

    finish with style and vide

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import PictureCard from './PictureCard'
 import Header from './component/Header'
 
+
+
 function App() {
   return (
     <div className="App">
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,17 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React from 'react'
 import '../App.css'
-
+import styled from 'styled-components'
 export default function Header(){
 
 
-
+const HeaderWhole = styled.div`
+h1{
+    font-size: ${pr=> pr.theme.sizeTitle};
+    margin-top: 4%;
+    color:${pr=> pr.theme.primaryColor};
+}
+div{
+    display:flex;
+    justify-content: center;
+}
+a{
+    margin: 1% 2% 3% 2%;
+    text-decoration: none;
+    color:${pr=>pr.theme.secondaryColor};  
+    font-size:${pr => pr.theme.sizeHeader};
+    font-weight: 'bold';
+}
+`
 
 
 
 
 return(
-    <div>
+    <HeaderWhole>
         <h1 className='title'>Nasa Photo Of The Day</h1>
         <div className='header'>
             <a href='#'>Home</a>
@@ -19,11 +35,5 @@ return(
             <a href='#'>History</a>
             <a href='#'>Contact</a>
         </div>
-       
-    </div>
-
-
-)
-
-
-}
+    </HeaderWhole>
+)}
