0dfa731663ccf9ad43ee7dbcfa156eee679fa5c2
Karla Cervantes <cervantes.karla14@gmail.com>
2020-12-09 23:25:14 -0600

    two completed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import { BASE_URL, API_KEY, DATE } from "../src/constants/index";
+import Header from "./components/header";
+
+
 
 function App() {
-  const [picture, setPicture] = useState({});
+  const [picture, setPicture] = useState([]);
+  const header = useState();
   
   useEffect(() => {
     const fetchData = () =>{
@@ -12,7 +16,7 @@ function App() {
     .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}&date=${DATE}`)
     .then((res) => {
       setPicture(res.data.hdurl);
-      // console.log(res.data)
+      // console.log(res.data.hdurl)
     })
     .catch((err) => {
       console.log(err);
@@ -21,14 +25,15 @@ function App() {
     fetchData();
   }, []);
 
-
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
-      
+      <Header header = {header}/>
+      <img src={picture} alt="picture of the day"/>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import "./App.css";
+
+const Header = props =>{
+    const {header} = props;
+    return (
+        <div className='header'>
+            <h1>This is the Header</h1>
+        </div>
+    )
+}
+
+export default Header;
