6813065aa049a44630f1aec36521393ee9707f0e
MaurooRen <maurooren@gmail.com>
2020-12-10 15:47:29 -0500

    creating Menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types'
 
 const Menu = (props) => {
 
+    function Link() {
+        return 
+    }
+
+    //Create Menu Component that show you the next option:
+    // Yesterday - Today - Tomorrow
+
     return (
         <>
-            
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
         </>
     )
 }
