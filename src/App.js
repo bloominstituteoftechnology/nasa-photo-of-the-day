dc44bdedccb8ea360897e992a4ce7ca43d3b1b94
Michelle Chen <mchen0062@gmail.com>
2020-12-09 16:24:45 -0800

    NASA video is attached to DOM. New page to be created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+
+  const [photo, setPhoto] = useState([]);
+
+  const BASE_URL = "https://api.nasa.gov"; 
+  const API_KEY = "6of5PdLxZy0xhzasrrOGzAWzkfbX4ujrfSdO6qLD";
+
+  useEffect(()=>{
+    const fetchPhoto = () => {
+      axios
+      .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+        setPhoto(res.data)
+      })
+      .catch(err => {
+        console.log('the error is right before your eyes!')
+      })
+    } 
+    fetchPhoto();
+  }, []);
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <iframe src ={photo.url} width="100%" height="300"></iframe>
     </div>
   );
+
 }
 
 export default App;
