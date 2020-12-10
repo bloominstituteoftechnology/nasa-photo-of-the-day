12b985b95c25e309bf3e41f65f5cd3caf2d69a7b
Corie Stewart <coriestewart1989>
2020-12-10 09:14:45 -0500

    working on it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+const BASE_URL = ('https://api.nasa.gov/planetary/apod');
+const API_KEY = ('dvcz5lK4r0MvsoARAp7YadQBvxBDWj9kPxSADKtU');
+
 
 function App() {
+const[photoOfTheDay, setPhotoOfTheDay] = useState()
+  const fetchPhoto =() =>{
+    axios
+    .get(`${BASE_URL}?api_key=${API_KEY}&date=2020-12-08`)
+    .then((res) => {
+
+console.log(res.data)
+    })
+  }
+useEffect(() => {
+fetchPhoto()
+}, [])
   return (
     <div className="App">
       <p>
