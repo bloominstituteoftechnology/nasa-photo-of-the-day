4cc41bd43a07d81eaf8ec28f7ab6b2c2938d9bf5
Matthew Justice <matthewajustice@gmail.com>
2020-12-10 20:16:35 -0500

    build?

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Card, CardBody, CardTitle, CardText, CardHeader } from 'reactstrap';
 import CardImage from './cardimg';
