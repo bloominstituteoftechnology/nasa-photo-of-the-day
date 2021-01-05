3644a3d7a95d1e0e10123bd274859ab0638b1615
daniel-vazquez <da.vazquez728@gmail.com>
2021-01-05 00:55:09 -0700

    imported useState, useEffect, and axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+.then(success => console.log('successful',success))
+.catch(fail => console.log('get fail', fail))
+
+//nasa axios request:
+//https://api.nasa.gov/planetary/apod
+
+
+
+
 
 function App() {
+  
   return (
     <div className="App">
       <p>
---
+++ src/NasaPotd.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+
+
+export default function NasaPhotoImport() {
+    
+const [copyright, setCopyright] = useState([])
+const [date, setDate] = useState([])
+const [explain, setExplain] = useState([])
+const [title, setTitle] = useState([])
+const [url, setUrl] = useState([])
+
+    
+useEffect( () => {
+    axios   
+        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(success => {
+            setCopyright(success.data.copyright)
+            setDate(success.data.date)
+            setExplain(success.data.explanation)
+            setTitle(success.data.title)
+            setUrl(success.data.url)
+        })
+
+        .catch(fail => console.log('get fail', fail))
+
+    },[])
+
+
+    return (
+
+        <div className = 'photo-info'>
+            <h1>Welcome to the NASA Photo of the day for ${date}</h1>
+            <h3>Title: {title}</h3>
+            <img src= {url} />
+            <h3>Description:<h3>
+            <p>{explain}</p>
+
+            </div>
+        )
+
+    }
+
+
+
+
+
+
+
+
+
+}
