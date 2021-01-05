98a867293e3c2b104379502daa8aaef32d4f5a8c
daniel-vazquez <da.vazquez728@gmail.com>
2021-01-05 01:01:02 -0700

    fixed syntax errors on photo import component, added useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
+import PhotoImport from './NasaPotd';
 
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-.then(success => console.log('successful',success))
-.catch(fail => console.log('get fail', fail))
 
 //nasa axios request:
 //https://api.nasa.gov/planetary/apod
@@ -17,10 +15,7 @@ function App() {
   
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      
     </div>
   );
 }
--- src/NasaPotd.js
+++ src/NasaPotd.js
@@ -2,27 +2,27 @@ import React, { useState, useEffect } from "react"
 import axios from 'axios'
 
 
-export default function NasaPhotoImport() {
+export default function PhotoImport() {
     
-const [copyright, setCopyright] = useState([])
-const [date, setDate] = useState([])
-const [explain, setExplain] = useState([])
-const [title, setTitle] = useState([])
-const [url, setUrl] = useState([])
+const [copyright, setCopyright] = useState([]);
+const [date, setDate] = useState([]);
+const [explain, setExplain] = useState([]);
+const [title, setTitle] = useState([]);
+const [url, setUrl] = useState([]);
 
     
 useEffect( () => {
     axios   
         .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
         .then(success => {
-            setCopyright(success.data.copyright)
-            setDate(success.data.date)
-            setExplain(success.data.explanation)
-            setTitle(success.data.title)
-            setUrl(success.data.url)
+            setCopyright(success.data.copyright);
+            setDate(success.data.date);
+            setExplain(success.data.explanation);
+            setTitle(success.data.title);
+            setUrl(success.data.url);
         })
 
-        .catch(fail => console.log('get fail', fail))
+        .catch(fail => console.log('get fail', fail));
 
     },[])
 
@@ -33,20 +33,10 @@ useEffect( () => {
             <h1>Welcome to the NASA Photo of the day for ${date}</h1>
             <h3>Title: {title}</h3>
             <img src= {url} />
-            <h3>Description:<h3>
+            <h3>Description:</h3>
             <p>{explain}</p>
 
             </div>
         )
 
     }
-
-
-
-
-
-
-
-
-
-}
