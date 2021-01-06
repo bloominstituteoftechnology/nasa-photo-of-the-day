b61f3e1c8c38f92ee4ba86778904c1e1d8dc47af
Polly Yospan <porgroovy@gmail.com>
2021-01-06 02:13:11 -0500

    success fetching data

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
