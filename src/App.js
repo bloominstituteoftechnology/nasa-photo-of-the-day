51d3889b7fd0a1094f1b6f569fdf91f3bc9a15bf
Trevor Locke <locke.trevor@icloud.com>
2021-01-06 18:32:29 -0600

    MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import Header from './Header'
+import styled from 'styled-components'
+import Image from './Image'
 
 function App() {
+
+ const [ nasaPhoto, setNasaPhoto ] = useState();
+
+ useEffect(() =>{
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=UfbWWCZhdrghwIGgXzn0QCv4RMcjsn2jBUxk880J')
+  .then((res) =>{
+    console.log(res.data);
+    setNasaPhoto(res.data)
+  })
+  .catch(err => console.log(err))
+}, [])
+
+if (!nasaPhoto) return <h3>Loading...</h3>;
+
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+    <StyledApp className="App">
+    <Header />
+    <Image title={nasaPhoto.title} photo={nasaPhoto.url} date={nasaPhoto.date} details={nasaPhoto.explanation} />
+  </StyledApp>
+
+);
+
 }
+
+
+const StyledApp = styled.div`
+  color: white;
+  background-Color: #003366;
+`;
+
 
 export default App;
