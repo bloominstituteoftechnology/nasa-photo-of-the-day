bed6a4628369b2a05c63c6a5d4844910c3472bf3
Polly Yospan <porgroovy@gmail.com>
2021-01-07 22:39:16 -0500

    Successfully installed react strap into project

--- src/components/Apod.js
+++ src/components/Apod.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Copyright from "./Copyright";
 
 export default function Apod() {
   const [apod, setApod] = useState([]);
-  const { title, date, hdurl, explanation, copyright } = apod;
+  const { title, date, hdurl, explanation } = apod;
 
   useEffect(() => {
     axios
-      //.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-      .get(
-        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2021-01-05"
-      )
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
       .then((response) => setApod(response.data))
       .catch((err) => console.log(err));
   }, []);
-
-  function copy() {
-    apod.forEach((e) => {
-      if (e === "copyright") return <p>Copyright: {copyright}</p>;
-    });
-  }
 
   return (
     <div>
@@ -28,14 +20,14 @@ export default function Apod() {
       <h2>PICTURE OF THE DAY: {date}</h2>
       <img
         src={hdurl}
-        alt="NASA Astronomy Picture of the Day"
+        alt="NASA Astronomy of the Day"
         width="90%"
         height="600"
       />
       <div>
         <h3>Explanation</h3>
         <p>{explanation}</p>
-        {copy()}
+        <Copyright data={apod} />
       </div>
     </div>
   );
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Apod from "./Apod";
+import { Button } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
       <Apod />
+      <Button color="danger">Danger!</Button>
     </div>
   );
 }
