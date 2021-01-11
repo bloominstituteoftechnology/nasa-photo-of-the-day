cb35a6bb1f62a9d7014410487aaa82b05ddfa336
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-01-10 20:12:44 -0700

    Added two components, Header/Main - added styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+ import React, {useState} from "react";
+ import axios from 'axios';
+ import Header from './components/Header.js'
+ import Main from './components/Main.js'
 import "./App.css";
 
 function App() {
+axios.get('https://api.nasa.gov/planetary/apod?api_key=odfiFzCoKkOrwsmXGGjpc6pAiegaEJa5pI1dGAA8')
+.then(response => {
+  console.log(response.data);
+})
+.catch(error => {
+  console.log("Houston we have a problem")
+})
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
+      <Main />
+      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../App.css"
+
+export default function nav() {
+    return (
+        <div>
+            <h1>Astronomy Photo Of The Day</h1>
+        </div>
+    )
+}
