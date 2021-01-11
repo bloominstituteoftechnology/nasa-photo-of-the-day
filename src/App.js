4ef298d657459a9a29f3cccbf83490d813f5676d
bigbootygoblins <55123268+bigbootygoblins@users.noreply.github.com>
2021-01-10 23:25:56 -0800

    should be up to MVP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
-import "./App.css";
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+function apod(){
+    const [apod, setApod] = useState([])
+    useEffect(() => {
+    const fetchApod = (id) => 
+        fetch(`https://api.nasa.gov/planetary/apod`)
+            .then(response => response.json())
+            .then(apod => {
+                console.log(apod)
+                setApod(apod.id)
+            })
+            fetchApod(id)
+        }, [id])
+        return apod
 }
 
-export default App;
+ 
+
+function App({ id, apod }) {
+    const apod
+    <div>
+        <h3>{title.id}</h3>
+        <a href={hdurl.id}>
+        <img src={url.id} alt={media_type.id} width='100%' height='100%'/>
+        </a>
+        <p>{explanation.id}</p>
+
+    </div>
+
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import App from 'App.js'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
