cffc8f28339974781d2a7b2a07a45bb39338c601
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-01-11 21:56:36 -0700

    Project is complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
- import React, {useState} from "react";
+ import React, {useState, useEffect} from "react";
+ import Navbar from './components/Navbar.js'
+
  import axios from 'axios';
- import Header from './components/Header.js'
- import Main from './components/Main.js'
 import "./App.css";
 
 function App() {
-axios.get('https://api.nasa.gov/planetary/apod?api_key=odfiFzCoKkOrwsmXGGjpc6pAiegaEJa5pI1dGAA8')
-.then(response => {
-  console.log(response.data);
-})
-.catch(error => {
-  console.log("Houston we have a problem")
-})
+  const defaultData = {title: '', explanation: '', url: '' }
+  const [ data, setData ] = useState(defaultData)
+useEffect(()=> {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=odfiFzCoKkOrwsmXGGjpc6pAiegaEJa5pI1dGAA8')
+  .then(response => {
+    console.log(response.data);
+    setData(response.data)
+  })
+  .catch(error => {
+    console.log("Houston we have a problem")
+  });
+  
+}, [])
 
   return (
     <div className="App">
-      <Header />
-      <Main />
-      
+      <Navbar />
+      <br />
+   <h1>Astronomy Photo Of The Day</h1>
+      <br />
+      <iframe width="600" height="400" src={data.url}></iframe>
+      <h2>{data.title}</h2>
+      <p><strong>Date: </strong>{data.date}</p>
+      <p>{data.explanation}</p>
     </div>
   );
 }
