7b106028030f66bf6436a654de172992082d01b6
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-01-21 16:49:48 -0800

    implemented styling to componenets

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,20 +1,48 @@
 import React from "react";
 import "../../App.css";
+import styled from "styled-components"
 
 const Display = (props) => {
 
     const { data } = props;
 
     return(
-        <div className="display-container">
-            <div className="display-item">
-                <img alt="picture of the day" src={data.url}></img>
-            </div>
-            <div className="display-item"> {data.explanation} </div>
-        </div>
+        <StyledContainer>
+            <ImageContainer>
+                <Image alt="picture of the day" src={data.url}></Image>
+            </ImageContainer>
+            <TextContainer> {data.explanation} </TextContainer>
+        </StyledContainer>
     );
 };
 
 
 
 export default Display;
+
+const StyledContainer = styled.div ` 
+    background-color: #393B45;
+    color: white;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    
+`
+
+const ImageContainer = styled.div ` 
+        &:hover{
+            border: 2% solid;
+            border-color: #D8d9DE;
+            border-radius: 1%1%;
+            transition: all 0.5s ease-in-out;
+        };
+
+`
+const TextContainer = styled.div ` 
+    border: 2px solid white;
+    width: 60%;
+
+`
+const Image = styled.img ` 
+    height:70vh;
+`
--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React from "react"
 import "../../App.css"
+import styled from "styled-components"
+
 
 const HeaderBar = (props) => {
 
     const { data } = props;
 
     return(
-        
-        <div className="header-container">
-            <div className="header-item"> {data.title} </div>
-            <div> Astronomy Picture of the Day</div>
-            <div> {data.date} </div>
-        </div>
+        <StyledHeader>
+            <HeaderItem> title {data.title} </HeaderItem>
+            <HeaderItem> Astronomy Picture of the Day</HeaderItem>
+            <HeaderItem> date {data.date} </HeaderItem>
+        </StyledHeader>
     );
     
 };
@@ -19,3 +20,17 @@ const HeaderBar = (props) => {
 
 
 export default HeaderBar;
+
+const StyledHeader = styled.div `
+    background-color: #393B45;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    padding: 1% 5%;
+
+`
+const HeaderItem = styled.div `
+    font-size: 1.2rem;
+    font-weight: bold;
+    text-shadow: 3px 3px 5px #6E7889;
+`
