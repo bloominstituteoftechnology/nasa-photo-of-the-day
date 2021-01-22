29aa83519cd65fca68c20edac88b23869b1dc4dd
Kevin Klukowski <kklukowski12@gmail.com>
2021-01-21 20:14:19 -0500

    initial styling commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import styled, { keyframes } from 'styled-components';
 import "./App.css";
 import axios from 'axios'
 import GetDate from './Header';
 import desc from './imgFooter';
 import GetDescription from "./imgFooter";
+import { button, Card, CardBody, CardHeader } from 'reactstrap';
+//import { Img } from './styles.js';
 
 
 export const API_KEY = 'q15LIlf3Ltjx9gf81QlPn3DXbg7m4QzUCKfWf6L7';
@@ -29,10 +32,32 @@ useEffect(() => {
   return (
     <div className="App">
       <GetDate />
-      <img src={image} alt='of day'></img>
+      <Img src={image} alt='of day'></Img>
       <GetDescription />
     </div>
   );
 }
+//styles - until exporting works!!
+const kf = keyframes`
+  100%{
+    opacity: 1;
+    transform: scale(1)
+  }
+`
+
+const Img = styled.img`
+    max-width: 60%;
+    max-height: 50%fit-content;
+    animation: ${kf} 5s ease-in-out forwards;
+    box-shadow: inset 1 0 1px rgba(0,0,0,.8),
+                inset 1 2px 0 rgba(255,255,255,.5),
+                inset 1 -1px 0 rgba(0,0,0,.4);
+    border-radius: 20px;
+`
+
+
+
+
+
 
 export default App;
--- src/imgFooter.js
+++ src/imgFooter.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./App.css";
 import { URL, API_KEY } from './App';
+import { Button, Card, CardBody, CardHeader } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.css";
+//import { Button } from 'react-bootstrap';
+
 
 export default function GetDescription() {
 
@@ -20,8 +25,22 @@ export default function GetDescription() {
     return (
         <>
             <p>{likes}</p>
-            <button onClick={e => setLikes(likes + 1)}>Like</button>
-            <p>{details}</p>
+            <Button className='button' variant='danger' onClick={e => setLikes(likes + 1)} size='lg' active>Like</Button>
+            <Button className='button' variant='secondary' onClick={e => setLikes(likes - 1)} size='lg' active>Hate</Button>
+            <Card>
+                <CardHeader>Image Description</CardHeader>
+                <CardBody>
+                    <blockquote className='blockquote mb-0'>
+                        <p>
+                            {details}
+                        </p>
+                        <footer className='blockquote-footer'>
+                            Taken by NASA
+                        </footer>
+                    </blockquote>
+                </CardBody>
+            </Card>
         </>
     )
 }
+
