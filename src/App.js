1eae2271724033e0e5e343f3161c9f25f92586da
andrewroti <andrewroti@gmail.com>
2021-02-03 00:04:55 -0500

    added axios and called the nasa api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
+const apiURL ='https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
 
 function App() {
+  
+  
+  axios.get(apiURL)
+  .then(res =>{
+    let getPic = res.data.url;
+  })
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <p>The pic of the day is: 
+        <img src={getPic} alt=""></img>
+        </p>
+
     </div>
   );
 }
