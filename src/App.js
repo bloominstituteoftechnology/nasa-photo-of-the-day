8845ea221266e298f38e7cd7270e9edcdad778d4
andrewroti <andrewroti@gmail.com>
2021-02-03 00:46:34 -0500

    added video renderer aka ReactPlayer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
+import ReactPlayer from "react-player"
 
-const apiURL ='https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+
+const apiURL ='https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14'
 
 function App() {
+  const [currPic, setPic] = useState();
   
-  
-  axios.get(apiURL)
-  .then(res =>{
-    let getPic = res.data.url;
-  })
+ useEffect(()=>{
+   axios.get(apiURL)
+   .then(res=> {
+     setPic(res.data.url)
+    console.log(res.data.url)
+    
+    })
+   
+   .catch(err=> console.log(err));
+ }, []);
   return (
     <div className="App">
       <p>
@@ -18,8 +26,9 @@ function App() {
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
       <p>The pic of the day is: 
-        <img src={getPic} alt=""></img>
-        </p>
+       { currPic ? <img src={currPic} alt=""></img> :
+        <ReactPlayer url={currPic}/>}
+      </p>
 
     </div>
   );
