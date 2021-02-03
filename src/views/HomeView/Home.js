ca9e8cbcd4751df45f144cee94cc96d96466f60c
MaurooRen <maurooren@gmail.com>
2021-02-03 02:55:11 -0500

    fixing vertical aliment of Home View

--- src/views/HomeView/Home.js
+++ src/views/HomeView/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Nav from '../../components/Nav'
+//importing style
+import '../../styles/App.css'
 //importing nasa logo
+import logo from '../../assets/images/logo.png'
 
 const Home = () => {
     return (
@@ -8,8 +11,14 @@ const Home = () => {
             <Nav />
             <div className="hero">
                 <div className="brand">
-                    <img src={logo}>
-
+                    <img src={logo} alt='NASA Logo' />
+                    <h1>NASA OPEN APIs</h1>
+                </div>
+                <div className="title">
+                    <h2>
+                        <span>The sky is the limit,</span>
+                        <span><i>luckily</i> we go APIs</span>
+                    </h2>
                 </div>
             </div>
         </div>
