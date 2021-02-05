75a6e9c1ef8eedf44d93d046458262e50fc035f1
JuanCalixto <juancalixtomorales@gmail.com>
2021-02-04 21:50:22 -0600

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import Pic from './components/pic';
+import theme from './theme';
 import './App.css';
+import { ThemeProvider } from "styled-components";
 
 function App() {
-  // const [title, setTitle] = useState("");
-  // const [explanation, setExplanation] = useState("");
-  // const [url, setUrl] = useState("");
-  const [data, setData] = useState([]);
+  return (
 
-  useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=ue1vG6b3OZGEq6IEFNO4UimHRrjVhtieKcV2CHex')
-    .then((result) => {
-      setData(result.data);
-    }).catch((err) => {
-      console.log('Error')
-    }, []);
+    <div className="App">
+      <ThemeProvider theme={theme}>
+      <Pic />
+      </ThemeProvider>
+    </div>
 
-    return (
-      <div className="App">
-        <p>
-          Read through the instructions in the README.md file to build your NASA
-          app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-        </p>
-      </div>
-    );
-  }
+  );
+}
 
 export default App;
---
+++ src/theme/index.js
@@ -0,0 +1,7 @@
+
+export default {
+  primaryColor: 'wheat',
+  secondaryColor: 'black',
+  blackBorder: '2px solid #000',
+  wheatBorder: '3px solid wheat'
+}
