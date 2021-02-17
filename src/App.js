32a9b2fa8b77ffce3ec934e8753fda53f742f2be
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-17 13:20:10 -0800

    axios settup and state hook added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from 'axios'
 import APODData from './dummy-data/dummy'
 
 const API_KEY = `S4BSTHbyAzZp4XrreYaSUMkFEq1O7jPL0efEZT4B`;
+const baseURL = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`
 
 function App() {
+
+  const [currentDate, SetCurrentDate] = useState(baseURL);
+
+
+  useEffect(() =>{
+    const fetchPhoto = () =>{
+      axios.get(`${baseURL}`)
+      .then(res => {
+        debugger
+        SetCurrentDate(res.data)
+      })
+      .catch ( err =>{
+        debugger
+        console.log(err)
+      })
+    }
+    fetchPhoto();
+  },[])
+
   return (
     <div className="App">
       <p>
--- src/dummy-data/dummy.js
+++ src/dummy-data/dummy.js
@@ -2,16 +2,7 @@ export default [
     {
         "copyright": "Mike Cohea",
         "date": "2021-02-17",
-        "explanation": "This was not a typical sun pillar.  Just after sunrise two weeks ago in Providence, Rhode
-     Island, USA, a photographer, looking out his window, was suddenly awestruck. The astonishment was caused by 
-    a sun pillar that fanned out at the top.  Sun pillars, singular columns of light going up from the Sun, are t
-    hemselves rare to see, and are known to be caused by sunlight reflecting from wobbling, hexagon-shaped ice-di
-    sks falling through Earth's atmosphere.  Separately, upper tangent arcs are known to be caused by sunlight re
-    fracting through falling hexagon-shaped ice-tubes. Finding a sun pillar connected to an upper tangent arc is 
-    extraordinary, and, initially, took some analysis to figure out what was going on. A leading theory is that t
-    his sun pillar was also created, in a complex and unusual way, by falling ice tubes.  Few might believe that 
-    such a rare phenomenon was seen again if it wasn't for the quick thinking of the photographer -- and the came
-    ra on his nearby smartphone.    News from Mars: NASA Perseverance Coverage",
+        "explanation": "The explination",
         "hdurl": "https://apod.nasa.gov/apod/image/2102/SunPillar_Cohea_3400.jpg",
         "media_type": "image",
         "service_version": "v1",
