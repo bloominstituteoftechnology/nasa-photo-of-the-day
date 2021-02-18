9375e1df2f4ddae1b70413de09dbf0ec37a4edc0
Tarah <tarah.agbokhana@gmail.com>
2021-02-18 03:30:51 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -2,53 +2,45 @@ import React, {useState, useEffect} from "react";
 import './styles.less'
 import axios from "axios";
 import Details from './Details';
-import { BASE_URL, API_KEY, API_CODE} from './constants/index';
+import { BASE_URL, API_KEY} from './constants/index';
 
 
 function App() {
   const [photo, setPhoto] = useState([]);
-  const [dataId, setDataId] = useState(null);
-
-  const openDetails = (id) => {
-    setDataId(id);
-  };
-  const closeDetails = () => {
-    setDataId(null);
-  };
 
   useEffect(() => {
     const fetchPhoto = () => {
       axios
       .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
         .then((res) => {
-          setPhoto(res.data);
-         
+          setPhoto(res.data); 
         })
         .catch((err) => {
           console.log(err);
         });
     };
-    fetchPhoto ();
+    fetchPhoto();
    
   }, []);
 
   const Photo = (props) => (
     <div className="friend">
-      <img src={API_CODE} alt={photo.title} />
-      <button onClick={() => openDetails(photo.id)}>See details</button>
+      <a href = {photo.url} >
+        <img src={photo.url} alt={photo.title} />
+        </a>
     </div>
   );
 
   return (
     <div className="container">
+      <h1>Astro Photo of the Day!</h1>
+        <h2>{photo.title}:</h2>
       {
-        // If the initial value of `friends` state weren't an empty array,
-        // this would crash due to invoking `map` method on non-array.
-        // We'd need a guard against this.
+        
          <Photo key={photo.id} info={photo} />
       }
-      {dataId && (
-        <Details friendId={dataId} next={closeDetails} />
+      {photo && (
+        <Details data = {photo}  />
       )}
     </div>
   );
--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { BASE_URL, API_KEY } from "./constants/index";
+import { API_CODE } from "./constants/index";
 import axios from "axios";
 
 export default function Details(props) {
-    const { dataId, close } = props;
+    const { dataId} = props;
     const [details, setDetails] = useState(null);
 
     useEffect(() => {
@@ -20,7 +20,7 @@ export default function Details(props) {
 
     useEffect(() => {
     axios
-      .get(`${BASE_URL}/friends/${dataId}?api_key=${API_KEY}`)
+      .get(`${API_CODE}`)
       .then((res) => {
         setDetails(res.data);
       })
@@ -35,19 +35,19 @@ export default function Details(props) {
 
   return (
     <div className="container">
-      <h2>{details.title}:</h2>
+    
       {details && (
         <>
           <p>
            Published:  {details.date}  
           </p>
           <p>
-            Copyright by: {details.copyright}
+            <i>Source</i>: {details.copyright}
           </p>
           <p>{details.explanation}</p>
      </>
       )}
-      <button onClick={close}>Close</button>
+      
     </div>
   );
 }
