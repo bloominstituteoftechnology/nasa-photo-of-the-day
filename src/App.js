80cd978fd81d19cb3be4da6b39c433df71d08011
Tarah <tarah.agbokhana@gmail.com>
2021-02-18 01:26:45 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, {useState, useEffect} from "react";
+import './styles.less'
+import axios from "axios";
+//import Details from './Details';
+import { BASE_URL, API_KEY } from "../constants";
+
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  const [dataId, setDataId] = useState(null);
+
+  useEffect(() => {
+    const fetchPhoto = () => {
+      axios
+        .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+        .then((res) => {
+          setPhoto(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchPhoto();
+  }, []);
+
+
   return (
-    <div className="App">
+    <div className="container">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
