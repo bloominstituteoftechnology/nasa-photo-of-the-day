124b43612da920bc2c6f5bb017298a43f9e46fda
nickcabuno <nickcabuno17@gmail.com>
2021-02-18 04:00:40 -0500

    NASA

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from 'axios';
+
+
+// axios.get('https://api.nasa.gov/planetary/apod?api_key=kw4li6koQpnaZzFVA75n8NeQFl2B38hxVNrMCSLy')
+// .then(response => {
+//   console.log(response.data)
+// })
+// .catch(error => {
+//   console.log(error)
+// });
 
 function App() {
+  const [info, setInfo] = useState([])
+
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=kw4li6koQpnaZzFVA75n8NeQFl2B38hxVNrMCSLy')
+    .then((response) => {
+      setInfo(response.data);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1 id="h1">OUTLANDIA</h1>
+      <div className="picDiv">
+        <div className="pic">
+          <img className="picpic" src={info.url}></img>
+        </div>
+      </div>
+      <p> {info.explanation} </p>
+      <button onClick={() => (info.date)}>ANOTHER</button>
     </div>
   );
 }
