e0ec12836b909ce31a193e4eb62a02a551e050ef
ErikBurdett <erikaburdet@gmail.com>
2021-02-18 15:57:23 -0600

    Probably need to flex this unit lol

---
+++ src/Styles.js
@@ -0,0 +1,13 @@
+export default {
+    primaryColor: 'purple',
+    secondaryColor: 'crimson',
+    tertiaryColor: 'lime',
+    dangerColor: 'red',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)',
+    padding: {
+        small: '4px',
+        large: '12px'
+    }
+}
