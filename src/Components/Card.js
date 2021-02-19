b0ae5b7b0ccf2b28f0f83b9dc821e2d329f8313a
Bri <brittanymorris@MacBook-Pro.local>
2021-02-18 18:52:14 -0700

    I added styling to the image of the day by using styled-components.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+    img{
+    height: 75%;
+    width: 75%;
+    }
+`
 
 const Card = props =>{
     const {imgInfo} = props;
     console.log(imgInfo);
-
     return(
-        <div>
+        <StyledCard>
             <img src = {imgInfo.hdurl} />
-        </div>
+        </StyledCard>
     );
 }
 
