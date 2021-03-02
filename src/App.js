bde6e24ef470b417d6c60e7d632af6b2ac3fe790
josh jacques <jp.jacques@yahoo.com>
2021-03-02 12:16:49 -0600

    will work on adding components in a bit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL } from './constants/constants.js'
+import image from './assets/grapefruit-slice-332-332.jpg'
 
 function App() {
+  const [image, setImage] = useState();
+  const [explain, setExplain] = useState();
+  const [date, setDate] = useState();
+  const [title, setTitle] = useState();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+    .then(({ data }) => {
+      console.log(data)
+      setImage(data.url)
+      setExplain(data.explanation)
+      setTitle(data.title)
+      setDate(data.date)
+    });
+  }, []);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <img src={ image } />
+    <h2>{ title }</h2>
+    <p>{ date }</p>
+    <p>{ explain }</p>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=XwwfF2m5bLuTr1GJg6nHmtNRCwqFufYTAjIuehYQ';
