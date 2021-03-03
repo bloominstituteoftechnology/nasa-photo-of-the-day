704721f52add0ea05ee93860953692ddd0e67520
yonathan Admasu <admyoni4@yahoo.com>
2021-03-02 23:46:37 -0800

    made axios request and worked on useEffects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React  from "react";
 import "./App.css";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+  const [todayPhotos, setTodayPhotos] = useState([]);
+  useEffect(() =>{
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
+    .then(res =>{
+      setTodayPhotos(res.data.hdurl)
+
+    })
+    .catch(err => console.log('something went wrong'))
+  },[])
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <p>
