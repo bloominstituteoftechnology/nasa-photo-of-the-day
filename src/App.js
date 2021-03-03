51319f2bf7e49e67147dd8c5a9c3aad2331a9cbd
Lilia Ovcharenko <lilia.ovcharenko@gmail.com>
2021-03-02 23:46:23 -0800

    worked on project, need to finish it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import axios from 'axios'
 function App() {
+  const [imageOfToday, setImageOfToday] = useState([]);
+  useEffect(() =>{
+
+  axios.get (`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
+       .then(res =>(setImageOfToday(res.data.url)))
+       .catch(err => console.log('error'))
+
+  },[])
+
+
+
+
+
+
   return (
     <div className="App">
       <p>
