701186d30a8ee2952f2b6ef0618c4ce07df33d26
jbev <axe@Jake.local>
2021-03-02 20:51:08 -0700

    i am almost done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+const [dailyImage, setDailyImage] = useState(true);
+
+const BASE_URL = "https://api.nasa.gov/planetary/"
+const API_KEY = 'C1fqRgcG5cp1VtQ8rKju45mWqlLQfQ25X1kPLmuM'
+
+  useEffect(() => {
+axios.get(`${BASE_URL}apod?api_key=${API_KEY}`)
+.then(({hdurl}) => setDailyImage(hdurl))
+}, [dailyImage])
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun!
       </p>
+      <div role="img" aria-label='go!'>🚀 The daily image is: </div>
+      <img src={dailyImage} alt=''></img>
     </div>
   );
 }
