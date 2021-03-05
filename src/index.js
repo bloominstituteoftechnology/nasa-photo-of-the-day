2e2ea5d48efd7f9f441f6b62e760cf61138c7f98
Trevor Mandina <aurelion@Trevors-MBP.hitronhub.home>
2021-03-04 19:24:58 -0600

    did not finish

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+export const BASE_URL = 'http://localhost:3000/'
+export const API_KEY = 'QugCz7dP1gqY85emciSvSfBa9n6TWQweFKMTq3fl'
+
 ReactDOM.render(<App />, document.getElementById("root"));
