9a2d29552d7f3bb176dd9800be3e7a2974d77e44
alextran0899 <alextran@MacBook-Air.local>
2021-03-18 18:05:19 -0500

    alex-tran

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styled from 'styled-components'
 import "./App.css";
 import axios from"axios"
 import Photo from"./photo"
@@ -20,20 +21,59 @@ function App() {
       })
   }, [])
 
+
+  const Mystyle = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Mulish:wght@400;900&display=swap');
+  body
+  {
+    width: 80vw;
+  }
+  h1{
+    font-size:3rem;
+    font-weight:900;
+    font-family: 'Mulish', sans-serif;
+    margin:5%;
+   
+
+  }
+  .imgPara {
+    display:flex;
+  }
+  .imgPara img{
+      height: 100vh; 
+  }
+  .imgPara p{
+    font-size:1.5rem;
+    text-align:center;
+    margin:50px;
+    font-weight: 400;
+    font-family: 'Mulish', sans-serif;
+  }
+  @media screen and (max-width: 600px){
+  .imgPara {
+    flex-direction: column;
+  }
+  .imgPara img{
+    height: auto; 
+    width:100vw;
+}
+}
+  `
+
   return (
-    <div className="App">
-      <header> 
-        <Title data={details}/>
-      </header>
+    <Mystyle className="App">
       <div className="imgPara">
         <Photo help={details}/>
+        <div>
+        <Title data={details}/>
         <Para data={details}/>
-      </div>
-      <footer>
         <Footer data={details}/>
-      </footer>
+        </div>
+     
+      </div>
       
-    </div>
+    </Mystyle>
   
   );
 }
