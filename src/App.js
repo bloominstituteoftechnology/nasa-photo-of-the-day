46cb543b640df698c4bb129d20df0a19417b30be
Jonathan Garcia <70542639+CoderJonathan@users.noreply.github.com>
2021-03-17 20:03:15 -0700

    DayOne MVP Complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+import POD from './POD.js'
 
 function App() {
+  const [picture, setPicture]= useState([])
+useEffect(( )=> {
+const data = axios.get('https://api.nasa.gov/planetary/apod?api_key=0HiOdkPqAbX6r4SgVCG5CsviJfkGcsBvWwm0xgkl').then((response)=> {console.log(response) 
+setPicture(response.data)}).catch((error )=> { console.log(error)})
+console.log(data)
+},[])
+
   return (
     <div className="App">
-      <p>
+      {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      </p> */}
+      <POD 
+      key={picture.id}
+      date= {picture.date}
+      explanation={picture.explanation}
+      title={picture.title}
+      url={picture.url}
+      copyright={picture.copyright}
+      />
     </div>
   );
-}
+} 
 
 export default App;
