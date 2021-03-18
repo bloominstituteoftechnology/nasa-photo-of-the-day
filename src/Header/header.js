464a2da83e3596e22873de7409340e651a50fec4
Zachary Cooremans <zcooremans@gmail.com>
2021-03-18 14:38:40 -0500

    day2 submission

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+    background-color: black;
+
+    div{
+        color:white;
+    }
+    h1{
+        background-color: black;
+    }
+`
 function Header () {
     return (
-        <div>
+        <StyledContainer>
             <div>
                 <h1>Astronomy Picture of the Day</h1>
                 <a href="archivepix.html">Discover the cosmos! </a>
                 Each day a different image or photograph of our fascinating universe is
                 featured, along with a brief explanation written by a professional astronomer.
             </div>
-        </div>
+        </StyledContainer>
         
     )
 }
