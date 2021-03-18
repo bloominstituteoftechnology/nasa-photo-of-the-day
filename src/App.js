fb2e8962ef72f885067a6c612b9366b83211ab34
Timothy Langham <graniteblood@gmail.com>
2021-03-18 17:49:00 -0400

    day 2 begun

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ import "./App.css";
 function App() {
   const [data, setData] = useState({})   // data is the declaration of an empty object that will be filled with the information from the API request below, and the master App Component will return the invocations of other components below... setData changes what is inside data, once it is fetched from the API, or if there is an event. 
   
+const baseURL = 'https://api.nasa.gov/planetary/apod?api_key=1XcrC3xroozQArT3QJTKfIW0acN4yfDqhwHVqWZm'
 // below is the 'axios call' to fetch the API with .get - .then, it will respond, by binding the Object Keys recieved from the API, to the setData Object.
   useEffect(() => {  // declare and invoke useEffect, which is a function
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=1XcrC3xroozQArT3QJTKfIW0acN4yfDqhwHVqWZm")  // API request from axios
+    axios.get(`${baseURL}`)  // API request from axios
       .then((res) => {   // once the API is recieved, respond with the function of setData's.data object
         setData(res.data)
         console.log(res.data)  // to show the object's keys and values to use/access throughout your work
@@ -24,10 +25,13 @@ function App() {
       })
   }, [])
   
-  // below is the return of all the components linked to this App. The invocations are in <JSX /> form, and must declare the function needed =equal to= the API Keys' Value you want to use. 
+  // below is the return of all the components linked to this App. The invocations are in <JSX /> form, and must declare the prop needed =equal to= the API Keys' Value you want to use. 
 
   // all of the components need to be inside an <>empty</> div so that all of the components are returned inside on div itself, the App's output MUST be ONE thing. It can only return ONE THING.
 
+  //    🌸 SYNTAX  STRUCTURE 🌸   //
+  // <Summary summary = {data.explanation}/>
+  // JSX Component
   return (
     <div className = "App">
     <Title title = {data.title}/>
