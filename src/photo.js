f4ce8affc642f8f0ad85748f473de77b6e550a0e
Daniel Brannon <daniel-brannon@hotmail.com>
2021-03-17 19:29:14 -0500

    ok really done now

--- src/photo.js
+++ src/photo.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from"react"
 
-const help = (props) => {
-
-    const {help} = props
-    // console.log(data)
-    return (
-            <img src={help.hdurl} alt=''></img>
-        )
-    
+const help = (props) =>{ 
+const {help} = props
+return (
+    <div>  
+    <header> 
+      <h1>{props.title}</h1>
+    </header>
+        <img src={help.hdurl} alt ='space'></img>
+    </div>
+)
 }
 
 export default help
