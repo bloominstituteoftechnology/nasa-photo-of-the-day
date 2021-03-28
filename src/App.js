b1a3079bce75f37f2dd96abdfcda2908eb27135e
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-03-28 12:51:05 -0400

    need to figure out how to find the current day. also purpose fo teh project? image url doesnt work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   // fetch will go here
-  const [data, setData] = useState(fetch('https://api.nasa.gov/planetary/apod?api_key=hbQeB0ODFwlwO5hu3xLYgmNfwQicougVUBPwVIdx')
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=hbQeB0ODFwlwO5hu3xLYgmNfwQicougVUBPwVIdx';
+  const APIResponse = fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
-  )
+    .then(data => console.log(data));
+  console.log(Date.now());
+  const [data, setData] = useState(APIResponse)
+  // useEffect(
+  //   setData(
+  //     fetch(url)
+  //       .then(response => response.json())
+  //       .then(data => console.log(data))
+  //   ), [Date.day]
+  // )
   return (
     <div className="App">
+      <img src={data.url}></img>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
