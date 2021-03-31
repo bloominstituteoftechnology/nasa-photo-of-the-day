700cfff142aa26fc91a98cade64c3f08a70e56ba
vida <ofo28@aol.com>
2021-03-31 01:12:23 -0400

    finish for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from  'React';
 import "./App.css";
+import Gallery from './gallery'
+import Header from './header'
+import Details from './details'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([])
+
+
+  useEffect(()=> {
+    axios
+    .get(`https://apod.nasa.gov/apod/image/2103/M87bhPolarized_Eht_3414.jpg`)
+    .then(res => {
+      setData(res.data)
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <Header headerData={data}/>
+      <Gallery imgData={data}/>
+      <Details info={data}/>
+
     </div>
   );
 }
---
+++ src/details.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+	import './details.css'
+	
+
+	const Details = (props) => {
+	    const {info} =props;
+	    return(
+	        <div className='details-container'>
+	            <p>
+	                {info.explanation}
+	            </p>
+	        </div>
+	    )
+	}
+	
+
+	export default Details;
