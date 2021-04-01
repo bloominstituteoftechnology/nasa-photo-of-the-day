76744cbfa0b84e558f47c9baa628a52020d09a0d
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-01 11:49:31 -0400

    confusing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
-  // fetch will go here
+
   const url = 'https://api.nasa.gov/planetary/apod?api_key=hbQeB0ODFwlwO5hu3xLYgmNfwQicougVUBPwVIdx';
-  async function getData(url) {
+  const getData = async (url) => {
     try {
       const res = await axios.get(url);
       const picture = await res.data;
-      return picture;
+      setData(picture);
     } catch(err) {
       console.log(err)
     }
   }
-  const APIResponse =  getData(url);
-  const [data, setData] = useState(APIResponse)
-  // this is a use effect that changes the picture when the day changes
-  // useEffect(
-  //   setData(
-  //     fetch(url)
-  //       .then(response => response.json())
-  //       .then(data => console.log(data))
-  //   ), [Date.day]
-  // )
+  const [data, setData] = useState();
+  useEffect(() => {
+    getData(url);
+  }, []);
   console.log(data);
   return (
     <div className="App">
-      <img src={data.url} alt="picture"></img>
+      <img href={data.url}></img>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
