ac8a7ffcf35a1a0b2fb3aaf979db8de204e6dc1d
unknown <twessar42@gmail.com>
2021-04-04 00:06:52 -0700

    mvp complete and super happy

--- src/components/PhotoOfTheDay/PhotoOfTheDay.js
+++ src/components/PhotoOfTheDay/PhotoOfTheDay.js
@@ -1,17 +1,23 @@
 /** @format */
 
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+import DescriptionPopover from './DescriptionPopover/DescriptionPopover';
 import MediaPlayer from './MediaPlayer/MediaPlayer';
 
 function PhotoOfTheDay(props) {
 	const { date, description, title, url, mediaType } = props;
 
 	return (
-		<section className='photoComponentParent'>
-			<h1>{title}</h1>
-			<h2>{date}</h2>
-			<MediaPlayer title={title} url={url} mediaType={mediaType} />
-			<p>{description}</p>
+		<section>
+			<Jumbotron fluid>
+				<Container fluid>
+					<h1 className='display-3'>{title}</h1>
+					<h2 className='lead'>{date}</h2>
+					<MediaPlayer title={title} url={url} mediaType={mediaType} />
+					<DescriptionPopover description={description} title={title} />
+				</Container>
+			</Jumbotron>
 		</section>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /** @format */
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
