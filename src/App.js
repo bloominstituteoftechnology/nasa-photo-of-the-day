c806392d866b043dcd49da33548e8f778389b6a9
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-14 16:28:44 -0400

    added state to app.js for the data ill be getting from NASA

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    
   );
 }
 
