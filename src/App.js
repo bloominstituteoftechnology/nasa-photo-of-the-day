f06dc631e742b52b01ca1d402f6080bfe4e9ab88
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-14 17:01:06 -0600

    updates to files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import header from './header';
-import body from './body';
+import Header from './Components/header';
+import Body from './Components/body';
+import axios from 'axios';
 
 function App() {
+const [nasaData, setNasaData] = useState([]);
+const [title, setTitle] = useState([]);
+const [pic, setPic] =useState([]);
+const [discription, setDiscription] = useState([]);
+useEffect(() => {
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=MjOoXMh2ahiorGCM7YMmKlPo88UBehPSatVjdv8i")
+  .then((res) => {
+    setTitle(res.data.title);
+    setPic(res.data)
+    setDiscription(res.data.url);
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}, []);
+
   return (
     <div className="App">
       <p>
