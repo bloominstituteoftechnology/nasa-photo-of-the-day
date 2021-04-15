d33a90e9da889e5175efe871bdf3177a71c7a60d
Zachary Cooremans <zcooremans@gmail.com>
2021-04-15 16:22:13 -0500

    mvp day2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,26 @@
 import React from 'react'
-
+import styled from 'styled-components'
 export default function Header(props) {
     const {data} = props
-    
+
+    const StyledContainer = styled.div`
+        background-color: black;
+
+        div{
+            color:white;
+        }
+        h1{
+            background-color: black;
+        }
+    `
     if (!data) return <h3>Loading...</h3>;
     return(
-        <>
+        <StyledContainer>
             <h1>Astronomy Picture of the Day</h1>
             <a href="archivepix.html">Discover the cosmos! </a>
             Each day a different image or photograph of our fascinating universe is 
             featured, along with a brief explanation written by a professional astronomer.
             <p>{data.date}</p>
-        </>
+        </StyledContainer>
     )
 }
