d599811e914174912406b89a833e3b6647a1a3d2
cynthiaannberg <cynthia.ann.berg@me.com>
2021-04-14 21:44:24 -0600

    nasa

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
+
 function App() {
+  const [dataSet, setDataset] = useState(null)
+  const [photo, setPhoto] = useState(null)
+
+  useEffect(function(){
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=J1HZTloMkDyltYkA5IkQUYsKarYaqq3qrqwYWCvk')
+    .then(function(res){
+      setDataset(res.data)
+      setPhoto(res.data.hdurl)
+      console.log(res.data.hdurl)
+    })
+    .catch(function(err){
+      console.log(err)
+    })
+  })
+
+  console.log(dataSet)
+
+  const NasaImage = (props) => (
+    <div>
+      <img src = {props.photo} alt='alt'></img>
+    </div>
+  )
+
+
   return (
     <div className="App">
+      <NasaImage photo={photo}/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
