979daf20a897eac1372cb68a6003ee5035ce0e03
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-15 17:10:16 -0400

    populated theme object

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,14 @@
 //Theme 
 
-export default = {
-    
+export default {
+    primaryColor: '#301b3f',
+    secondaryColor: '#3c415c',
+    tertiaryColor: '#b4a5a5',
+    black: '#151515',
+    whitw: '#f8f5f1',
+    padding: {
+        small: '4px',
+        medium: '8px'
+    }
+
 }
