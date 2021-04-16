68c2e0e95602e545e55b6b1049bc975ec91283a5
mariusmoldovan <biris_21@yahoo.com>
2021-04-15 22:42:24 -0500

    day 2 finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { API_CODE, BASE_URL, API_KEY  } from "./components/Url";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
-import Url from "./components/Url"
-import Details from "./components/Details"
+import Details from './Details';
+import { BASE_URL, API_KEY} from './constants/index';
+import Header from './Header';
+import Footer from './Footer';
+import styled from 'styled-components';
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const fetchPhoto = () => {
       axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
         .then((res) => {
           setPhoto(res.data); 
         })
@@ -24,15 +24,94 @@ function App() {
     fetchPhoto();
    
   }, [photo.id]);
-  return (
-    <div className="App">
+
+  const PhotoFrame = styled.div`
+    padding: 10px;
+  `;
+
+  const PhotoImg = styled.img`
+    display: block;
+    margin: 0 auto;
+    width: 93%;
+    box-shadow: 0px 1px 6px -2px slategray;
+    border: 2px solid #94cbe2;
+    padding: 10px;
+  `;
+
+  const Photo = (props) => (
+    <PhotoFrame>
+      <a href = {photo.url} >
+        <PhotoImg src={photo.url} alt={photo.title} />
+        </a>
+    </PhotoFrame>
+  );
+
+  const Container = styled.div`
+    width:90%;
+    margin: 0 auto;
+    border: 1px solid rgb(210, 210, 210);
+    box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+    background-color: #78e08f;
+  `;
+ 
+ const Content = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+const H2 = styled.h2`
   
-     
+    font-family:  sans-serif;
+    font-weight: #636e72;
+    color: #0984e3 ;
+    background-color: #ffeaa7;
+`;
+
+const H3 = styled.h3`
+
+    font-family: sans-serif;
+    font-weight: bold;
+    padding: 0 25px 0 25px;
+    text-align: center;
+  `;
+
+const Blockquote = styled.blockquote`
+    border: 1px solid slategray;
+    width: 50%;
+    background-color: #e17055;
+    font-weight: bold;
+    font-size: 1rem;
+`;
+
+const Styled = styled.div`
+  color: red;
+`
+
+
+  return (
+    <div>
+    <Header />
+    <br></br>
+    <Container>
+      <Content>
+      <H2>Astro Photo of the Day!</H2>
+      <Blockquote> 
+         <H3>	{photo.title}:</H3>
+      </Blockquote>
+      {
+        <Photo key={photo.id} info={photo} />
+      }
+
+      {photo && (
+        <Details data = {photo}  />
+      )}
+      </Content>
+        </Container>
+        <br></br>
+        <Footer />
     </div>
   );
 }
-
-
-
 
 export default App;
--- src/components/Url.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'https://api.nasa.gov'
-export const API_KEY = 'DEMO_KEY'
-export const API_CODE = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
-
