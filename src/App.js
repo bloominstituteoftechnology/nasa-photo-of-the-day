eb56532ef48b8a70bc9cbd68e5e2e5c30e0c142e
Jake Linn <jake_linn@Jakes-MacBook-Air-2.local>
2021-04-27 19:56:34 -0400

    mvp day 2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,48 @@ import React from "react";
 import "./App.css";
 import Header from './Header'
 import Photo from './Photo'
+import styled from 'styled-components'
+
+const HeaderDiv = styled.div `
+
+color: blue;
+font-family: courier-new;
+padding: 20px;
+
+
+`
+
+const StyledApp = styled.div `
+
+background-color: gray; 
+
+padding: 185px;
+
+
+`
+const StyledPhoto = styled.div `
+
+color: #f7b508;
+font-family: courier-new;
+padding: 20px;
+word-spacing: 5px;
+
+
+`
+
+
 
 function App() {
   return (
     <div className="App">
+      <StyledApp>
+      <HeaderDiv>
       <Header/>
+      </HeaderDiv>
+      <StyledPhoto>
       <Photo/>
-
+      </StyledPhoto>
+    </StyledApp>
     </div>
   );
 }
