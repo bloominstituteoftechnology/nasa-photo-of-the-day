90fe8228982dadae6aa43d138c002fbc54bba53b
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-28 18:10:36 -0500

    running into erro 429 going to step back from it again

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import axios from 'axios'
-import Picture from '../src/components/Picture'
+import axios from 'axios';
+// import Picture from '../src/components/Picture';
 
 function App() {
-  const [data, setData] = useState('')
-  
-  useEffect( ()=>{
-    axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then(res =>{
-      console.log(res)
-      setData(res.data)
-    })
-    .catch(err =>
-      console.log(err))
-    },[])
-  
-  
-  
+  const [data, setData] = useState({});
+
+  axios
+  .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(res =>{
+    console.log(res)
+  })
+  .catch(err =>{
+    console.log('this is the error', err)
+  })
+
   return (
     <div className="App">
       <h1> Nasa OPAD</h1>
       <h4>Made by: Steve Rivera</h4>
-      <Picture data ={data}/>
+      {/* <Picture data ={data}/> */}
     </div>
       );
 
-  }
+  };
 export default App;
