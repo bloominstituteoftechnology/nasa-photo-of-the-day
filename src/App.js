8ec35f25a1824e26725b99fcbfde37cebba56a46
Aidan Jenkins <aidan.jenkins@gmail.com>
2021-04-27 19:36:50 -0700

    finish project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import "./App.css";
+import axios from 'axios';
+import Media from './components/Media/Media'
 
 function App() {
+
+  const [apod, setApod] = useState({})
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+          .then(response => {
+            console.log(response)
+            setApod(response.data)
+          })
+          .catch(error => console.error("failed to get profile info:", error))
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="container">
+        <h1 className="pageTitle"> NASA Picture of the Day</h1>
+        <Media pod={apod} />
+        <h3 className="mediaTitle">{apod.title}</h3>
+        <p className="date">{apod.date}</p>
+        <p className="explanation">{apod.explanation}</p>
+      </div>
     </div>
   );
 }
