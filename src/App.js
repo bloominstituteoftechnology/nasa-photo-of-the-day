3a6d0ac7d538887c4d85c32c0261e60a0b8e998a
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-28 00:34:00 -0500

    i feel like i am getting the hang of it, but it still does not make sense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState({})
+  
+  useEffect(()=>{
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res=>{
+      console.log(res)
+      setData(res.data)
+      
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  },[])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+      <h1>
+        Nasa OPAD
+      </h1>
+      <h4>
+        Made by: Steve Rivera
+      </h4>
+      <div className="title">{data.title}
+        <div className="data">{data.date}
+          <div className="hdurl">{data.url}</div>
+        </div>
+      </div>
+
+
+      </div>
+      
+   
   );
 }
 
