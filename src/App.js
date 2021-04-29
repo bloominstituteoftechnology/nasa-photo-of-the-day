5eb51ce8e05b23e35d889b5a515cf551a50e05b3
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-29 12:34:07 -0500

    it works, it is alive!!!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
-// import Picture from '../src/components/Picture';
+import Picture from '../src/components/Picture';
 
 function App() {
   const [data, setData] = useState({});
 
-useEffect(()=>{
-  getNasaPicture()
-  
-  async function getNasaPicture(){
-    try{
-      const {data} = await axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  useEffect(()=>{
+    const getData = () =>{
+          axios
+              .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+                  .then(res =>{
+                    console.log(res)
+                    setData(res.data)
+              })
+                  .catch(err =>{
+                    console.log(err)
+              })
+
     }
-  }
-})
+    getData()
+  },[])
 
   return (
     <div className="App">
       <h1> Nasa OPAD</h1>
       <h4>Made by: Steve Rivera</h4>
-      {/* <Picture data ={data}/> */}
+      <img src={data.url} alt="there should be space here!"/>
     </div>
       );
 
