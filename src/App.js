e6560e83a3e1677dc3521eaea80290c4cb65bf97
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-29 12:37:15 -0500

    finally understand how to do this project. i can get the information from the api, and I have learned to display it on to the browser

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
-import Picture from '../src/components/Picture';
+
 
 function App() {
   const [data, setData] = useState({});
@@ -27,6 +27,9 @@ function App() {
       <h1> Nasa OPAD</h1>
       <h4>Made by: Steve Rivera</h4>
       <img src={data.url} alt="there should be space here!"/>
+      <p>{data.title}</p>
+      <p>{data.date}</p>
+      <p>{data.explanation}</p>
     </div>
       );
 
