6bbad2bd31ccbe192c2a798cb36c96f9d428e812
tamara-taylor <ttaylor8522@gmail.com>
2021-04-29 20:15:02 -0400

    finished build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [photo, setPhoto] = useState([])
+
+  useEffect(() => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=HNNHuGBqSW7LisA8zWx06wMMUvfrwJZYIVhCLlxk')
+      .then(({photo}) => setPhoto(photo))
+      .catch(err => console.log(err))
+  }, [])
+  
+  
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Photo of the Day</h1>
+      <img src={photo.url} alt="Nasa All Day!"/>
+      <p>{photo.title}</p>
+      <p>{photo.date}</p>
+      <p>{photo.explanation}</p>
+      <h4>Tamara Taylor</h4>
     </div>
   );
 }
