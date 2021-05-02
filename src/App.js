abbba63b62f0c98a40231035e37cbc81331e3e26
Mallory <mallorydoescoding@gmail.com>
2021-05-01 20:31:07 -0500

    done, I finished project and for some reason it would not let me push so I had to create a whole new file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getData = () => {
+      axios 
+        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+          .then (res => {
+            console.log(res)
+            setData(res.data)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+
+    }
+    getData()
+  },[])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Photo of the Day: Presented by NASA</h1>
+      <img src = {data.url} alt = 'Nasa photo of the day, it is probably space'/>
+      <h3>{data.title}</h3>
+      <p>{data.date}</p>
+      <p>{data.explanation}</p>
+      <h4>Created by Mallory Hall</h4>
     </div>
   );
 }
