6cac54b16efb4ac21d264c3065eb5aad06ae1981
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-05-02 19:55:36 -0500

    I styled the project using css inside the app.js, I could not get the reactstrap to work, however, I am not giving up until i do

--- src/App.js
+++ src/App.js
@@ -1,38 +1,59 @@
 import React, {useState, useEffect} from "react";
-import "./App.css";
 import axios from 'axios';
+import { Card, CardBody, CardTitle, CardImg, CardText } from "reactstrap";
 
+const card={
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  backgroundColor: '#8576cf'
+}
+
+const title={
+  color: 'white',
+  fontSize: '30px',
+  padding: '10px'
+}
+
+const text={
+  color: 'white',
+  fontSize: '20px',
+  padding: '10px'
+}
+
+const img={
+  width: '800px',
+  height: '600px',
+  padding: '10px'
+
+}
 
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({})
 
   useEffect(()=>{
     const getData = () =>{
-          axios
-              .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-                  .then(res =>{
-                    console.log(res)
-                    setData(res.data)
-              })
-                  .catch(err =>{
-                    console.log(err)
-              })
-
+      axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => 
+        {
+          setData(res.data)
+        })
+      .catch(err => {console.log(err)})
     }
     getData()
   },[])
 
-  return (
-    <div className="App">
-      <h1> Nasa OPAD</h1>
-      <h4>Made by: Steve Rivera</h4>
-      <img src={data.url} alt="there should be space here!"/>
-      <p>{data.title}</p>
-      <p>{data.date}</p>
-      <p>{data.explanation}</p>
-    </div>
-      );
-
-  };
+return (
+  <Card style={card}>
+    <CardBody>
+      <CardTitle style={title}>{data.title}</CardTitle>
+      <CardText style={text}>{data.date}</CardText>
+      <CardImg  src={data.url} alt='nasa opad' style={img}/>
+      <CardText style={text}>{data.explanation}</CardText>
+    </CardBody>
+  </Card>
+)
+}
 export default App;
