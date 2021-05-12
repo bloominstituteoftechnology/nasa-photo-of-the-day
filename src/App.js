c37d5db0705390a070f1d0e96670d49d31c2b01c
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-12 09:42:33 -0500

    built out scaffolding for App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+// import component
+// import component
+
+const App = () => {
+
+  // State slice hooks
+  // const [] = useState()
+  // const [] = useState()
+  // const [] = useState()
+  
+  // GET https://api.nasa.gov/planetary/apod
+  // axios
+  //   .get("https://api.nasa.gov/planetary/apod")
+  //   .then((res) => {
+
+  //   })
+  // .catch((err) => {
+  //   debugger
+  // })
+  
+  // };
 
 function App() {
   return (
     <div className="App">
       <p>
+        {/* add child components and props to inject in each appropriate child(ex = <Pictures picture={picture}/>) */}
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
   );
-}
+};
+
 
 export default App;
