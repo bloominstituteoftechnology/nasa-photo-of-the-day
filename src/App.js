ec1c6751b4959b77ee40b691de31916e5455bad8
Williamlarose <38011333+Williamlarose@users.noreply.github.com>
2021-05-12 18:20:58 -0400

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-
+import Head from "./compoenets/header"
 function App() {
   return (
     <div className="App">
+      <Head />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+
       </p>
     </div>
   );
---
+++ src/compoenets/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Head = function (props) {
+    return (
+        < div >
+            < h1 > Behold Tomorrow</h1 >
+            <div>
+                <p>Nasa Photo Of The day</p>
+            </div>
+        </div >)
+}
+
+export default Head;
