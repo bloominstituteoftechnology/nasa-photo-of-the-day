94395d97d13439860623330351b297b288660218
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-12 15:25:55 -0500

    added header section to app. added image date and details below image header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return (
 
         <div>
-            
+            <h1>NASA Photo of the Day</h1>
         </div>
 
     )
--- src/components/Posts/PostDetails.js
+++ src/components/Posts/PostDetails.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const PostDetails = props => {
 
-    const {   } = props;
+    const { date, explanation } = props;
 
     return (
-
         <div>
-            
+            <div>
+                <p>Date: {date}</p>
+            </div>
+            <div>
+                <p>{explanation}</p>
+            </div>
         </div>
 
     )
