da9b4441b96528bdd233599f3627a0161abffc5e
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-12 12:58:26 -0600

    getting app set up with axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(()=> {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=MjOoXMh2ahiorGCM7YMmKlPo88UBehPSatVjdv8i")
+    .then((res)=>{
+      setNasaData(res.data);
+      console.log(res.data);
+    })
+    .catch((err)=> {
+      console.log(err);
+    })
+  }, [])
   return (
     <div className="App">
       <p>
