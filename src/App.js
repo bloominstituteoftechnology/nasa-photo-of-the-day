7571291902c6500f46a0794389bdf4d1944bc583
davedwight <dpdwight@gmail.com>
2021-05-13 16:14:06 -0600

    Added reactstrap button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import axios from 'axios';
 import Image from './Image';
 import styled from 'styled-components';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
 
 const Container = styled.div`
   color: #33322f;
@@ -16,6 +17,8 @@ const Container = styled.div`
 function App() {
   
   const [nasaData, setNasaData] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   useEffect(() => {
     axios
@@ -35,6 +38,17 @@ function App() {
   return (
     <Container>
       <h1>Nasa Photo of the Day</h1>
+      <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Choose Date</Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+            Anim pariatur cliche reprehenderit,
+           enim eiusmod high life accusamus terry richardson ad squid. Nihil
+           anim keffiyeh helvetica, craft beer labore wes anderson cred
+           nesciunt sapiente ea proident.
+          </CardBody>
+        </Card>
+      </Collapse>
       <Image src={nasaData.url} alt={nasaData.title} />
     </Container>
   );
