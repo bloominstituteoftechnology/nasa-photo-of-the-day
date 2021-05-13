146d44b509289095f17d87a01732bb19e73b8179
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-13 14:21:12 -0500

    added initial styled header

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from 'styled-components';
 
+import theme from './theme/index';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import styles from './index.css';
+
+ReactDOM.render(
+<ThemeProvider theme={theme}>
+    <App />
+</ThemeProvider>, 
+document.getElementById("root"));
---
+++ src/theme/index.js
@@ -0,0 +1,13 @@
+export default {
+    primaryColor: 'royalblue',
+    secondaryColor: 'crimson',
+    tertiaryColor: 'lime',
+    dangerColor: 'red',
+    black: 'black',
+    white: 'white',
+    breakPointMobile: '(max-width: 550px)',
+    padding: {
+        small: '4px',
+        large: '12px'
+    }
+}
