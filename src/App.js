e3eedb6ed84ce759516c24515d15fde71803a8ee
ferengitooth <ehitsamill@gmail.com>
2021-05-12 17:02:36 -0700

    inject title skeleton into app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import Title from './Components/Title'
 
 function App() {
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=8DzApwJb2t9VBex2dKLyY7FqiPVYR8ImjjanFyVy")
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  },[])
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Title />
     </div>
   );
 }
---
+++ src/Components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div>
+            <h1>Nasa Photo/Video Of The Day!</h1>
+        </div>
+    )
+}
+
+export default Title;
