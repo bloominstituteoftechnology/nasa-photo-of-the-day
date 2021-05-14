31fa0d043d954b2913c34655082fc2c0cfb1a648
Williamlarose <38011333+Williamlarose@users.noreply.github.com>
2021-05-13 20:40:34 -0400

    Style

---
+++ src/compoenets/theme/theme.js
@@ -0,0 +1,11 @@
+
+
+export default {
+
+    primaryColor: 'goldenrod',
+    secondaryColor: 'snow',
+    tertiaryColor: 'darkcyan',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)',
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from "styled-components"
+import themeobj from "./compoenets/theme/theme"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <ThemeProvider theme={themeobj}>
+        <App />
+    </ThemeProvider >, document.getElementById("root"));
