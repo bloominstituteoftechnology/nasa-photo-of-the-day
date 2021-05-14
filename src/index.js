ac11dd92e6c751355567fcdca746a4674733f268
Amanuel Gilay <amanuelgilay@Amanuels-MacBook-Pro.local>
2021-05-13 22:40:04 -0700

    finished nasa style

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ThemeProvider } from 'styled-components'
+import theme from './theme/style'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(<ThemeProvider theme ={theme}><App /></ThemeProvider>, document.getElementById("root"));
+
---
+++ src/theme/style.js
@@ -0,0 +1,18 @@
+export default {
+    primaryColor: 'crimson',
+    secondaryColor: 'royalblue',
+    tertiaryColor: '#154360',
+    backgroundColor:'#E5E7E9',
+    backgroundColor2:'#AED6F1',
+    dangerColor: 'red',
+    black: 'black',
+    white: 'white',
+    breakPointMobile: '(max-width: 550px)',
+    firstSize: '20px',
+    thirdSize:'28px',
+    secondSize: '40px',
+    padding: {
+        small: '4px',
+        large: '12px'
+    }
+}
