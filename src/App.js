69db5d5421b4c5730844cbebfe0db501a86e2ba3
Kyle <kyle.e.cardwell@gmail.com>
2021-05-25 19:26:35 -0600

    layed out initial html & design

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="container">
+        <div className="nasaBox">
+          <div className="nasaInfo">
+            <h1>
+              NASA ASTRONOMY PHOTO OF THE DAY
+            </h1>
+            <h2 className="nasaDate">
+              5/26/21
+            </h2>
+            <p className="photoExp">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta expedita harum quae dolores aspernatur quod molestiae blanditiis dolorum sint, praesentium reprehenderit incidunt libero officiis aliquam facere debitis esse error? Consequuntur!
+            </p>
+          </div>
+          <div className="nasaPhoto">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
