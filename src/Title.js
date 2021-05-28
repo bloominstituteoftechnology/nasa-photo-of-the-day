567b03ae2164bf09961b0393683254743132cdd5
Cynthia Coronado <cynthia.softball12@gmail.com>
2021-05-27 20:34:23 -0700

    Syling completed

--- src/Title.js
+++ src/Title.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledTitle = styled.div`
+font-size: 30px;
+text-decoration: underline 5px;
+color: #474044;
+`
 function Title() {
     return (
-        <div className='titleDiv' >
-            <h1>Rachel's NASA Photo of the Day!</h1>
-        </div>
+        <StyledTitle >
+            <h1>Rachel's NASA Photo of the Day!<br></br>June 21st, 1958 - February 24th, 2017</h1>
+        </StyledTitle>
     )
 }
 export default Title;
