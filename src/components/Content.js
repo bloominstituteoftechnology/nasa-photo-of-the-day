0ccb7fd1bb642fb53fb0fb067b171e16320e42da
quinn5moli <quinn.moli@gmail.com>
2021-05-27 21:04:37 -0700

    mvp

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledContent = styled.div`
+color: ${(props) => props.theme.primaryColor};
+&:hover {
+    color: ${(props) => props.theme.dangerColor};
+}
+    button {
+        background-color: #f5f5dc
+    }`
 
 const Content = (props) => {
     const {info, title, date} = props;
 
     return (
+       
         <container>
             <h2>{title}</h2>
             <h3>{date}</h3>
             <p>{info}</p>
         </container>
+        
     )
 }
 
