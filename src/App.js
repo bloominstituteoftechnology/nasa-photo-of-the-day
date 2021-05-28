44f8eaace27e3a994cfcdaa59018b01b3f52d2ce
Aaron <aarongabriel147@gmail.com>
2021-05-27 20:43:26 -0700

    almost

--- src/App.js
+++ src/App.js
@@ -5,11 +5,38 @@ import Info from './components/Info.js'                   // When Linking compon
 import styled from 'styled-components'
 
 // Component Style. (npm install styled-components) & (import styled from 'styled-components')
+// Never nest defined component styles inside React components. Keep them outside of the components.
+// TERMONOLOGY: Define styles, not declare styles!
+// You can import styles.
 
-const StyledDiv = styled.div`
+const StyledH1 = styled.h1`
   color: dodgerblue;
   font-size: 3rem;
+  
 `;
+
+const StyledDiv = styled.div`
+  background-color: lightgrey;
+  
+
+`;
+
+
+const H1Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  // border: 2px solid red;
+`;
+
+const IMGwrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  // border: 3px solid blue;
+`;
+
+
+
+//_____________________________________________________________________________________
 
 // Parent
 export default function App(){
@@ -17,7 +44,7 @@ export default function App(){
   const [apodData, setApodData] = useState({})            // Since we set an object, we create an empty object, although other things usually work fine. 
   console.log(apodData)                                   // Always console.log to see what is going on.
 
-  useEffect(() => {                                       // This makes the data fetch occur once rather than on every state change.                     
+  useEffect(() => {                                       // This makes the data fetch occur once rather than on every state change. Because of [].                    
     axios
       .get('https://api.nasa.gov/planetary/apod?api_key=I7FKEo5cyioKDlwxTjWorNCaEg8P0SWvuZkXMoNN') // This is where we get our data from.
       .then(res => {
@@ -26,16 +53,39 @@ export default function App(){
       .catch(err => {
         console.log('ERR')
       })
-  }, [])                                                  // [] Makes the useEffect occur once.
+  }, [])                                                  // [] Makes the useEffect occur once. Dependency array.
 
   return (
-    <>
-      <div>
-        <StyledDiv>NASA Picture of the Day</StyledDiv>
-      </div>
-      <img src={apodData.url} alt={apodData.title}/>
+    <StyledDiv>
+      <H1Wrapper>
+        <StyledH1>NASA Picture of the Day</StyledH1>
+      </H1Wrapper>  
+      <IMGwrapper>
+        <img src={apodData.url} alt={apodData.title}/>
+      </IMGwrapper>  
+      
       
       <Info apodData={apodData}/>  {/* Key value pair, & props is the object name in the child. Also, where you place the line matters, just like in HTML.*/}
-    </>
+
+    </StyledDiv>
   );
 }
+
+
+//_________________________________________
+
+// Scraps:
+
+// display: flex;
+// flex-direction: column;
+
+// flex-wrap: wrap;
+// min-height: 100vh;
+// align-items: center;
+// justify-content: space-evenly;
+// color: white;
+// &:hover {
+//   color: silver;
+//   transition: 5s;
+//   background-color: #1D2951;
+// }
