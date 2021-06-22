e94cbd362fab1d6662783bad9cc048c47322a657
Collin-Falstad <wildkard91@gmail.com>
2021-06-22 16:31:27 -0400

    defined state, added API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState()
+  const [date, setDate] = useState()
+  const [data, setData] = useState()
+  const [title, setTitle] = useState()
+  const [info, setinfo] = useState()
+ 
+
+
+  // the API call
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(({data}) => setPhoto(data))
+    .catch(err => console.log('Error getting picture of the day, API limit? ', err))
+  }, [])
+
+
+
+
   return (
     <div className="App">
       <p>
