d979a43b1904b97a32094b4b998df7044a7d24f8
Owen  Damron <damronowen@yahoo.com>
2021-06-22 22:31:27 -0500

    Owen-Damron

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios'
 import "./App.css";
 
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'DEMO_KEY'
+
 function App() {
+
+  const [picOfDay, setPicOfDay] = useState([])
+
+   useEffect(() => {
+
+     const getPicOfDay = () => {
+      axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res)
+        setPicOfDay(res.data)
+      })
+  
+      .catch(err => {
+        console.log(err)
+      })
+  
+     }
+    getPicOfDay()
+
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+          <p>Nasa Photo Of The Day!</p>
+          <p>{picOfDay.title}</p>
+          <p>{picOfDay.date}</p>
+          <p>{picOfDay.explanation}</p>
     </div>
   );
 }
