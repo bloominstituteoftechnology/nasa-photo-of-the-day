3d9570f91e1cadf2fb761abf0da3d83ff1c52fca
Dalian <DalianAlyxander@me.com>
2021-06-23 02:07:48 -0400

    app.js and about.js components built

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import { BASE_URL, api_key } from './nasa'
+import AboutInfo from "./aboutInfo";
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+
+  
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}${api_key}&date=2012-03-14`)
+    .then(({data}) => setNasaData(data))
+    .catch(err => console.log('Error getting photo of the day: ', err))
+  }, [])
+
+  
+  const picOfTheDay = nasaData.url
+
+ 
+
+ console.log(nasaData)
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1><span role="img" aria-label="stars emoji">✨</span> Welcome Stargazer <span role="img" aria-label="rocket emoji">🚀</span></h1>
+      <p className='subtitle'>NASA photo of the day</p>
+      <img src={picOfTheDay} alt="NASA pic of the day"></img>
+        {<AboutInfo />}
+      {/* <p className='pic-info'>{title}</p>
+      <p>{info}</p>
+      <p>{date}</p> */}
+      
+      {/* <NasaData date={date} info={info}/> */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/nasa.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key='
+export const api_key = '2x0wustjP4GxugDl0cQqTW1YhCNede1svwgJGThF'
