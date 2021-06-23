872618537121a398eaa0af563ea5d77e9d111d2c
Trevor Locke <locke.trevor@gmail.com>
2021-06-22 21:26:39 -0500

    finished page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios' 
+
 
 function App() {
+  handleClick = () => {
+   console.log('clicked')
+  };
+  const [photoData, setPhotodata] = useState(null);
+  useEffect(() => {
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=EYpMqg3J4DtJJuJfOR7VyJDVWOY3O6kq1iGlRtV5')
+    .then((res) =>{
+      setPhotodata(res.data)
+    })
+    .catch((err) =>{ 
+      console.log(err)
+    });
+  },[])
+  if(!photoData) return <div><h1>Loading....⏳⏳⏳</h1></div>;
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{photoData.title}</h1>
+      <img src={photoData.hdurl} alt="potd" />
+      <p>{photoData.explanation}</p>
+      <button onClick={handleClick}>Get todays photo!!</button>
     </div>
   );
 }
