ef047b67a63ff922eb412b6093b0831fda156f70
jlbevans <jacob-bevans@lambdastudents.com>
2021-06-22 21:05:55 -0600

    i have completed the assignment, for some reason my axios gave out.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
+
+  const [picture, setPicture] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(({data}) => setPicture(data))
+    .catch(err => console.log('Error getting friends: ', err))
+  }, [])
+
+  console.log(picture)
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <h2>{picture.date}</h2>
+        <span role="img" aria-label='go!'>🚀{picture.hdurl}</span>
+        <h3>{picture.title}</h3>
+        <p>{picture.explanation}</p>
       </p>
     </div>
   );
