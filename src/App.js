550cf5c35e32946c3eba3f6344601b6d365743bb
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-23 13:22:43 -0500

    setting up the request.js to be able to handle axios request and display information to page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Request from './Request'
 
 function App() {
   return (
--- src/Request.js
+++ src/Request.js
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 //this module will hold axios request and it is the parent of wireframe component
 
-useEffect(() => {
-	axios
+export default function Request() {
+const [photo, setPhoto] = useState([])
+	useEffect(() => {
+		axios
 		.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 		.then(res => {
 			console.log(res)
+			setPhoto(res.data)
 		})
 		.catch(err => {
-		console.log(err)
-	})
-}, [])
+			console.log(err)
+		})
+	}, [])
+
+	return (
+		<p>
+			{ photo.date } 
+		</p> 
+	)
+}
 //the empty array makes sure that the use effect only calls the api once
+
+
