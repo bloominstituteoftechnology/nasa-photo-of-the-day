cace99b310a4bd7a9b5d900dc9ae9fab1e66b835
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-23 14:00:04 -0500

    working on rendering wireframe, got distracted with styling

---
+++ src/WireFrame.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './WireFrame.css'
+
+const WireFrame = props => {
+	return (
+		<div className="container">
+			<div className="header">
+				<img src="https://gpm.nasa.gov/sites/default/files/NASA-Logo-Large.jpg" alt="Nasa Logo" className="nasa-logo" />
+				<p className="coder">Steve Rivera</p>
+			</div>
+			<div className="app-body">
+				
+			</div>
+		</div>
+	)
+}
+
+export default WireFrame
