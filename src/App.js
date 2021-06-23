e5cb3a7d72ad042d63ee58a42df2458326f21de2
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-23 13:00:05 -0500

    set up axios request in new Request module

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <Request />
     </div>
   );
 }
---
+++ src/Request.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+//this module will hold axios request and it is the parent of wireframe component
+
+useEffect(() => {
+	axios
+		.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+		.then(res => {
+			console.log(res)
+		})
+		.catch(err => {
+		console.log(err)
+	})
+}, [])
+//the empty array makes sure that the use effect only calls the api once
