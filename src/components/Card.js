73382b91b0f8b7bae956ad09681204fe530046e4
Mykael Barnes <mykael.barnes@hotmail.com>
2021-06-22 18:17:10 -0700

    added components

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardHeader from './components/CardHeader'
+
+const Card = props => {
+
+  return (
+    <div className='card-border'>
+      <CardHeader/>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/CardHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CardHeader = () => {
+
+  return (
+    <div className='card-header'>
+        <h1>Astronomy Photo Of The Day</h1>
+    </div>
+  );
+};
+
+export default CardHeader;
