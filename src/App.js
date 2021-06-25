59c6af9cff37d9c4aa1d312fbd4153813cc8cac1
Owen  Damron <damronowen@yahoo.com>
2021-06-24 21:36:45 -0500

    Finished Assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios'
+import {Card, Col} from 'reactstrap'
 import "./App.css";
 
 export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
@@ -8,6 +9,16 @@ export const API_KEY = 'DEMO_KEY'
 function App() {
 
   const [picOfDay, setPicOfDay] = useState([])
+
+  const colorStyling = {
+    backgroundColor: 'royalblue',
+    color: 'White'
+  }
+
+  const img = {
+    maxHeight: 600,
+    maxWidth: 400
+  }
 
    useEffect(() => {
 
@@ -29,10 +40,15 @@ function App() {
 
   return (
     <div className="App">
-          <p>Nasa Photo Of The Day!</p>
-          <p>{picOfDay.title}</p>
-          <p>{picOfDay.date}</p>
-          <p>{picOfDay.explanation}</p>
+      <Card style={colorStyling}>
+        <Col sm = "12" md = {{size: 6, offset: 3}}>
+          <h1>Nasa Photo Of The Day!</h1>
+          <img src={picOfDay.url} alt='Newly Discovered Space Anomaly' style={img}/>
+          <h2>{picOfDay.title}</h2>
+          <h3>{picOfDay.date}</h3>
+          <p>{picOfDay.explanation}</p>         
+        </Col>
+      </Card>          
     </div>
   );
 }
