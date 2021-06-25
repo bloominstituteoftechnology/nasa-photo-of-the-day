7692e2f7c82227f287aec87b70f492b07ec44891
jlbevans <jacob-bevans@lambdastudents.com>
2021-06-24 20:25:06 -0600

    i have updated the page

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import "./App.css";
 function App() {
 
   const [picture, setPicture] = useState([])
+  const [mainData, setMainData] = useState([])
 
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-    .then(({data}) => setPicture(data))
+    .then((res) => setPicture(res.data.url))
     .catch(err => console.log('Error getting friends: ', err))
   }, [])
 
-  console.log(picture)
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then((res) => setMainData(res.data))
+    .catch(err => console.log('Error getting friends: ', err))
+  }, [])
 
   return (
     <div className="App">
       <p>
-        <h2>{picture.date}</h2>
-        <span role="img" aria-label='go!'>🚀{picture.hdurl}</span>
-        <h3>{picture.title}</h3>
-        <p>{picture.explanation}</p>
+        <h1>Nasa Update Page!</h1>
+        <h2>{mainData.date}</h2>
+        <span role="img" aria-label='go!'>🚀</span>
+        <iframe width="800" height="550" src={picture}></iframe>
+        <h3>{mainData.title}</h3>
+        <p>{mainData.explanation}</p>
       </p>
     </div>
   );
