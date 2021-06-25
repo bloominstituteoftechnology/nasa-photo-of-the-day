7103562c6ccb49115561f7efbcc4dc65e0309db1
unknown <j.ordonez419@gmail.com>
2021-06-25 00:01:29 -0500

    mvp complete for both projects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import Photo from './components/Photo'
+import { Button } from 'reactstrap';
+import Example from './components/Example'
 
 function App() {
+
+  const [photo, setPhoto] = useState([])
+  let today = new Date().toISOString().slice(0, 10)
+
+  useEffect(() => {
+    console.log(today)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=gGa6hgmeKLSEJWTyugiafAgj9sY8H6esdgAYbM6P&date=2021-06-24`)
+      .then(({ data }) => setPhoto(data))
+      .catch(err => console.log(err))
+    return () => console.log('Cleaning up')
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Photo photo={photo} title={photo.title} description={photo.explanation} date={photo.date} imageURL={photo.url} />
     </div>
+
   );
 }
+
 
 export default App;
