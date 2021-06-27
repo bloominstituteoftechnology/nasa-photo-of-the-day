bacff5a3381e66554560446e8f8630e78b0c26bb
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-26 22:40:55 -0500

    working on CardImg and subtext

--- src/WireFrame.js
+++ src/WireFrame.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import './WireFrame.css'
+import {Toast, Card, CardTitle, CardImg} from 'reactstrap'
 
 const WireFrame = props => {
 	return (
-		<div className="container">
-			<div className="header">
-				<img src="https://gpm.nasa.gov/sites/default/files/NASA-Logo-Large.jpg" alt="Nasa Logo" className="nasa-logo" />
-				<h2>Nasa Photo of the Day</h2>
-				<p className="coder"> <span>Made by:</span> Steve Rivera</p>
-			</div>
-			<div className="app-body">
-				<div className="title-header">
+		<div>
+			<Toast style={{
+				margin: '0 auto'
+			}}>
+					<img src="https://gpm.nasa.gov/sites/default/files/NASA-Logo-Large.jpg" alt="Nasa Logo" className="nasa-logo" />
+				<Toast>
+					<h2>Nasa Photo of the Day</h2>
+					<p className="coder"> <span>Made by:</span> Steve Rivera</p>
+				</Toast>
+			</Toast>
+
+			<Card>
+				<CardTitle className="title-header">
 						<div className="title">{props.photo.title}</div>
+				</CardTitle>
 						<div className="date">{props.photo.date}</div>
-				</div>
-				<div className="photo-body">
-					<img src={props.photo.url} alt="nasa apod" className="photo" />
+				<CardImg style={{ width: '50%', height: '100%', margin: '0 auto'}} src={props.photo.url} alt="nasa apod" className="photo" />
 					<p className="photo-explanation">{props.photo.explanation}</p>
-				</div>	
-				</div>
+				
+				</Card>
 			</div>
 		
 	)
