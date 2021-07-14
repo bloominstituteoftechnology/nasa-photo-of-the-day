c6ad67efb7ec42c11f502774858e46cb3119aeae
Klove Adams <kalebsxott@gmail.com>
2021-07-14 16:33:07 -0400

    starting for real

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?date=2012-03-14`)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => console.log(err))
+  }, []);
+
   return (
     <div className="App">
       <p>
