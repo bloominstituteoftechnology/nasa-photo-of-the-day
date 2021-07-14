2efe4756e265605904a8d7b933302c0b88432a42
Klove Adams <kalebsxott@gmail.com>
2021-07-14 17:40:41 -0400

    Image done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Image from "./components/Image"
 import "./App.css";
+const dummyData = {
+  "copyright": "Alan FriedmanAverted Imagination",
+  "date": "2012-03-14",
+  "explanation": "It's one of the baddest sunspot regions in years. Active Region 1429 may not only look, to some, like an angry bird -- it has thrown off some of the most powerful flares and coronal mass ejections of the current solar cycle. The extended plumes from these explosions have even rained particles on the Earth's magnetosphere that have resulted in colorful auroras. Pictured above, AR 1429 was captured in great detail in the Sun's chromosphere three days ago by isolating a color of light emitted primarily by hydrogen. The resulting image is shown in inverted false color with dark regions being the brightest and hottest. Giant magnetically-channeled tubes of hot gas, some longer than the Earth, are known as spicules and can be seen carpeting the chromosphere. The light tendril just above AR 1429 is a cool filament hovering just over the active sunspot region. As solar maximum nears in the next few years, the increasingly wound and twisted magnetic field of the Sun may create even more furious active regions that chirp even more energetic puffs of solar plasma into our Solar System.   Gallery: See images of the latest planetary conjunction",
+  "hdurl": "https://apod.nasa.gov/apod/image/1203/angrysun_friedman_1080.jpg",
+  "media_type": "image",
+  "service_version": "v1",
+  "title": "Angry Sun Erupting",
+  "url": "https://apod.nasa.gov/apod/image/1203/angrysun_friedman_960.jpg"
+}
 
 function App() {
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
+
+  
+  useEffect(() => {
+    setData(dummyData)
+    console.log(data)
+  }, [])
 
   // useEffect(() => {
   //   axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
@@ -16,10 +33,7 @@ function App() {
 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Image data={data}/>
     </div>
   );
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,3 @@
 import React from "react";
 
-const Image = props => {
-}
+export default ({data}) => <img src={data.url}/>;
