986b45907e100d35c47d2e3a230a477ebcdf1b6d
Klove Adams <kalebsxott@gmail.com>
2021-07-14 16:35:45 -0400

    endpont working with console log

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState({})
 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?date=2012-03-14`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14`)
     .then(res => {
       console.log(res.data)
     })
