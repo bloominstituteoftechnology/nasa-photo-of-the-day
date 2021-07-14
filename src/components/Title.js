9ed89b6652e4e7d967ce909a0019cd7a21cb84e9
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-07-14 16:47:32 -0700

    commit

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,7 @@ const Title = (props) => {
     <h1>{props.title}</h1>
     <h2>{props.date}</h2>
     <h3>{props.explanation}</h3>
-
+    <a href={props.src}>{props.src}</a>
     </div> )
 }
 export default Title;
