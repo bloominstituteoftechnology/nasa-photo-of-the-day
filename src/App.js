b7f07c832535b542a431728b9bc44d95cc3c00d5
Rick <rmansfield@realpeoplesratings.com>
2021-07-14 17:43:08 -0500

    U2.2M3 nasa set up constants >> index.js & imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL, API_KEY} from './constants/index';
 
 function App() {
   return (
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = '';
+export const API_KEY = '';
