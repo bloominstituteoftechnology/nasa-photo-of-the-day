60e1066d77e33d363f686c8db89af1b3b2e5cfe2
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-07-14 16:39:27 -0700

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios'
+import Title from './components/Title'
+import NasaPhoto from './components/NasaPhoto'
+import Explanation from './components/Explanation'
 
 function App() {
+const[title, setTitle] = useState([])
+const[date,setDate] = useState([])
+const[explanation,setExplanation] = useState([])
+const[img, setImg] = useState("")
+
+  const takeNewData = () =>{
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=WscU2prYd7VBNSf7t33LfWOLSQscDvTlHrHcSoVB
+    `)
+    .then((res) =>{
+      console.log(res.data)
+      setTitle(res.data.title)
+      setDate(res.data.date)
+      setExplanation(res.data.explanation)
+      setImg(res.data.url)
+
+
+    })
+    .then(() =>{
+      console.log(title)
+      console.log(date)
+      console.log(img)
+
+
+    })
+    .catch(err => console.log(err))
+  }
+  useEffect(() => {
+   takeNewData()
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Title title={title} date={date} explanation={explanation} src={img}/>
+        
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'; 
+
+const Title = (props) => {
+    return (<div className="App">
+    <h1>{props.title}</h1>
+    <h2>{props.date}</h2>
+    <h3>{props.explanation}</h3>
+
+    </div> )
+}
+export default Title;
