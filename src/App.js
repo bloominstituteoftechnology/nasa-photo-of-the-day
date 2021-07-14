055ab4c8af7fc922b314eb8785e1a65bf438e93f
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-07-14 17:30:55 -0400

    Adding current changes, adding date changer next commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+import Image from "./Components/Image";
+import Video from "./Components/Video";
+
 function App() {
+  const [apodData, setApodData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=cPnxYU2eBFaVE84dw9qlg6kWLIjYaVwMOSeSbIbK"
+      )
+      .then((response) => {
+        console.log(response.data)
+        setApodData(response.data);
+      });
+  }, []);
+
+  function changeDateBackwards(date) {
+    // Based on current date go back one day
+  }
+
+  function changeDateForwards(date){
+    // Based on current date go fowards one day
+  }
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="header-container">
+        <div className="header-left">
+          <img id="header-logo" src="NASA.png" alt="NASA Logo" />
+          <h1>
+            {" "}
+            Picture of <br /> The Day
+          </h1>
+        </div>
+        <div className="header-right">
+          <h1>Gallery</h1>
+        </div>
+      </div>
+
+      <div className="media-container">
+        {apodData.media_type === "photo" ? (
+          <Image data={apodData} />
+        ) : (
+          <Video data={apodData} />
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Image.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Image(props) {
+  const { title, explanation, date , url } = props.data;
+
+  if (!url) return <h3> Loading... </h3>;
+
+  return (
+    <div style={{ display:'flex', width: "100%", padding: '2vh 2vw' }}>
+    <div style={{width: '50%', padding:'25px'}}>
+     <img src={url} alt="of the day"/>
+     </div>   
+
+   <div style={{width: '50%', padding:'25px'}}>
+     <h2>{title}</h2>
+     <h3>{date}</h3>
+     <p>{explanation}</p>
+   </div>
+   </div>
+  );
+}
+
+export default Image;
