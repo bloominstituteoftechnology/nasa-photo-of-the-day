21cef2ef2ded5ef1ff67ad73733bafaddde2b960
umer-kaiser <umerkaiser@yahoo.com>
2021-07-14 21:34:44 -0700

    nasa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
 import "./App.css";
 
+import React, { useEffect, useState } from "react";
+
+import Header from "./components/Header";
+import Photo from "./components/Photo";
+import Title from "./components/Title";
+import axios from "axios";
+
 function App() {
+  const [title, setTitle] = useState("")
+  const [url, setURL] = useState("")
+
+  // fetch api using fetch
+  // useEffect(() => {
+  //   fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").then(res => res.json()).then(res => {
+  //     setState({url: res.url})
+  // }).catch(err => console.log(err))
+  // }, [])
+
+  // fetch api using axios
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").then(res => {
+      console.log('data: ', res)
+      setTitle(res.data.title)
+      setURL(res.data.url)
+  }).catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Header />
+      <Photo url={url} />
+      <Title title={title}/>
     </div>
   );
 }
-
-export default App;
