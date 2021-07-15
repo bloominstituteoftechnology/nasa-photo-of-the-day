02827c1b341766959f610655fe58e84db6f0c84e
Bimisola Daniel <bimisoladaniel@Bimisolas-MacBook-Air.local>
2021-07-15 06:50:43 +0100

    I'm still working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+useEffect(() => {
+  axios
+    .get("https://api.nasa.gov/planetary/apod")
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}, []);
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
