8e25b85d74c550bb220ed304ef373781158e295f
Bimisola Daniel <bimisoladaniel@Bimisolas-MacBook-Air.local>
2021-07-15 15:42:12 +0100

    I'm still working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-useEffect(() => {
-  axios
-    .get("https://api.nasa.gov/planetary/apod")
-    .then(res => {
-      console.log(res.data);
-    })
-    .catch(err => console.log(err));
-}, []);
 function App() {
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(res => {
+        console.log(res.data);
+        setLightOn(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  const [lightOn, setLightOn] = useState();
+
   return (
     <div className="App">
       <p>
