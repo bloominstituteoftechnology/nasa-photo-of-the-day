1fa181d684914fde5c4946deed7f2638c50f5e9f
justinfineberg <justinf1818@gmail.com>
2021-08-11 12:40:07 -0400

    more changes to the style

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import Main from './Main'
 import axios from 'axios'
 
 
+
 function App() {
   const [image, setImage] = useState(null)
+  const [caption, setCaption] = useState('')
   useEffect(()=>{
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(res=>{
       setImage(res.data.url)
+      setCaption(res.data.explanation)
     })
     .catch(err=>{
       console.log(err);
     })
   },[])
 
+  const pStyle = {
+    width: '70%',
+    border: '1px solid black',
+    margin: '0 auto',
+    marginBottom: '50px'
+  }
+  
   return (
     <div className="App">
       <h1>Nasa Photo of the Day</h1>
+      <p style={pStyle}>{caption}</p>
       <Main image={image} />
     </div>
   );
