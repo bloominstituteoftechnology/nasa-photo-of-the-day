903aca435157f6b95312a533a698672bf7f2f431
Leo Hays <starryhornet@gmail.com>
2021-08-12 18:21:49 -0500

    space launch/picture

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import "./App.css";
 import axios from 'axios';
 import { BASE_URL, API_KEY } from './api.js/index';
 import Heading from './Heading/Heading'
+import Paragraph from './Paragraph/Paragraph'
 
 function App() {
   const [error, setError] = useState(null)
   const [title, setTitle] = useState(null)
   const [img, setImg] = useState(null)
   const [copyright, setCopyright] = useState(null)
-  const [head, setHead] = useState([])
-  
-
-
+  const [paragraph, setParagraph] = useState(null)
+  const [date, setdate] = useState(null)
   
   useEffect(() => {
     axios.get(`${BASE_URL}?api_key=${API_KEY}`)
@@ -20,21 +19,20 @@ function App() {
       setImg(res.data.hdurl)
       setTitle(res.data.title)
       setCopyright(res.data.copyright)
+      setParagraph(res.data.explanation)
+      setdate(res.data.date)
     })
     .catch(err => {
       console.error(err)
-      setError('ERROR CODE 51 Technical Dificulties')
+      setError('ERROR CODE 51')
     })
   })
 
   return (
     <div className="App">
       {error && <h1>{error}</h1>}
-      <Heading title={title} img={img} copyright={copyright} />
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Heading title={title} img={img}  />
+      <Paragraph date={date} paragraph={paragraph} copyright={copyright}/>
     </div>
     
   );
