1a2c6f39f5268d2660404dcef66e335c3f5de74c
Monica Salas <monicasalas@Monicas-Air-2.attlocal.net>
2021-08-12 20:50:11 -0500

    Fetched data from NASA APOD API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [dailyPic, setDailyPic] = useState('');
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=BZR4pKnRyIJnc6h8hTbL9z45MturqMMxyGLA7ovR`)
+      .then(res => setDailyPic(res.data.hdurl))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className='App'>
+      <h1>NASA Picture of the Day</h1>
+      <img src={dailyPic} alt='NASA APOD'/>
     </div>
   );
 }
