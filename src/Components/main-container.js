97549a5d1294be2d7856b3f4c76c6bbe18bb8c8e
Monica Salas <monicasalas@Monicas-Air-2.attlocal.net>
2021-08-12 21:40:32 -0500

    main-container page frame

---
+++ src/Components/main-container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    width: 90%;
+    height: 100%;
+`;
+
+function Container () {
+    return (
+        <div className='main-container'>
+            
+        </div>
+    );
+}
