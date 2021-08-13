897ef31da288b483386c30c8efc31745239d0740
Monica Salas <monicasalas@Monicas-Air-2.attlocal.net>
2021-08-13 01:06:19 -0500

    added and sized image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import styled from 'styled-components';
-// import imgDate from './Components/image-info';
+import Date from './Components/imgDate';
 import axios from "axios";
 
 
 const Page = styled.div`
   width: 90vw;
 `
+const NasaImg = styled.img`
+    width: 300px;
+    height: 300px;
+    `
 
 function App() {
   const [NasaData, setNasaData] = useState('');
+  const [DailyPic, setDailyPic] = useState('');
 
   useEffect(() => {
     axios
@@ -18,16 +23,21 @@ function App() {
       .then(res => {
         console.log(res.data)
         setNasaData(res.data)
+        setDailyPic(res.data.hdurl)
       })
       .catch(err => {
         console.log(err)
       })
   }, [])
 
+  
+
   return (
     <Page className='App'>
       <h1>Nasa Photo of the Day</h1>
-      
+      <Date date={NasaData.date}/>
+      <NasaImg src={DailyPic} alt='NASA APOD'/>
+
     </Page>
   );
 }
