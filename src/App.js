ccc66a4db6eeedf92ddc991bff8656847a66eb5d
nickcabuno <nickcabuno17@gmail.com>
2021-08-24 02:31:28 -0400

    '.'

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React, {useEffect, useState} from "react";
 import "./App.css";
 import axios from 'axios';
 
-
-// axios.get('https://api.nasa.gov/planetary/apod?api_key=kw4li6koQpnaZzFVA75n8NeQFl2B38hxVNrMCSLy')
-// .then(response => {
-//   console.log(response.data)
-// })
-// .catch(error => {
-//   console.log(error)
-// });
-
 function App() {
   const [info, setInfo] = useState([])
 
@@ -27,14 +18,15 @@ function App() {
 
   return (
     <div className="App">
-      <h1 id="h1">OUTLANDIA</h1>
+      <h1>OUTLANDIA</h1>
       <div className="picDiv">
-        <div className="pic">
-          <img className="picpic" src={info.url}></img>
+        <div className="picPic">
+          <img className="pic" src={info.hdurl} alt="nasa of the day"></img>
         </div>
       </div>
+      <h2>{info.title}</h2>
       <p> {info.explanation} </p>
-      <button onClick={() => (info.date)}>ANOTHER</button>
+      <button>LIKE</button>
     </div>
   );
 }
