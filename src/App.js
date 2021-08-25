ed5f91aa4e5cdf1c58fd76a468216dba853ebecb
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-08-24 22:00:37 -0400

    NasaAPIsetup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React { useState, useEffect } from 'react';
+import axios from 'axios'
 import "./App.css";
 
-Working on it
-
+const [data, setData] = useState([])
 
 function App() {
+
+  useEffect(() => {
+    axios.get("http//WhateverAPIcallGoesHere")
+     .then(response => {
+       setData(response.data)
+      const dataKeys = Object.keys(data)
+     })
+      .catch(error => {
+       console.log(error)
+      })
+
+    }, [])
+
+
+
   return (
     <div className="App">
       <p>
+
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
