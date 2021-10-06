146a6eb9d9f265b2ad43057bf26e363d9719d917
Ryan Howard <ryanghoward@protonmail.com>
2021-10-06 13:20:55 -0700

    Imports set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './constants';
 
 function App() {
+
+  const [nasaData, setNasaData] = useState([]);
+  const [photo, setPhoto] = useState('URL');
+
+useEffect (() => {
+  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res => {
+      const setNasaData = [res.data];
+      console.log(setNasaData);
+      return nasaData;
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <p>
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'R3pFcbCTvHTPalZfb9oCRLKk5zfJ5xzAbwdkmial'
