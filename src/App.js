7a59a5a4ab859d93d5d96a0cc4421a1d128be246
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-06 15:27:23 -0400

    created axios data request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=JPdEuTaesq36aLpUHKFaSl9AfGzypW2GMFeDDa86')
+  .then(resp=>{
+    console.log(resp)
+  })
+  .catch(err=>{
+    console.error(err);
+  })
   return (
     <div className="App">
       <p>
