922ddfff888fcd371d5e162b210b30512c8f8258
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2021-10-06 19:45:50 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [apiData, setApiData] = useState([])
+
+
+
+  useEffect(() => {
+   axios.get(`https://api.nasa.gov/planetary/apod?api_key=7YnANaDwNEdTS4uqUbwoDkDAcAHvtMIxYmL33f68`)
+   .then(resp => {
+     console.log(resp.data);
+     setApiData(resp.data);
+   }).catch(err => {
+     console.error(err)
+   })
+  }, [])
+
+
+  
+
+
+
+
+
   return (
     <div className="App">
+      <h1>{apiData.title}</h1>
+      <h2> {apiData.copyright}</h2>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+      <img src={`${apiData.url}`} alt="image of nasa" />
       </p>
+      <h3>
+      {apiData.date}
+      </h3>
+      <p>
+        {apiData.explanation}
+      </p>
+      
     </div>
   );
 }
