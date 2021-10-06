477cb9558dfaf3d76ef0578657d09b2ee68b6372
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-10-06 14:21:12 -0400

    finished MVP, added nasa Photo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Photo from "./Photo";
 
 function App() {
   return (
@@ -8,8 +9,10 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+
+    <Photo />
     </div>
   );
 }
-
+// api key 1Qg5Hb00n3IJDrbRLm8Um2zoXqJwFg6vZhSLcWm2
 export default App;
---
+++ src/Photo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import GetPhoto from "./GetPhoto";
+
+
+
+function Photo() {
+    return (
+        <div className="Photo">
+        <GetPhoto />
+      </div>
+    );
+  }
+
+
+
+
+
+export default Photo;
