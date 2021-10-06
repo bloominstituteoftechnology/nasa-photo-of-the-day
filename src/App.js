0669f7ade8b7368cba6bd6abbe738c48a04c0486
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-10-06 12:51:08 -0700

    opening pull request

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
     </div>
   );
 }
