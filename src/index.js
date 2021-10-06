23bdf276b88fe49fa8122e8bf5b78899d827f859
javarcho <alanvarcho@gmail.com>
2021-10-06 17:03:54 -0500

    well this was interesting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { ThemeProvider } from 'styled-components'
+import themeObj from './Theme/theme'
+
+ReactDOM.render(
+<ThemeProvider theme={themeObj}>
+    <App />
+</ThemeProvider>,
+ document.getElementById("root"));
