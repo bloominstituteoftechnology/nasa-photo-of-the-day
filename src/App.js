1b9cb64f09b6146cfd1e63ec1e21cbb5ca6c0971
Enas <enaselmahdwi@gmail.com>
2021-10-06 14:15:01 -0600

    updata files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
-import "./App.css";
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BASE_URL, API_KEY } from '../constants';
+import { BASE_URL, API_KEY } from './constants';
+import "./App.css";
+
+
 function App() {
   return (
     <div className="App">
