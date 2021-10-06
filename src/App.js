fa124c443f8716567fd7ab776db8d2fcb2e2e9d2
Angelique Abacajan <ma_utsey@yahoo.com>
2021-10-06 17:23:23 -0400

    Implemented useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+// export const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=qXYnhDSG0waj3uWYCmaxNAhsF5ghT3Wf7o1fHiby≈";
+// export const API_KEY = "qXYnhDSG0waj3uWYCmaxNAhsF5ghT3Wf7o1fHiby";
+
 function App() {
+
+  const [nasaPOD, setNasaPOD] = useState([]);
+
+  // useEffect
+  useEffect(() => {
+    const pictureOfTheDay = () => {
+      axios.get("https://api.nasa.gov/planetary/apod?api_key=qXYnhDSG0waj3uWYCmaxNAhsF5ghT3Wf7o1fHiby")
+       .then(res => {
+         setNasaPOD(res.data);
+  
+       }).catch(err => {
+         console.error(err);
+       })
+    }
+
+    pictureOfTheDay();
+  }, [])
+
+  
+
   return (
     <div className="App">
       <p>
