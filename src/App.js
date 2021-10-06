71fdd9f15d2b873c9f71bac5539f4c5bea76b0c3
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-06 16:49:00 -0700

    Button is functioning correctly and toggles display of image

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import axios from 'axios';
 import Description from "./Description";
 import Image from "./Image";
-import Button from "./Button"
+
 
 function App() {
 
   
   const [data, setData] = useState([])
-
+  const [isToggled, setIsToggled] = useState(true)
   // https://api.nasa.gov/planetary/apod?api_key=eZ9SoGT9vzhRWxb96gplCe8kADvwAfuKbtVaVnRj
   // https://api.nasa.gov/planetary/apod?api_key=WZW4y7Tt3gRhfUUFBWb6ml29HpVbuL30dFinJ7rV
 
@@ -31,8 +31,10 @@ console.log(data)
       <h1>NASA Photo of the Day!</h1>
       <Description data={data}/>
       <p>The photo was taken on {data.date}</p>
-      <Button data={data} className='off'/>
-      <Image data={data}/>
+      <button onClick={() => setIsToggled(!isToggled)}>
+      Click me!
+    </button>
+      {isToggled && <Image data={data}/>}
     </div>
   );
 }
