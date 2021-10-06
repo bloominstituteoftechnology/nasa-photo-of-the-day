e82a4cc42ff1ec2abffbf83f5db3fb60af979b83
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-06 16:50:39 -0700

    Changed some of the code on the button and got rid of button file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   
   const [data, setData] = useState([])
-  const [isToggled, setIsToggled] = useState(true)
+  const [isToggled, setIsToggled] = useState(false)
   // https://api.nasa.gov/planetary/apod?api_key=eZ9SoGT9vzhRWxb96gplCe8kADvwAfuKbtVaVnRj
   // https://api.nasa.gov/planetary/apod?api_key=WZW4y7Tt3gRhfUUFBWb6ml29HpVbuL30dFinJ7rV
 
@@ -32,7 +32,7 @@ console.log(data)
       <Description data={data}/>
       <p>The photo was taken on {data.date}</p>
       <button onClick={() => setIsToggled(!isToggled)}>
-      Click me!
+      CLICK FOR IMAGE
     </button>
       {isToggled && <Image data={data}/>}
     </div>
