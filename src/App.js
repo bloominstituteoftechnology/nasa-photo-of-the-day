a659c2102df28297c857be4b446b5f754c25aafe
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-06 17:00:42 -0700

    Succeed GET data from API

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import "./App.css";
 import axios from 'axios';
 
 function App() {
+
+  // Use an effect that runs after FIRST DOM surgery caused by the first render only.
+  // Use `useEffect` from React, which should consist of a call to the API using axios.
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then(res => {
+        console.log('res', res);
+      }).catch(err => {
+        console.error(err);
+      })
+  }, [])
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      {/* <OverView /> */}
     </div>
   );
 }
