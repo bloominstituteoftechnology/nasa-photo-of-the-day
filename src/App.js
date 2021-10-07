b688f2460f61f51dbeeb79370ae3bb70f0ae4907
djho57 <daniel.j.ho57@gmail.com>
2021-10-06 23:30:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
+import { API_KEY, BASE_URL, KEY_CONNECT } from "./Getapi.js";
 
 function App() {
+
+  const [nasaData, setNasaData] = useState([]);
+  const [spacePhoto, setSpacePhoto] = useState('imgURL')
+
+  useEffect(() => {
+  axios.get(`${BASE_URL}${KEY_CONNECT}${API_KEY}`)
+    .then(response => {
+      setNasaData(response.data)
+      console.log(nasaData);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  },[]
+  )
+
   return (
     <div className="App">
       <p>
---
+++ src/Getapi.js
@@ -0,0 +1,3 @@
+export const BASE_URL="https://api.nasa.gov/planetary/apod"
+export const API_KEY="sdfa4wwcYjtaxn3XnEnuw1boxWbTDqatygmlikBj"
+export const KEY_CONNECT="?api_key="
