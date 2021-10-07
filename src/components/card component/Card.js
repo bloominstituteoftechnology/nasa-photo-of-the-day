053d9bb2c5097fc6e0662bcbe1cbc6d4f8d4cfa7
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-10-06 19:04:36 -0700

    done with styling and typography

--- src/components/card component/Card.js
+++ src/components/card component/Card.js
@@ -13,13 +13,18 @@ export default function Card() {
   const [cardData, setCardData] = useState(null);
 
   useEffect(() => {
-    // using this hook only on mount, so that the api only calls once per render. Doing this by setting the second parameter in the useEffect hook to an empty array, since the value of the array can't change, then the first param will only run once after the return in this component.
+    // using this hook only on mount, so that the api only calls once per render. Doing this by setting the second parameter in the useEffect hook to an empty array, since the value of the array can't change, then the first param will only run once after the return in this component. Personal note: First the dom is made, then the useEffect is called, then the dom is painted again. The dom re renders because there was a change in state too.
     axios
       .get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&thumbs=true`)
       .then((res) => {
         setCardData(res.data);
+        // The card requires a "url" property, a "title" property, and a "explination" property.
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err);
+        // If there is an error, then the loading screen will render.
+        setCardData(null)
+      });
   }, []);
 
   return (
@@ -27,6 +32,7 @@ export default function Card() {
       {cardData ? (
         <>
           <Image imageUrl={cardData.url} />
+
           <div className="text-content">
             <Date date={cardData.date} />
             <Title title={cardData.title} />
