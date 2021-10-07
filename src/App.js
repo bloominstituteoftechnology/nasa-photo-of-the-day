cd69c7aa63e690add097f4e1a17cf5deae3c81fb
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2021-10-06 20:31:07 -0600

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import { BASE_URL, API_KEY, KEY_CONNECT } from "./getAPOD";
 import "./App.css";
 
+
+
 function App() {
+  const [nasaData, setNasaData] = useState([])
+  //const [imgDisplay, setImgDisplay] = useState(null)
+useEffect(()=> {
+  axios.get(`${BASE_URL}${KEY_CONNECT}${API_KEY}`)
+    .then(res => {
+      console.log(res.data)
+      setNasaData(res.data);
+    }).catch(err =>{
+      console.log(err)
+    })
+},[])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <h1>{nasaData.title}</h1>
+    <p>{nasaData.date}</p>
+    <div className='body'>
+      <img src={nasaData.url} alt={'Nebula'}/>
     </div>
+    <p>{nasaData.explanation}</p>
+    </div>
+    
   );
 }
 
