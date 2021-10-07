395239db2ead9ed0e089d756e55d9ff426e00d68
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-10-07 15:47:18 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Photo from "./Photo";
+import axios from 'axios'
+import Header from "./Header";
+import Summary from "./Summary";
+import styled from 'styled-components'
+
+
+const StyledWrapper = styled.div`
+text-align:center;
+background-color:black;
+color:white;
+border: black 2px solid;
+padding:20px;
+
+`
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        NASA PHOTO OF THE DAY<span role="img" aria-label='go!'>🚀</span>!
-      </p>
+  const apiKey = '1Qg5Hb00n3IJDrbRLm8Um2zoXqJwFg6vZhSLcWm2';
 
+  const [nasaData, setNasaData] = useState('');
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    .then(res => {
+        setNasaData(res.data);
+      //setDetails(res.data)
+    }).catch(err => {
+      console.error(err);
+    })
+  }, [])
+
+
+  return (
+    
+    <StyledWrapper>
+     <Header nasaData={nasaData}/>
     <Photo />
-    </div>
+    <Summary nasaData={nasaData}/>
+    </StyledWrapper>
+    
   );
 }
 
--- src/Photo.js
+++ src/Photo.js
@@ -1,15 +1,37 @@
-import React from "react";
-import GetPhoto from "./GetPhoto";
+import React, {useEffect, useState} from "react";
+import axios from 'axios'
+import styled from 'styled-components'
 
+const StyledPhoto = styled.img`
+    max-width:100%;
+    height:auto;
+`
 
 
 function Photo() {
-    return (
-        <div className="Photo">
-        <GetPhoto />
-      </div>
-    );
-  }
+    
+const apiKey = '1Qg5Hb00n3IJDrbRLm8Um2zoXqJwFg6vZhSLcWm2';
+
+const [nasaPhoto, setNasaPhoto] = useState('');
+
+    useEffect(() => {
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+        .then(res => {
+            setNasaPhoto(res.data.url);
+          //setDetails(res.data)
+        }).catch(err => {
+          console.error(err);
+        })
+      }, [])
+
+
+
+    return(
+        <StyledPhoto src={nasaPhoto} alt="nasa space"/>
+    )
+
+}
+  
 
 
 
