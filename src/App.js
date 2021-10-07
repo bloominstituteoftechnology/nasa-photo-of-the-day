e9913535a5b3a130677659af28af3b178f48369c
Cassius-Cassity <80988165+Cassius-Cassity@users.noreply.github.com>
2021-10-07 17:31:46 -0600

    finished MVP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import { BASE_URL, API_KEY, KEY_CONNECT } from "./getAPOD";
 import "./App.css";
+import styled from 'styled-components';
+
+const StyledFriend = styled.div`
+  width: 60%
+  display: flex;
+  justify-content: space-between;
+  padding: 8px
+  border-bottom: 2px solid white;
+  line-height: 1.6;
+
+  background-color: ${pr => pr.theme.primaryColor};
+  color: ${pr => pr.theme.white}
+`
 
 
 
@@ -19,6 +32,7 @@ useEffect(()=> {
 },[])
 
   return (
+    <StyledFriend>
     <div className="App">
     <h1>{nasaData.title}</h1>
     <p>{nasaData.date}</p>
@@ -27,7 +41,7 @@ useEffect(()=> {
     </div>
     <p>{nasaData.explanation}</p>
     </div>
-    
+    </StyledFriend>
   );
 }
 
