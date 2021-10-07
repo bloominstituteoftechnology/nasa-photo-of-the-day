8c62dc888784478a6275ca4bc38dffa0c7914727
Angelique Abacajan <ma_utsey@yahoo.com>
2021-10-07 15:24:39 -0400

    Added Card.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+
+  const [nasa, setNasa] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=qXYnhDSG0waj3uWYCmaxNAhsF5ghT3Wf7o1fHiby")
+     .then(res => {
+       const nasaData = res.data;
+       setNasa(nasaData);
+     }).catch(err => {
+       console.error(err);
+     })
+  }, [])
+
   return (
     <div className="App">
       <p>
