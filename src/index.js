94b66ec660f264f787786988c32306fe3fe33a06
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-07 17:32:06 -0400

    added styling files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 //import ReactDOM from "react-dom";
 import { render } from "react-dom";
+import { ThemeProvider } from 'styled-components';
+
 import "./index.css";
 import App from "./components/App";
+import theme from './theme';
 
 render(
-    <App />,
+  <ThemeProvider theme={theme}>
+    <App />
+    </ThemeProvider>,
     document.querySelector('#root')
   )
---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: 'red',
+    secondaryColor: 'green',
+    tertiaryColor: 'mediumseagreen',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 550px)'
+}
