0e42e447b1ef959a39ff5bdea89a21f529809551
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-10-07 14:17:42 -0500

    top half

--- src/imageWrapper/ImageWrapper.js
+++ src/imageWrapper/ImageWrapper.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import InnerCard from "../innerCard/InnerCard";
-import './styles.css'
+import styled from 'styled-components'
+
+const ImageCard = styled.a`
+height: 400px;
+position: relative;
+padding: 20px;
+box-sizing: border-box;
+display: flex;
+align-items: flex-end;
+text-decoration: none;
+border: 4px solid #b0215e;
+margin-bottom: 5px;
+background-size: cover;
+background-image: url('${props => props.imageUrl}');
+@media (min-width: 768px) {
+    height: 500px;
+`
 
 const ImageWrapper = ({ data }) => {
     const { url } = data
     return (
-        <a href={url} className="card" style={{ backgroundImage: `url(${url})` }} target="_blank" rel="noreferrer noopener">
-            <InnerCard data={data}/>
-      </a>
+        <ImageCard imageUrl={url}><InnerCard data={data}/></ImageCard>
     )
 }
 
--- src/innerCard/InnerCard.js
+++ src/innerCard/InnerCard.js
@@ -1,15 +1,50 @@
 import React from 'react'
-import './styles.css'
+import styled from 'styled-components'
+
+
+const Inner = styled.div`
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: white;
+  box-sizing: border-box;
+  padding: 40px;
+  @media (min-width: 768px) {
+      width: 50%;
+      height: 100%;
+  }
+`;
+
+const Title = styled.h2`
+font-size: 24px;
+    color: black;
+    text-align: center;
+    font-weight: 700;
+    color: #181818;
+    text-shadow: 0px 2px 2px #a6f8d5;
+    position: relative;
+    margin: 0 0 20px 0;
+  @media (min-width: 768px) {
+      font-size: 30px;
+    };
+`
+
+const Subtile = styled.time`
+color: #b0215e;
+text-align: center;
+`;
 
 const InnerCard = ({ data }) => {
-    console.log('INNERCARD', data)
+    
 
     const { title, explanation, copyright, date } = data
     return (
-        <div className="inner">
-          <h2 className="title">{title}</h2>
-          <time className="subtitle">{`Copyright: ${copyright}, Date: ${date}`}</time>
-        </div>
+        <Inner>
+          <Title>{title}</Title>
+          <Subtile>{`Copyright: ${copyright}, Date: ${date}`}</Subtile>
+        </Inner>
     )
 }
 
