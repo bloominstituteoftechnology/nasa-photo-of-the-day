8f44b0abf451359e9186d8193fa47bf025f59ae6
djho57 <daniel.j.ho57@gmail.com>
2021-10-07 20:31:15 -0500

    added styled-compenents

--- src/App.js
+++ src/App.js
@@ -2,31 +2,43 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import "./App.css";
 import { API_KEY, BASE_URL, KEY_CONNECT } from "./Getapi.js";
+import styled from 'styled-components';
 
+const StyledApp = styled.div`
+  display: flex;
+  border: 2px solid red;
+  background-color: orange;
+  padding: 8px;
+`
+const StyledImg = styled.img`
+  width: 60%;
+`
+const StyledP = styled.p`
+  font-weight: bold;
+`
 function App() {
 
   const [nasaData, setNasaData] = useState([]);
-  const [spacePhoto, setSpacePhoto] = useState('imgURL')
 
   useEffect(() => {
   axios.get(`${BASE_URL}${KEY_CONNECT}${API_KEY}`)
     .then(response => {
-      setNasaData(response.data)
-      console.log(nasaData);
+      setNasaData(response.data.url)
     })
     .catch(error => {
       console.log(error);
     })
   },[]
   )
+  console.log(nasaData);
 
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <StyledApp>
+      <StyledP>
+        Pic of the day!!
+      </StyledP>
+      <StyledImg src={nasaData} alt="a random pic"/>
+    </StyledApp>
   );
 }
 
--- src/Getapi.js
+++ src/Getapi.js
@@ -1,3 +1,4 @@
 export const BASE_URL="https://api.nasa.gov/planetary/apod"
-export const API_KEY="sdfa4wwcYjtaxn3XnEnuw1boxWbTDqatygmlikBj"
+export const API_KEY="m0DenzvyAodwRZiB0MgcsKOdhfdYPvWpCXmkL6PT"
 export const KEY_CONNECT="?api_key="
+export const date="2021-10-05"
