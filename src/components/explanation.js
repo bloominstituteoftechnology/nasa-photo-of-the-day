bc31095b158559da6a2041f0eaf6f70c11278dfc
Jesse <eoshout@Gmail.com>
2021-10-28 21:28:57 -0400

    updated css

--- src/components/explanation.js
+++ src/components/explanation.js
@@ -1,18 +1,20 @@
 import React from "react"
 import styled from 'styled-components'
 const ExpStyle = styled.div`
-display: flex;
-  text-align: center;
+  display: flex;
+    justify-content: 'center';
+    align-items: 'center';
 color: purple;
   font-weight: bold;
   width:86vh;
   display: flex;
   text-align: center;
-  margin-left: 20%;
+  margin-left: 28%;
 `
+
 export default function Explnation({explanation}){
     return (<ExpStyle>
-        <p>{explanation}</p>
+      <para><p>{explanation}</p></para>
     </ExpStyle>)
     
 }
