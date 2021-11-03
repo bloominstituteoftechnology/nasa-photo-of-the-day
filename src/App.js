1acbe459974321ef08ab5861350f059e59b9034d
Bobby Gomez <bobbygomez@Bobbys-MacBook-Pro.local>
2021-11-03 17:29:28 -0500

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Photo from "./Photo";
 
 function App() {
   return (
@@ -8,8 +9,12 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+
+      <Photo/>
     </div>
   );
 }
+
+// api key VxmyeETx5uJSltRR3PiVxvyyOu4P0DfNlLkbupmJ
 
 export default App;
