027fd7307bd9a3a829e6afc6198d2049b4d003ed
quavo <qua.thesage19@gmail.com>
2021-11-03 18:55:24 -0500

    added components to the dom, title

--- src/Pod.js
+++ src/Pod.js
@@ -1,26 +1,32 @@
 import React, { useEffect, useState} from 'react';
-
+import PodData from './PodData';
 import axios from 'axios';
 
 
 function Pod() {
-    const[podData, setPodData] = useState([]);
+    const[podList, setPodList] = useState([]);
 
     useEffect(() => {
         axios.get('https://api.nasa.gov/planetary/apod?api_key=L5kG68ulrnuvDh7bGftjtsG0HjmbxUKmqCZcN2YA')
         .then(res => {
-            setPodData(res.data);
+            setPodList(res.data);
         })
         .catch(error => {
             console.error(error)
         })
     }, []);
     
-}
+
 
 return (
     <div className = 'card'>
-
+        <PodData key = {podList.data}
+        title = {podList.title}
+        
+        />
     </div>
 )
 
+};
+
+export default Pod;
--- src/PodData.js
+++ src/PodData.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const podData = props => {
+const PodData = props => {
     return (
         <div className = 'podData' key={props.data}>
 
-            <img className='img' src={props.Img}/>
+            <img className='img' src={props.Img} alt=''/>
+            <h1>{props.data}</h1>
+            <p>{props.title}</p>
+            <p>{props.version}</p>
+            <p>{props.explanation}</p>
 
         </div>
-    )
-}
+    );
+};
+
+export default PodData;
