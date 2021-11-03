e38b0abc472e9507aec96d2647c50083c4c17227
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-11-03 14:48:32 -0400

    finished setting up my app.js files for day 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import Header from './Components/header';
+import Top from './Components/topsection';
+import Info from './Components/infosection';
+import Image from './Components/image';
+
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(res => {
+      // console.log(res.data)
+      setData(res.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }, [])
+
+
   return (
     <div className="App">
       <p>
