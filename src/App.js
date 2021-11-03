4cc6efd9fb8c1c96c7ebc395bffeb170c0a7752c
UrukuTelal <ao.bier1101@gmail.com>
2021-11-03 16:00:03 -0700

    trying to get the image getting code 429: Too Many Requests

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios';
 //Read through the instructions in the README.md file to build your NASA
 //app! Have fun
-axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-.then(resp => {
-  console.log(resp);
 
-})
-.catch(err => {
-  console.error(err);
-})
 
-function App() {
+
+function App(props) {
+  const [nasa, setNasa] = useState([]);
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(resp => {
+    //console.log(resp);
+    setNasa(resp.data);
+  })
+  .catch(err => {
+    console.error(err);
+  })
   return (
     <div className="App">
+      <img url = {nasa.url} alt=''/>
       <p>
          <span role="img" aria-label='go!'>🚀</span>!
       </p>
