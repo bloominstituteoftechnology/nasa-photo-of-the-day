de8fc719f5d88f948c27442cab2730556ce34c24
quavo <qua.thesage19@gmail.com>
2021-11-03 18:38:07 -0500

    created pod function that uses useEffect and axios

--- src/Pod.js
+++ src/Pod.js
@@ -6,5 +6,21 @@ import axios from 'axios';
 function Pod() {
     const[podData, setPodData] = useState([]);
 
+    useEffect(() => {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=L5kG68ulrnuvDh7bGftjtsG0HjmbxUKmqCZcN2YA')
+        .then(res => {
+            setPodData(res.data);
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }, []);
     
 }
+
+return (
+    <div className = 'card'>
+
+    </div>
+)
+
