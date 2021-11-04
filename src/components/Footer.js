00f359ec0bcdf64530d3f84130dcf43fddb71b39
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-03 21:47:39 -0700

    added hover color to footer buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import style from "./Footer.css";
 
 const Footer = (props) => {
     console.log(props);
@@ -6,7 +7,7 @@ const Footer = (props) => {
     <div className="footerCard">
       <ul>
         {props.menuItems.map((item) => {
-          return <li className= "fList"> <button className ="alienB"> {item}</button></li>;
+          return <li className= "fList"> <button> {item}</button></li>;
         })}
       </ul>
     </div>
