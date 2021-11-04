074d4b296199e9c1c1380ae60e47bf08d8625267
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2021-11-03 20:26:51 -0400

    Some light

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState('')
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=WRmpzQ07LjbF2hNG9g5O0M4yDhLsCbFqcUb5vx7u')
+    .then(res => {
+      setUrl(res.data.hdurl)
+
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+
+  function IOFD ({url}) {
+    // eslint-disable-next-line
+    return <img className='img' src={url} alt='NASA Image of the Day'/>
+  }
+  const refresh = () => {
+    window.location.reload();
+  }
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA IMAGE OF THE DAY</h1>
+      <IOFD url={url}/><br></br>
+      <button className='button' onClick={refresh}>Reload</button>
     </div>
   );
 }
