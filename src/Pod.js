3927fffbf78f0b3e0a790e0ae46bee1744f37762
quavo <qua.thesage19@gmail.com>
2021-11-03 19:21:50 -0500

    Added content to the dom;

--- src/Pod.js
+++ src/Pod.js
@@ -7,8 +7,9 @@ function Pod() {
     const[podList, setPodList] = useState([]);
 
     useEffect(() => {
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=L5kG68ulrnuvDh7bGftjtsG0HjmbxUKmqCZcN2YA')
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=VivAhNalbXGkj707tqxVNaVJ6v1p5LPCqXs7kLYy')
         .then(res => {
+            
             setPodList(res.data);
         })
         .catch(error => {
@@ -22,6 +23,9 @@ return (
     <div className = 'card'>
         <PodData key = {podList.data}
         title = {podList.title}
+        date ={podList.date}
+        explanation = {podList.explanation}
+        Img = {podList.url}
         
         />
     </div>
--- src/PodData.js
+++ src/PodData.js
@@ -4,9 +4,9 @@ const PodData = props => {
     return (
         <div className = 'podData' key={props.data}>
 
+            <h1>{props.title}</h1>
             <img className='img' src={props.Img} alt=''/>
-            <h1>{props.data}</h1>
-            <p>{props.title}</p>
+            <p>{props.date}</p>
             <p>{props.version}</p>
             <p>{props.explanation}</p>
 
