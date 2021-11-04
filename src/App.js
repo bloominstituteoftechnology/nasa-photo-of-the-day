892076f036a4b63130e7dd5c19fd4668936e793e
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-11-03 19:21:16 -0600

    working on it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
-import {API_KEY, BASE_URL} from './Constants'
-
+import {API_KEY, BASE_URL} from './Constants/index'
+import Image from "./components/image";
 export default function App() {
 
   const [nasaData, setNasaData] = useState();
-
+  const [image, setImage] = useState();
+  
   useEffect(() => {
       axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
         .then(res => {
-          setNsaData(res.data);
-        
+          setNasaData(res.data);
+        console.log(res.data);
+        setImage(nasaData.hdurl);
         })
         .catch(err => {
           console.log(err)
@@ -22,15 +24,11 @@ export default function App() {
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <p> Picture of the Day</p>
+      <Image image={image} setImage={setImage}/>
     </div>
   );
 }
-
-export default App;
 
 }
 
---
+++ src/Constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'OyQJe4Map0xtCVyXmdNQDZqqnW8MeE3z2P5kOEnH'
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
