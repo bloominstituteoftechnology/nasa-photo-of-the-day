05ec2cc9bbcebe679913a61603d6fa4306824b40
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2021-11-04 02:04:37 -0400

    i worked on this homwork with a couple of students from class, I feel I did the coding thoroughly but cant figure out where I went wrong. anyways im submiting before the end of the due date period

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import "./App.css";
+import axios from 'axios'
+import Img from './img'
+import Explain from './Explain'
+import Extra from './Extra'
 
 function App() {
+
+  const [nasa, setNasa] = useState([])
+  useEffect(() =>{
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=vYasWXhZq0LKf8nf7DK0BDHi7VtXFR&date=2012-03-14`)
+    .then(res => {
+      setNasa(res.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>Nasa Image of the Year</h1>
+      <Extra nasaExtra = {nasa}/>
+      <input/><button>reset</button>
+      <Img nasaImg = {nasa}/>
+      <Explain nasaInfo = {nasa}/>
     </div>
   );
 }
