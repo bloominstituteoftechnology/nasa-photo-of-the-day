f3d65c2f07fac37d74a9320da51a31efed8ab5bb
DayLaFavor <danelafavor1@gmail.com>
2021-11-04 20:19:16 -0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
-import Style from "style-components"
+import Style from "style-components";
+import "./Header.js";
 
 function App() {
+  const [NPFTD, setNPFTD] = useState(null)
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=QJvQ8epqTuHyftbwtNI2zWGIYkhTewyNThhIADJ5`)
+    .then(resp => {
+      setNPFTD(resp.data)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        NASA - Photo of the day <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      {<Header NPFTD={NPFTD}/>}
     </div>
   );
 }
