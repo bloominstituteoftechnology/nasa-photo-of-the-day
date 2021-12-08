36aeab8b7bd398aca107a99a4c6eab9218204264
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2021-12-08 11:36:41 -0500

    edited the text to create pull request

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun (having fun) <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
   );
