2c45d94c9828032b675f8bb6487781a02d9057ee
lomelo_x <aalomelo@gmail.com>
2021-12-08 15:05:19 -0600

    Completed Task 2: Planning & File Structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
+  const [info, setInfo] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=iuZ0MCtSF5sDdt8apQEPwg3AxliAa6jSz22qjzTz')
+    .then(res => {
+      console.log(res.data)
+      setInfo(res.data)
+    })
+    .catch(err => {
+      debugger
+    })
+  }, [])
+
   return (
     <div className="App">
       <p>
