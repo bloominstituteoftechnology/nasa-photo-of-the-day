0a232818521181f9391462127d8523377acb8567
Brittany Petersen <britt.petersen100@gmail.com>
2021-12-08 15:22:35 -0700

    added axios code to access api data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { BASE_URL, API_KEY } from './data'
+import axios from 'axios'
 
 function App() {
+
+  const [todaysImage, setTodaysImage] = useState([])
+  const [currentDate, setCurrentDate] = useState(null)
+
+
+
+  useEffect(() => { 
+    const fetchImage = () => {
+      axios.get(`${BASE_URL}/apod?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data)
+        setTodaysImage(res.data.url)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    fetchImage()
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
+  
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+
     </div>
   );
 }
