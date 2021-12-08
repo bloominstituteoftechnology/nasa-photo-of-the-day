6db34b3e19db158aa4146a9d182da73d76c03f44
Chris Kelley <chriskelley85@gmail.com>
2021-12-08 14:27:20 -0600

    use effect to fetch APOD

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
+import Body from './Body.js';
 
 function App() {
+const [nasaData, setNasaData] = useState('')
+
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=bFnzGs0ui4bTcLqSA3DQgV9IHgadFW0AKLeGVsOL')
+    .then(res => {
+      setNasaData(res.data)
+    })
+    .catch(res => {
+      console.log(res.data)
+    })
+
+  },[])
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+       NASA app!<span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <Body nasaAPI = {nasaData} />
     </div>
   );
 }
