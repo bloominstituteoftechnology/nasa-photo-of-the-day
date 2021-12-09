98fcb62db3f4e99977e6b3fc0def06691ef17121
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-08 20:56:01 -0500

    getting somewhere but not quite theree yet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 
+
+
 function App() {
+  const [data, setData] = useState([]);
+  
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/#apod')
+    .then(resp => {
+      console.log(resp.data)
+      setData(resp.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!i
+        {data}
       </p>
     </div>
   );
