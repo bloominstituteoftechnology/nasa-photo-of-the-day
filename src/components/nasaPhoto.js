9c487de7ab220ad69787517fe10700bc7c6a9aa7
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2021-12-09 14:11:52 -0800

    added styles using styled-component library

--- src/components/nasaPhoto.js
+++ src/components/nasaPhoto.js
@@ -1,17 +1,54 @@
 //https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
 import React from "react";
+import styled from "styled-components";
+
+
+const StyledContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 8px;
+  border-bottom: 2px solid white;
+
+  img {
+    border-radius:5%;
+    transition: transform 1s;
+    &:hover {
+        transition: transform 1s;
+        transform: scale(1.5)
+        
+    }  
+  }
+  h2 {
+      color : #98817b;
+      &:hover {
+        background-color: #abcdef;
+      }
+  }
+  p {
+    &:hover {
+        background-color: #abcdef;
+      }
+  }
+  h4 {
+    &:hover {
+        background-color: #abcdef;
+      }
+  }
+`
 
 const Photos = (props) => {
     const { info } = props;
     return (
-        <div className='photo'>
+        <StyledContent className='photo'>
             <div className='Heading'>
                 <h2>{info.title}</h2>
-                <h3>{info.explanation}</h3>
+                <p>{info.explanation}</p>
                 <h4>{info.date}</h4>
             </div>
             <img src={info.url} />
-        </div>
+        </StyledContent>
 
     )
 }
