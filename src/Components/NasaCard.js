d22cbfec305a4b6106704c1d883898b6642bd270
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2021-12-09 15:31:59 -0800

    finally got started

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -1,14 +1,20 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledContent = styled.div`
+color: dodgerblue;
+img-width: 60%;
+`
 
 const NasaCard = (props) => {
     return (
-        <>
+        <StyledContent className='container'>
             <h1>Daily Nasa Photo</h1>
             <p>{props.data.date}</p>
             <p>{props.data.title}</p>
             <img src={props.data.url} alt='Nasa photo of the day' />
             <footer>This is my footer section</footer>
-        </>
+        </StyledContent>
     )
 }
 
