5f2d9f39e13920c95be90807c26308d0ceb6bbb7
Markeisha Wallacce <Markeisha94@gmail.com>
2021-12-09 13:04:29 -0600

    finished styling

---
+++ src/theme/index.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: 'orange',
+    secondaryColor: 'purple',
+    tertiaryColor: 'mediumseagreen',
+    black: 'black',
+    white: 'white',
+    breakpointMobile: '(max-width: 500px)'    
+}
