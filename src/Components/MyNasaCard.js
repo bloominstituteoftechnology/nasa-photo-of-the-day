8a7fd3fc8946ac5f20ffae87c7f3736c976446cf
Carlos Solano <solanoandy@gmail.com>
2021-12-09 18:30:29 -0600

    solved mvp today for styling

--- src/Components/MyNasaCard.js
+++ src/Components/MyNasaCard.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const NasaCard = (props) => {
-    return (
-        <>
-            <h1>Nasa Photo!</h1>
-            <p>{props.data.date}</p>
-            <p>{props.data.title}</p>
-            <img src={props.data.url} alt="Nasa's photo of the day"/>
-        </>
-    )
+
+const StyledTop = styled.div`
+width: 100%;
+height: 40vh;
+display: flex;
+justify-content: space-around;
+`
+
+const StyledLeft = styled.div`
+background-color: ${props => props.theme.primaryColor};
+border-radius: 5%;
+width: 50%;
+color: ${props => props.theme.white};
+text-align: center;
+font-style: 'lato';
+font-size: 0.85rem;
+padding: 2%;
+margin:1%;
+`
+const StyledRight = styled.div`
+    width: 50%;
+    border-radius: 5%;
+    
+img{
+ max-width: 100%;
+ border-radius: 1%;
+ border: 2px solid ${props => props.theme.primaryColor};
+ margin-bottom: 1%;
 }
-export default NasaCard;
+`
+
+
+export default function DayCard(props){
+const {data} = props;
+
+
+
+return (
+    <StyledTop>
+        <StyledLeft>
+            <h1>{data.date} - {data.title} </h1>
+            <p>{data.explanation}</p>
+        </StyledLeft>
+        <StyledRight>
+            <img src={data.url} alt="Nasa's img of the day"/>
+        </StyledRight>
+    </StyledTop>     
+);
+}
