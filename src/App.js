3ead4e1b69ebddba299d5ecbe93fe2999b5602a7
iamleahb <leah.ball@gmail.com>
2021-12-09 18:06:44 -0600

    basic styling achieved

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
+import styled from "styled-components";
 
-// import the children components
 import Main from "./Components/Main";
-import Sidebar from "./Components/Sidebar";
+import Header from "./Components/Header";
 
-//import axios key
-// import { BASE_URL, API_KEY } from './Components/Keys';
 const { REACT_APP_BASE_URL, REACT_APP_API_KEY } = process.env;
 
-function App() {
-  // const [ nasaTitle, setNasaTitle ] = useState(null);
-  // const [ nasaDate, setNasaDate ] = useState(null);
-  // const [ nasaImg, setNasaImg ] = useState(null);
-  // const [ nasaExp, setNasaExp] = useState(null);
-  // const [ nasaCopyright, setNasaCopyright ] = useState(null);
+const Wrapper = styled.div`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    max-width: 100%;
+}
+html{
+    font-size:62.5%;
+}
+body{
+    font-size:1.6rem;
+    line-height:1.5;
+    font-family: 'ABeeZee', sans-serif;
 
+}
+a{
+    font-family: 'Source Sans Pro', sans-serif;
+    color:#266035;
+    text-decoration: none;
+}`;
+
+function App() {
   const [ nasaData, setNasaData ] = useState({});
 
 
   useEffect(() => {
     axios.get(`${REACT_APP_BASE_URL}?api_key=${REACT_APP_API_KEY}`)
       .then(res => {
-        // setNasaTitle(res.data.title);
-        // setNasaDate(res.data.date);
-        // setNasaImg(res.data.hdurl);
-        // setNasaExp(res.data.explanation);
-        // setNasaCopyright(res.data.copyright);
         setNasaData(res.data);
       })
       .catch(err => {
@@ -36,12 +44,12 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    
+    <Wrapper className="App">
+      <Header nasaTitle={nasaData.title} nasaDate={nasaData.date} />
       <Main nasaImg={nasaData.hdurl} nasaExp={nasaData.explanation} nasaCopyright={nasaData.copyright} />
-      <Sidebar nasaTitle={nasaData.title} nasaDate={nasaData.date} />
       
-
-    </div>
+    </Wrapper>
   );
 }
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,28 @@
-// imports
 import React from 'react';
+import styled from 'styled-components';
 
-// Export default content
+const MainDiv = styled.div`
+    padding: 4%;
+    background-color: grey;
+    font-size: 1.3rem;
+    p {
+        padding: 10% 4% 4%;
+        border-top: 2px solid red;
+    }
+    span {
+        background-color: white;
+        font-style: italic;
+    }
+`
+
 function Main(props) {
     const { nasaImg, nasaExp, nasaCopyright } = props;
     return (
-        <div>
-            <img src={nasaImg} width= '65%' alt="Nasa Photo of the Day"/>
+        <MainDiv>
+            <img src={nasaImg} width= '100%' alt="Nasa Photo of the Day"/>
             <p>{nasaExp}</p> 
             <span>{nasaCopyright}</span>
-        </div>
+        </MainDiv>
     )
 }
 
