b1bda9eb869728e4fbee84c3179a0bd1cfe665b7
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2021-12-10 00:28:09 -0500

    day 2 done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import axios from 'axios'
 import Img from './img'
 import Explain from './Explain'
 import Extra from './Extra'
+import styled from 'styled-components';
+
+
+
+const StyleBack = styled.div`
+background-color: #e0afa0;
+display: flex
+justify-content center;
+padding: 0 10%;
+h1{
+  background-color: #bcb8b1;
+  padding-left: 12.5rem
+}
+`
+const StyleHead = styled.h1`
+font-size: 3.75rem
+`
 
 function App() {
 
@@ -19,13 +36,13 @@ function App() {
   }, [])
   
   return (
-    <div className="App">
-      <h1>Nasa Image of the Year</h1>
-      <Extra nasaExtra = {nasa}/>
-      <input/><button>RESET</button>
-      <Img nasaImg = {nasa}/>
-      <Explain nasaInfo = {nasa}/>
-    </div>
+    <StyleBack>
+      <StyleHead>Nasa Image of The Day</StyleHead>
+       <Extra nasaExtra = {nasa}/>
+       <Explain nasaInfo = {nasa}/>
+       <Img nasaImg = {nasa}/>
+     </StyleBack>
+
   );
 }
 
