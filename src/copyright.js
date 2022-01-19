5b42b61136afe7bc0e43a923993fe6dcb2528307
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-01-19 13:09:29 -0800

    nasa.js

---
+++ src/copyright.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Copyright = props => {
+    const {right} = props;
+    return (
+        <div>
+            <h2>Copyright</h2>
+        </div>
+    )
+}
+
+export default Copyright;
