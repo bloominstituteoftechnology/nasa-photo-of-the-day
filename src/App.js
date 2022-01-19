8111336f61b2f11009183745dec497870966377a
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-01-19 13:05:34 -0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
-}
+  const [nasaData, setNasaData] = useState();
 
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=jWaCWGvDdPdY2207ALzpXNfw5arVUICq624WiDDR`)
+      .then(res => {
+        setNasaData(res.data);
+      }).catch(err => console.error(err))
+  }, [])
+}
 export default App;
