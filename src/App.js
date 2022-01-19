95ce19a52477f8b694fd839f5fd3cc0d5d77ddd8
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2022-01-19 12:48:52 -0800

    fetching the data from the api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+
+useEffect(() => {
+  const getPhoto = () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=cE5KTLhSAfLJAkBh1kW9Uo748DteCGEStGfvJSmU')
+    .then(resp => {
+      console.log(resp.data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }
+  getPhoto()
+}, [])
+
+
+
   return (
     <div className="App">
       <p>
