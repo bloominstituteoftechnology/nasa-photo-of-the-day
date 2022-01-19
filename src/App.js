d9a3b9eb0cb5cf14fe1656e567f29a7dbc4e115c
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-01-19 13:22:12 -0800

    added-copyright

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
+import styled, { keyframes } from 'styled-components';
+import Nasa from './nasa';
+
+const kf = keyframes`
+  50% {
+    transform: scale(0.8);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }`
+
+const StyledApp = styled.div`
+  display: flex;
+  background-color: royalblue;
+  border: 2px solid pink;
+  color: darkred;
+ 
+  h1 {
+    width: 80;
+    justify-content: center;
+    background-color: pink;
+    border: 2px solid crimson;
+  }
+
+  p {
+    color: hotpink;
+  }
+
+  img {
+    border-radius: 10px;
+  }
+
+  transform: scale(2); // start of animation
+  opacity: 0; // start of animation;
+  animation: ${kf} 3s ease-in-out forwards;
+`
+
 
 function App() {
   const [nasaData, setNasaData] = useState();
@@ -11,5 +49,14 @@ function App() {
         setNasaData(res.data);
       }).catch(err => console.error(err))
   }, [])
+  return (
+    <StyledApp className="App">
+      {nasaData && <Nasa nasaData={nasaData} /> }
+    </StyledApp>
+  );
 }
+
 export default App;
+
+
+
