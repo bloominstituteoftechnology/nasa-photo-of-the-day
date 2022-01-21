4add7394b7cccf151f307b5729d48cc0a5b407a4
vladimir <vladimirsaakian@gmail.com>
2022-01-20 17:30:26 -0800

    added styles

---
+++ src/components/PhotoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import PhotoCard from './PhotoCard';
+
+export default function ImageList() {
+	return (
+		<div>
+			<h1>PhotoList return</h1>
+			<PhotoCard />
+		</div>
+	);
+}
