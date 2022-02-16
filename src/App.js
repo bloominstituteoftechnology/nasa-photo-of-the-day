7def1c5f79d538c20244e706374edd93541b410f
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-02-16 14:04:50 -0800

    installed axios, added useeffect and state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+  const [pics, setPics] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
       <p>
@@ -13,3 +24,4 @@ function App() {
 }
 
 export default App;
+
