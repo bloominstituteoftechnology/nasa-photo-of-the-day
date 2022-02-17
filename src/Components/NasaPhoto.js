5e2fdc9258f00f49f03f734fc53754021949a80a
Kevin Lai <kevin.lai@gmail.com>
2022-02-17 17:27:07 -0500

    did bare minimum

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const kf = keyframes`
+    100% {
+        opacity: 1;
+        transform: scale(1) rotateZ(0);
+    }
+`
+
+const StyledDetails = styled.div`
+    opacity: 0;
+    transform: scale(2) rotateZ(180deg);
+    animation: ${kf} 0.5s ease-in-out forwards
+`
 
 const NasaPhoto = (props) => {
     return (
-        <div className="nasa-photo">
+        <StyledDetails className="nasa-photo">
             <h3>{props.photo.title}</h3>
             <p>{props.photo.data}</p>
             <img src={props.photo.hdurl} />
             <p className="explanation"> {props.photo.explanation}</p>
-        </div>
+        </StyledDetails>
     )
 }
 
