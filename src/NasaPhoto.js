0c59b46922ef55e7cb88f78d225709d4d276e078
Ryan Berry <rberry16@hotmail.com>
2022-02-16 17:30:27 -0700

    all tasks completed

---
+++ src/NasaPhoto.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+  
+  const NasaPhoto = (props) => {
+      return (
+          <div className='nasa-photo-wrapper'>
+              <h3>{props.photo.title}</h3>
+              <p>{props.photo.date}</p>
+              <img src={props.photo.url}/>
+              <p className='explanation'>{props.photo.explanation}</p>
+          </div>
+      )
+  }
+
+  export default NasaPhoto;
