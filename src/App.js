db64a0c2362f48723bcba99cf032256da5494204
Kohlten Bills <kohltenbillsy@gmail.com>
2022-03-23 15:34:10 -0600

    main submit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import NasaPhoto from "./Components/NasaPhoto";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(()=> {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=OYQjUFNenvSho9S6HdJFN32UTu42cV94XX6lgD29')
+      .then(res => {
+        setData(res.data)
+      })
+      .catch(err => console.error(err))
+  }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      {data && <NasaPhoto photo={data}/> }
     </div>
   );
 }
 
 export default App;
+
+// https://api.nasa.gov/planetary/apod?api_key=OYQjUFNenvSho9S6HdJFN32UTu42cV94XX6lgD29
