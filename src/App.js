8a93a7563ca235895597e987e1f5f3e7f443a66f
Gage Clancy <gclancy121@gmail.com>
2022-03-23 15:51:01 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [spaceData, setSpaceData] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=hm1ZzTejYicWaa4IGPQovdxqa9tuz4OKHU9o4kO0')
+    .then (res => {
+      setSpaceData(res.data);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>
+        I'm escaping to the one place that hasn't been corrupted by capitalism......
+        S P A C E!</h1>
+        <h1>{spaceData.title}</h1>
+        <h4>{spaceData.explanation}</h4>
+        <img src={spaceData.hdurl} width={200} height={200} alt="the bubble nebula"/>
+      
+      
     </div>
   );
 }
