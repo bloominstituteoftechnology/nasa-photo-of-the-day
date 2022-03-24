13a60bd58165bc86ac966272df14b6b547d3d389
Gage Clancy <gclancy121@gmail.com>
2022-03-24 15:22:17 -0500

    updated the styling using the advanced methods

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
+import styled from 'styled-components';
+
+
+
+const StyledDetails = styled.div`
+background-color: ${pr => pr.theme.primaryColor};
+text-align: center; 
+  color: white;
+  text-decoration: none;
+  padding: 1%;
+  img {
+    height:100vmin;
+  }
+  header {
+    background-color: ${pr => pr.theme.headerColor};
+    min-height: 10vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+  }
+`
+
+
 
 function App() {
   const [spaceData, setSpaceData] = useState([]);
@@ -12,17 +36,17 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <div className="App-header">
+    <StyledDetails >
+      <header className="App-header">
       <h1>The NASA Astronomy Photo Of The Day</h1>
-        </div>
+      </header>
         <h1>{spaceData.title}</h1>
         <h4>{spaceData.explanation}</h4>
-        <img className="App-picture" src={spaceData.hdurl} alt="the bubble nebula"/>
+        <img src={spaceData.hdurl} alt="the bubble nebula"/>
         <div>
           <p>Made courtesy of the NASA APOD API, link to it can be found <a href = "https://api.nasa.gov/#apod" target="_blank" rel='noreferrer'>here.</a></p>
         </div>
-    </div>
+    </StyledDetails>
   );
 }
 
---
+++ src/page-2/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import styled from 'styled-components';
+
+function App() {
+    
+}
