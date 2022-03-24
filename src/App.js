c78bcc55df5bf1aafdf11f8fe55dcb93b9d15f2f
Bennett Kautz <bennettkautz@gmail.com>
2022-03-23 20:44:55 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then(res => {
+        console.log(res.data);
+        setNasaData(res.data);
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+
   return (
     <div className="App">
       <p>
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            content
+        </footer>
+    )
+}
+
+
+export default Footer;
