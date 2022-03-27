9cf5cf60f34c9f505dd7cf8051b5dc534fb7bac4
Claire List <claire.list2501@gmail.com>
2022-03-27 08:58:37 -0600

    img styling fun

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -8,19 +8,24 @@ const NasaCard = (props) => {
         <div>
         <Card classname='nasacard'>
         <h1>Welcome to the NASA DISPLAY CARD!</h1>
+            
+            <Img src={data.url} alt={`Nasa Photo of the day for ${data.date}`} /> {/*eslint-disable-line*/}
+
             <h2>{data.title}</h2>
             <p>{data.date}</p>
-            <img src={data.url} alt={`Nasa Photo of the day for ${data.date}`} /> {/*eslint-disable-line*/}
         </Card>
         </div>
     )
 }
 
 const Card = styled.div`
-css: css;
 display: grid;
 align-items: center;
 justify-content: center;
+`
+const Img = styled.img`
+height: 200px;
+width: 250px;
 `
 
 export default NasaCard;
