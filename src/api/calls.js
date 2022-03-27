68f4c9cf8b54c941365b38227b64547105639739
Claire List <claire.list2501@gmail.com>
2022-03-27 09:30:21 -0600

    calls written skeleton

--- src/api/calls.js
+++ src/api/calls.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 import {API_KEY} from './keys';
 
 //get ALL 
+export const getAll = () => {
+    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=1995-06-16`)
+    .then(res=>{
 
-//start_date?=1995-06-16 --> first APOD day woo
-//do not need to specify end date; will default to today
+    }).catch(err=>{
+        console.error(err);
+    })
+}
 
 //get TODAY
-const getToday = () => {
+export const getToday = () => {
     return axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
         .then(res=>{
 
@@ -17,4 +22,11 @@ const getToday = () => {
 }
 
 //get RANDOM
-//count?=1 --> returns a random image.
+export const getRandom = () => {
+    return axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1`)
+        .then(res=>{
+
+        }).catch(err=>{
+            console.error(err);
+        })
+}
