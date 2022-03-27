c22f9bc798c5164749b4c0bd31adf99abdfb3aa4
Claire List <claire.list2501@gmail.com>
2022-03-27 08:52:00 -0600

    styled components time

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -1,22 +1,26 @@
-//this the nasa card component!
-//we will use this to fill out the info for display
-
-
-
 import React from 'react';
-//do something else
+import styled from 'styled-components';
 
 const NasaCard = (props) => {
     const {data} = props;
 
     return (
         <div>
+        <Card classname='nasacard'>
         <h1>Welcome to the NASA DISPLAY CARD!</h1>
             <h2>{data.title}</h2>
             <p>{data.date}</p>
-            <img src={data.url} alt={`Nasa Photo of the day for ${data.date}`} />
+            <img src={data.url} alt={`Nasa Photo of the day for ${data.date}`} /> {/*eslint-disable-line*/}
+        </Card>
         </div>
     )
 }
+
+const Card = styled.div`
+css: css;
+display: grid;
+align-items: center;
+justify-content: center;
+`
 
 export default NasaCard;
