72267b6b7b0be43b75a8d43be58455003f84f246
RealSadLemon <esmodearburk@gmail.com>
2022-05-18 18:30:09 -0500

    Connected the API to the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "./Components/navBar";
+import NavBar from "./components/navBar";
+import axios from "axios";
+import { BASE_URL, API_KEY } from "./constants/index";
 import "./App.css";
 
 function App() {
   const [typeOfImg, setTypeOfImg] = useState('auto');
   
+  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res=>{
+      console.log(res);
+    })
+    .catch(
+      console.log('Something went wrong grabbing the API')
+      )
 
   return (
     <div className="App">
