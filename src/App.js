16b3737b719bcc8d57232d389d32125454b5677e
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-12-15 16:34:50 -0500

    built out the code in App.js to fetch data from the NASA APOD API docs and created a NasaPhoto component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
 import "./App.css";
 
+import NasaPhoto from "./Components/NasaPhoto";
+
+
 function App() {
+  const [ data, setData ] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        console.log(res);
+        setData(res.data);
+      }).catch(err => console.error(err));
+  }, [])
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      { data && <NasaPhoto photo={data} /> } 
     </div>
   );
 }
