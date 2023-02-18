e2854bb1a37ec43238654821d3a6ffa0ffa802f8
DJ1 <jimmynoble604@gmail.com>
2023-02-18 06:46:42 -0500

    App.js started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
-
+const dummyData = {
+  date: "2023-02-18",
+  explanation: "A mere 56 million light-years distant toward the southern constellation Fornax, NGC 1365 is an enormous barred spiral galaxy about 200,000 light-years in diameter. That's twice the size of our own barred spiral Milky Way. This sharp image from the James Webb Space Telescope's Mid-Infrared Instrument (MIRI) reveals stunning details of this magnificent spiral in infrared light. Webb's field of view stretches about 60,000 light-years across NGC 1365, exploring the galaxy's core and bright newborn star clusters. The intricate network of dusty filaments and bubbles is created by young stars along spiral arms winding from the galaxy's central bar. Astronomers suspect the gravity field of NGC 1365's bar plays a crucial role in the galaxy's evolution, funneling gas and dust into a star-forming maelstrom and ultimately feeding material into the active galaxy's central, supermassive black hole.",
+  hdurl: "https://apod.nasa.gov/apod/image/2302/JWSTMIRI_ngc1365.png",
+  title: "Barred Spiral Galaxy NGC 1365 from Webb"
+}
 function App() {
-  return (
+  const [data, setData] = useState(dummyData)
+  console.log("data", data);
+//   useEffect(() => {
+//     console.log("use-effect");
+//     axios.get('https://api.nasa.gov/planetary/apod?api_key=RvdnAQWTacbtu2fV51dufeaYSGWpDFaaw6E8jAgK')
+//     .then(res => {
+//       console.log("res");
+//       console.log(res);
+//       setData(res.data);
+//     })
+//     .catch(err => {
+//       console.log("error");
+//       console.error(err)
+//     });
+// }, [])
+ return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    {data.title}
     </div>
   );
 }
 
 export default App;
+
