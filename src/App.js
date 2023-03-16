8a4d71e49d1c9893aedda7c57e105e9314672b29
Emrie <emrie@macbook-pro.mynetworksettings.com>
2023-03-16 08:34:43 -0400

    dont know how to stop timeout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState} from "react";
 import "./App.css";
 
+
 function App() {
+const [showbutton, setShowbutton] = useState(false)
+const [image, setImage] = useState("")
+
+setTimeout(()=>{
+  setShowbutton(true)
+}, 6000)
+
+let timeoutID;
+
+function startTimer() {
+  timeoutID = setTimeout(() => {
+    setShowbutton(true);
+  }, 6000);
+}
+
+function stopTimer() {
+  clearTimeout(timeoutID);
+}
+
+const getImage = () =>{
+  // window.location.href = "http://google.com"
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=wiaCGZbAYr12ybcE87dk6myOc7rjsu3B0sJUEUkf").then(res=>{
+    setImage(res.data.url);
+    setShowbutton(false);
+    
+  })
+}
+
+
+// const removeButton =()=>{
+//   setShowbutton(false)
+ 
+// }
+
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="App containter">
+      
+        <p className="line-1 anim-typewriter">Welcome to NASA Photo of the Day </p>
+        <button 
+        onClick={getImage} 
+        className="fade-in-button" 
+        style={{display: showbutton ? "": "none"}}>Enter</button>
+      {image && (<img src={image} />)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap');
+</style>
 
 ReactDOM.render(<App />, document.getElementById("root"));
