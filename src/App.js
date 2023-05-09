7c5be65d6c5b61cb0930ea9c9edbd6494d8d98ca
Jomar Banting <shadowlover.05@gmail.com>
2023-05-08 20:49:25 -0400

    Finished the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";  
 import "./App.css";
+import NasaPhoto from "./Components/NasaPhoto"
 
 const dummyData = {
 date: "2023-05-08",
@@ -10,15 +11,18 @@ title: "The Spanish Dancer Spiral Galaxy"
 }
 
 function App() {
-  const {data, setData} = useState(dummyData);   
-  // useEffect(() => {
-  //   axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-  //   .then(res => {
-  //     console.log(res);
-  //   }).catch(err => console.error(err))
-  // }, [])
+  const [data, setData] = useState();   
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(res => {
+      setData(res.data);
+    }).catch(err => console.error(err))
+  }, [])
+
+  
   return (
     <div className="App">
+      { data && <NasaPhoto photo={data}></NasaPhoto>}
     </div>
   );
 }
---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,16 @@
+import react from "react";
+import axios from "axios";
+
+const NasaPhoto = (props) => {
+    return (
+        <div className="nasa-photo-wrapper">
+            <h3>{props.photo.title}</h3>
+            <p>{props.photo.date}</p>
+            <img src={props.photo.hdurl}></img>
+            <p className="explanation">{props.photo.explanation}</p>
+
+        </div>
+    );
+}
+
+export default NasaPhoto;
