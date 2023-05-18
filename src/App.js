c390c02346b04fc259952615f03e9f9edf755a1d
Jomar Banting <shadowlover.05@gmail.com>
2023-05-18 19:35:59 -0400

    Adjusted the css of the website

--- src/App.js
+++ src/App.js
@@ -22,20 +22,11 @@ function App() {
   
   return (
     <div className="App">
-      <h1>Picture of the Day</h1>
+      <h1 className="title">Picture of the Day</h1>
       {console.log(data)}
       { data && <NasaPhoto photo={data}></NasaPhoto>}
     </div>
   );
 }
-
-/*
-date: "2023-05-08",
-explanation: "If not perfect, then this spiral galaxy is at least one of the most photogenic. An island universe containing billions of stars and situated about 40 million light-years away toward the constellation of the Dolphinfish (Dorado), NGC 1566 presents a gorgeous face-on view. Classified as a grand design spiral, NGC 1566 shows two prominent and graceful spiral arms that are traced by bright blue star clusters and dark cosmic dust lanes. Numerous Hubble Space Telescope images of NGC 1566 have been taken to study star formation, supernovas, and the spiral's unusually active center. Some of these images, stored online in the Hubble Legacy Archive, were freely downloaded, combined, and digitally processed by an industrious amateur to create the featured image. NGC 1566's flaring center makes the spiral one of the closest and brightest Seyfert galaxies, likely housing a central supermassive black hole wreaking havoc on surrounding stars and gas.   Almost Hyperspace: Random APOD Generator",
-hdurl: "https://apod.nasa.gov/apod/image/2305/NGC1566_HubbleOdenthal_1280.jpg",
-media_type: "image",
-service_version: "v1",
-title: "The Spanish Dancer Spiral Galaxy"
-*/
 
 export default App;
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,17 +1,20 @@
 import react from "react";
 import axios from "axios";
+import "./NasaPhoto.css";
 
 const NasaPhoto = (props) => {
     const { photo } = props;
     return (
         <div className="nasa-photo-wrapper">
-            {console.log(photo)}
             <h3>{photo.title}</h3>
             <p>{photo.date}</p>
             <a href={photo.url} target="blank">
-                <img src={props.photo.hdurl}></img>
+            <img src={props.photo.hdurl}></img>
             </a>
             <p className="explanation">{props.photo.explanation}</p>
+            <nav>
+                <a href="https://api.nasa.gov/">Nasa API website</a>
+            </nav>
         </div>
     );
 }
